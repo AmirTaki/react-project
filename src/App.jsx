b35f9cc2ee8ba8e2046a5342a113004edc127fe6
amir <amirtaki623@gmail.com>
2025-08-31 21:56:58 +0330

    part 4

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,15 @@ function App() {
   return (
     <>
       <header>
-        <div className="">
-          
-            <a>help</a>
-            <a>orders and returns</a>
-            <a>gift cards</a>
-            <a>join</a>
-            <a>adClud</a>
+        <div className="bg-gray-500 flex items-center justify-end h-[50px]">
+            <div className="">
+              <a>help</a>
+              <a>orders and returns</a>
+              <a>gift cards</a>
+              <a>join</a>
+              <a>adClud</a>
+            </div>
+           
         </div>
         <div className=""></div>
       </header>
