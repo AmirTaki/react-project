ab4fe0010ab420206c70d0750a439aab4ed07f9d
amir <amirtaki623@gmail.com>
2025-08-31 23:59:11 +0330

    part 42

--- src/App.jsx
+++ src/App.jsx
@@ -14,18 +14,18 @@ function App() {
       <header>
         <div className="bg-gray-100 flex items-center justify-end h-[40px] border-b-[2px] border-amber-300">
             <div className="flex gap-4 p-5 justify-between items-center">
-              <a className='cursor-pointer text-gray-500 hover:text-gray-950 hover:underline  '>help</a>
-              <a className='cursor-pointer text-gray-500 hover:text-gray-950 hover:underline'>orders and returns</a>
-              <a className='cursor-pointer text-gray-500 hover:text-gray-950 hover:underline'>gift cards</a>
-              <a className='cursor-pointer text-gray-500 hover:text-gray-950 hover:underline'>join</a>
-              <a className='cursor-pointer text-gray-500 hover:text-gray-950 hover:underline'>adClud</a>
+              <a className='cursor-pointer text-gray-500 hover:text-gray-950! hover:underline  '>help</a>
+              <a className='cursor-pointer text-gray-500 hover:text-gray-950! hover:underline'>orders and returns</a>
+              <a className='cursor-pointer text-gray-500 hover:text-gray-950! hover:underline'>gift cards</a>
+              <a className='cursor-pointer text-gray-500 hover:text-gray-950! hover:underline'>join</a>
+              <a className='cursor-pointer text-gray-500 hover:text-gray-950! hover:underline'>adClud</a>
             </div>
         </div>
         <div className="h-[60px] bg-white- flex items-center justify-between shadow-lg shadow-gray-600 px-6">
-          <div className="  " >
+          <div className="" >
             <img src={logo} alt="" className='w-20' />
           </div>
-          <div className="flex gap-7  ">
+          <div className=" gap-7  max-md:hidden  md:flex ">
             <div className="bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer">item</div>
             <div className="bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer">item</div>
             <div className="bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer">item</div>
@@ -51,3 +51,4 @@ function App() {
 }
 
 export default App
+
