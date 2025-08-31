b294e144e07df4f9b3e84f525cd8a3e7761a322b
amir <amirtaki623@gmail.com>
2025-08-31 22:01:08 +0330

    part 6

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <header>
-        <div className="bg-gray-100 flex items-center justify-end h-[50px] border-b border-b-[2px] border-amber-300">
+        <div className="bg-gray-100 flex items-center justify-end h-[50px] border-b-[2px] border-amber-300">
             <div className="flex gap-3 p-5">
               <a>help</a>
               <a>orders and returns</a>
