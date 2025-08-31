35267f3d8ee86c1a3529cbfa0e68422eda604e6a
amir <amirtaki623@gmail.com>
2025-08-31 21:52:29 +0330

    part 1

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ function App() {
 
   return (
     <>
-      <h1 className='text-blue-500'>application</h1>
+      <header>
+        <div className=""></div>
+        <div className=""></div>
+      </header>
     </>
   )
 }
