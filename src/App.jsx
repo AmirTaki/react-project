bb1a2433ebdcb9fec060a2eaa20db1a03d265160
amir <amirtaki623@gmail.com>
2025-08-31 22:25:59 +0330

    part 12

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ function App() {
   return (
     <>
       <header>
-        <div className="bg-gray-100 flex items-center justify-end h-[50px] border-b-[2px] border-amber-300">
-            <div className="flex gap-3 p-5">
-              <a className='cursor-pointer text-gray-500 hover:text-gray-950 hover:underline '>help</a>
+        <div className="bg-gray-100 flex items-center justify-end h-[40px] border-b-[2px] border-amber-300">
+            <div className="flex gap-4 p-5 justify-between items-center">
+              <a className='cursor-pointer text-gray-500 hover:text-gray-950 hover:underline  '>help</a>
               <a className='cursor-pointer text-gray-500 hover:text-gray-950 hover:underline'>orders and returns</a>
               <a className='cursor-pointer text-gray-500 hover:text-gray-950 hover:underline'>gift cards</a>
               <a className='cursor-pointer text-gray-500 hover:text-gray-950 hover:underline'>join</a>
