05fc84f155fb0e2d3bb7549f7238f6fff6d3f74c
amir <amirtaki623@gmail.com>
2025-09-01 23:38:29 +0330

    day 2 part 32

--- src/Compnonet/headerBottom.jsx
+++ src/Compnonet/headerBottom.jsx
@@ -122,9 +122,7 @@ const  HeaderBottom = () => {
 
             {/* ------------- */}
                 {/* container mega menu */}
-                <div className="flex" >
-              
-                    {/* row 2 */}
+                 {/* row 2 */}
                     <div   className={` ${navbar ? "  group border-b-1!  border-b-gray-100! hover:bg-sky-100 flex items-center justify-between px-3 cursor-pointer hover:text-gray-950!" : "group bg-amber-500 "}`}>
                         <div className=" group-hover:text-black bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer max-md:border-0! max-md:text-gray-500 ">
                             item
@@ -181,8 +179,7 @@ const  HeaderBottom = () => {
                             </div>
                         
                         </div>
-                    </div>                
-                </div>
+                    </div>      
                 {/* end container mega menu */}
   
             
