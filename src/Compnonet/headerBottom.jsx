f0af74bc552f78c6c5009023d4695e3b054a9e3f
amir <amirtaki623@gmail.com>
2025-09-01 22:17:30 +0330

    day 2 prat 28

--- src/Compnonet/headerBottom.jsx
+++ src/Compnonet/headerBottom.jsx
@@ -1,10 +1,10 @@
 import logo from "../assets/logo.webp";
 import { createContext, useEffect, useState } from 'react';
-import MegaMenu from "./megaMenu";
 
 
 
-export const MegaMenuContext =  createContext("")
+
+
 const  HeaderBottom = () => {
     
     const [search, setSecarch] =  useState(false);
@@ -57,7 +57,6 @@ const  HeaderBottom = () => {
             </div>
             <div className= {`${navbar ? "bg-white fixed w-full top-0  -left-full z-30! ": "flex gap-7"} ${moveNavbar ? "-left-0! duration-800": "-left-full! duration-800"}` }>
                 {/* row 1 */}
-
                 <div className={navbar ? "flex justify-between items-center p-3 shadow-gray-300 border border-b-1 border-b-gray-300 h-[50px] " : "hidden"}>
                     <i className="bi bi-x-lg text-gray-300  hover:text-gray-700 cursor-pointer  duration-200 hover:scale-130" onClick={menuHandler} ></i>
                     <img src={logo} alt="" className='w-10' />
@@ -67,76 +66,70 @@ const  HeaderBottom = () => {
                     </div>
                 </div>
 
-                <userContext.Provider   value = {{search, setSecarch, navbar, setNavbar,moveNavbar, setMoveNavbar, megeMenu , setMegMenu}}>
-                    <MegaMenu />
-                </userContext.Provider>
+       
                 {/* container mega menu */}
                 <div className="flex" >
               
-                {/* row 2 */}
-                <div  onClick={handlerMegaMenu} onMouseLeave={HandlerMouseLeaveMegaMenu} className={` ${navbar ? "  group border-b-1!  border-b-gray-100! hover:bg-sky-100 flex items-center justify-between px-3 cursor-pointer hover:text-gray-950!" : "item bg-amber-500 "}`}>
-                    <div className=" group-hover:text-black bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer max-md:border-0! max-md:text-gray-500 ">
-                        item
-                    </div>
-                    <i className="bi bi-chevron-down text-gray-300 hover:text-red-500 md:hidden group-hover:rotate-180 duration-300 group-hover:text-red-500"></i>
+                    {/* row 2 */}
+                    <div  onClick={handlerMegaMenu} onMouseLeave={HandlerMouseLeaveMegaMenu} className={` ${navbar ? "  group border-b-1!  border-b-gray-100! hover:bg-sky-100 flex items-center justify-between px-3 cursor-pointer hover:text-gray-950!" : "item bg-amber-500 "}`}>
+                        <div className=" group-hover:text-black bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer max-md:border-0! max-md:text-gray-500 ">
+                            item
+                        </div>
+                        <i className="bi bi-chevron-down text-gray-300 hover:text-red-500 md:hidden group-hover:rotate-180 duration-300 group-hover:text-red-500"></i>
 
-                    {/* MegaMenu */}
-                    <div className={` ${navbar ? "fixed hidden" : " hidden  fixed top-[101px] left-0 w-[100%] h-[500px] bg-gray-100 z-50   justify-center  "   } ${megeMenu ? "flex! " : "hidden!"} `   } >
-                        {/* column1 */}
-                        <div className={`${navbar ? "" :"bg-yellow-200 w-[300px]"}`}>
-                            <div>item</div>
-                            <div>item</div>
-                            <div>item</div>
-                            <div>item</div>
-                            <div>item</div>
-                            <div>item</div>
-                            <div>item</div>
-                            <div>item</div>
+                        {/* MegaMenu */}
+                        <div className={` ${navbar ? "fixed hidden" : " hidden  fixed top-[101px] left-0 w-[100%] h-[500px] bg-gray-100 z-50   justify-center  "   } ${megeMenu ? "flex! " : "hidden!"} `   } >
+                            {/* column1 */}
+                            <div className={`${navbar ? "" :"bg-yellow-200 w-[300px]"}`}>
+                                <div>item</div>
+                                <div>item</div>
+                                <div>item</div>
+                                <div>item</div>
+                                <div>item</div>
+                                <div>item</div>
+                                <div>item</div>
+                                <div>item</div>
+                            </div>
+                            <div className={`${navbar ? "" : "flex "}`}>
+                                {/* column2 */}
+                                <div className={`${navbar ? "" : "bg-red-200 w-[300px]"}`}>
+                                    <div>item</div>
+                                    <div>item</div>
+                                    <div>item</div>
+                                    <div>item</div>
+                                    <div>item</div>
+                                    <div>item</div>
+                                    <div>item</div>
+                                    <div>item</div>
+                                </div>
+                                {/* column3 */}
+                                <div className={`${navbar ? "" : "bg-blue-200 w-[300px]"}`}>
+                                    <div>item</div>
+                                    <div>item</div>
+                                    <div>item</div>
+                                    <div>item</div>
+                                    <div>item</div>
+                                    <div>item</div>
+                                    <div>item</div>
+                                    <div>item</div>
+                                </div>
+                                {/* column4 */}
+                                <div className={`${navbar ? "" : "bg-green-200 w-[300px]"}`}>
+                                    <div>item</div>
+                                    <div>item</div>
+                                    <div>item</div>
+                                    <div>item</div>
+                                    <div>item</div>
+                                    <div>item</div>
+                                    <div>item</div>
+                                    <div>item</div>
+                                </div>
+                            </div>
+                        
                         </div>
-                        <div className={`${navbar ? "" : "flex "}`}>
-                        {/* column2 */}
-                        <div className={`${navbar ? "" : "bg-red-200 w-[300px]"}`}>
-                            <div>item</div>
-                            <div>item</div>
-                            <div>item</div>
-                            <div>item</div>
-                            <div>item</div>
-                            <div>item</div>
-                            <div>item</div>
-                            <div>item</div>
-                        </div>
-                        {/* column3 */}
-                        <div className={`${navbar ? "" : "bg-blue-200 w-[300px]"}`}>
-                            <div>item</div>
-                            <div>item</div>
-                            <div>item</div>
-                            <div>item</div>
-                            <div>item</div>
-                            <div>item</div>
-                            <div>item</div>
-                            <div>item</div>
-                        </div>
-                        {/* column4 */}
-                        <div className={`${navbar ? "" : "bg-green-200 w-[300px]"}`}>
-                            <div>item</div>
-                            <div>item</div>
-                            <div>item</div>
-                            <div>item</div>
-                            <div>item</div>
-                            <div>item</div>
-                            <div>item</div>
-                            <div>item</div>
-                        </div>
-                    </div>
-                    
+                    </div>                
                 </div>
-              </div>
-              
-          
-              
-                
-            </div>
-            {/* end container mega menu */}
+                {/* end container mega menu */}
 
             {/* ------------- */}
   
