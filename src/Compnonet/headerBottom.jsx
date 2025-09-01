e002de70fbf65d2499ba03efc3a24f24d9153a9d
amir <amirtaki623@gmail.com>
2025-09-01 23:43:40 +0330

    day 2 part 33

--- src/Compnonet/headerBottom.jsx
+++ src/Compnonet/headerBottom.jsx
@@ -61,16 +61,16 @@ const  HeaderBottom = () => {
        
                 {/* container mega menu */}
                    {/* row 2 */}
-                    <div   className={` ${navbar ? "  group border-b-1!  border-b-gray-100! hover:bg-sky-100 flex items-center justify-between px-3 cursor-pointer hover:text-gray-950!" : "group bg-amber-500 "}`}>
+                    <div   className={` ${navbar ? "  group border-b-1! bg-yellow-500  border-b-gray-100! hover:bg-sky-100 flex items-center justify-between px-3 cursor-pointer hover:text-gray-950!" : "group bg-amber-500 "}`}>
                         <div className=" group-hover:text-black bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer max-md:border-0! max-md:text-gray-500 ">
                             item
                         </div>
                         <i className="bi bi-chevron-down text-gray-300 hover:text-red-500 md:hidden group-hover:rotate-180 duration-300 group-hover:text-red-500"></i>
 
                         {/* MegaMenu */}
-                        <div className={` ${navbar ? "fixed hidden" : " group-hover:flex! hidden  fixed top-[101px] left-0 w-[100%] h-[500px] bg-gray-100 z-50   justify-center  "   } `   } >
+                        <div className={` ${navbar ? " " : " group-hover:flex! hidden  fixed top-[101px] left-0 w-[100%] h-[500px] bg-gray-100 z-50   justify-center  "   } `   } >
                             {/* column1 */}
-                            <div className={`${navbar ? "" :"bg-yellow-200 w-[300px]"}`}>
+                            <div className={`${navbar ? "fixed bg-red-500 left-0 top-[100px] overflow-hidden w-full" :"bg-yellow-200 w-[300px]"}`}>
                                 <div>item</div>
                                 <div>item</div>
                                 <div>item</div>
@@ -80,7 +80,7 @@ const  HeaderBottom = () => {
                                 <div>item</div>
                                 <div>item</div>
                             </div>
-                            <div className={`${navbar ? "" : "flex "}`}>
+                            <div className={`${navbar ? "hidden" : "flex "}`}>
                                 {/* column2 */}
                                 <div className={`${navbar ? "" : "bg-red-200 w-[300px]"}`}>
                                     <div>item</div>
