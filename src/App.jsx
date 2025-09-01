de10364facc78ddee839fcb153b4aac8a59e1e93
amir <amirtaki623@gmail.com>
2025-09-01 17:53:36 +0330

    day 2 part 9

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   const [search, setSecarch] =  useState(false);
   const [navbar, setNavbar] = useState(false);
   const [moveNavbar, setMoveNavbar] =  useState(false)
+  const [megeMenu , setMegMenu] = useState(false);
 
 
   const handerResize = () =>{
@@ -27,6 +28,10 @@ function App() {
 
   const menuHandler = () => {
     setMoveNavbar(!moveNavbar)
+
+  }
+
+  const handlerMegaMenu = () => {
 
   }
   return (
@@ -65,7 +70,7 @@ function App() {
             </div>
             {/* row 2 */}
             <div className="">
-              <div className={`${navbar ? " group border-b-1!  border-b-gray-100! hover:bg-sky-100 flex items-center justify-between px-3 cursor-pointer hover:text-gray-950!" : ""}`}>
+              <div onTouchStart={handlerMegaMenu} className={`${navbar ? " group border-b-1!  border-b-gray-100! hover:bg-sky-100 flex items-center justify-between px-3 cursor-pointer hover:text-gray-950!" : ""}`}>
                 <div className=" group-hover:text-black bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer max-md:border-0! max-md:text-gray-500 ">item</div>
                 <i className="bi bi-chevron-down text-gray-300 hover:text-red-500 md:hidden group-hover:rotate-180 duration-300 group-hover:text-red-500"></i>
               </div>
