9daf1599fe5250895ec53a023490fe66b7ff65f1
amir <amirtaki623@gmail.com>
2025-09-01 18:37:40 +0330

    day 2 part 12

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ function App() {
   const [megeMenu , setMegMenu] = useState(false);
 
 
+  
+
   const handerResize = () =>{
       setNavbar(window.innerWidth <= 768 ? true : false);
       setSecarch(false)
@@ -31,6 +33,9 @@ function App() {
 
   }
 
+  const mg = (document.querySelectorAll(".mg").length)
+
+ 
   const handlerMegaMenu = () => {
     setMegMenu(false)
     setMegMenu(true)
@@ -62,6 +67,7 @@ function App() {
           </div>
           <div className= {`${navbar ? "bg-white fixed w-full top-0  -left-full z-30! ": "flex gap-7"} ${moveNavbar ? "-left-0! duration-800": "-left-full! duration-800"}` }>
             {/* row 1 */}
+           
             <div className={navbar ? "flex justify-between items-center p-3 shadow-gray-300 border border-b-1 border-b-gray-300 h-[50px] " : "hidden"}>
               
               <i className="bi bi-x-lg text-gray-300  hover:text-gray-700 cursor-pointer  duration-200 hover:scale-130" onClick={menuHandler} ></i>
@@ -72,15 +78,19 @@ function App() {
               </div>
             
             </div>
-            {/* row 2 */}
-            <div className="" onMouseLeave={HandlerMouseLeaveMegaMenu}>
-              <div  onMouseOver={handlerMegaMenu} className={`${navbar ? " group border-b-1!  border-b-gray-100! hover:bg-sky-100 flex items-center justify-between px-3 cursor-pointer hover:text-gray-950!" : ""}`}>
+
+
+            {/* container mega menu */}
+            <div className="bg-amber-950 flex gap-5" onMouseLeave={HandlerMouseLeaveMegaMenu}>
+              
+              {/* row 2 */}
+              <div  onMouseOver={handlerMegaMenu} className={`mg ${navbar ? " group border-b-1!  border-b-gray-100! hover:bg-sky-100 flex items-center justify-between px-3 cursor-pointer hover:text-gray-950!" : ""}`}>
                 <div className=" group-hover:text-black bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer max-md:border-0! max-md:text-gray-500 ">item</div>
                 <i className="bi bi-chevron-down text-gray-300 hover:text-red-500 md:hidden group-hover:rotate-180 duration-300 group-hover:text-red-500"></i>
               </div>
               
               {/* MegaMenu */}
-              <div className={`${navbar ? "" : "hidden fixed top-[101px] left-0 w-[100%] h-[500px] bg-gray-100 z-50   justify-center "   }  ${megeMenu ? "flex!" : "hidden!"} `  } >
+              <div className={` ${navbar ? "" : " hidden fixed top-[101px] left-0 w-[100%] h-[500px] bg-gray-100 z-50   justify-center "   }  ${megeMenu ? "flex!" : "hidden!"} `  } >
                   {/* column1 */}
                   <div className={`${navbar ? "" :"bg-yellow-200 w-[300px]"}`}>
                     <div>item</div>
@@ -128,35 +138,68 @@ function App() {
                     </div>
                   </div>
                  
+              </div>    
+
+              {/* row 3 */}
+              <div  onMouseOver={handlerMegaMenu} className={`mg ${navbar ? " group border-b-1!  border-b-gray-100! hover:bg-sky-100 flex items-center justify-between px-3 cursor-pointer hover:text-gray-950!" : ""}`}>
+                <div className=" group-hover:text-black bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer max-md:border-0! max-md:text-gray-500 ">item</div>
+                <i className="bi bi-chevron-down text-gray-300 hover:text-red-500 md:hidden group-hover:rotate-180 duration-300 group-hover:text-red-500"></i>
               </div>
-            
+              
+              {/* MegaMenu */}
+              <div className={` ${navbar ? "" : " hidden fixed top-[101px] left-0 w-[100%] h-[500px] bg-gray-100 z-50   justify-center "   }  ${megeMenu ? "flex!" : "hidden!"} `  } >
+                  {/* column1 */}
+                  <div className={`${navbar ? "" :"bg-orange-200 w-[300px]"}`}>
+                    <div>item</div>
+                    <div>item</div>
+                    <div>item</div>
+                    <div>item</div>
+                    <div>item</div>
+                    <div>item</div>
+                    <div>item</div>
+                    <div>item</div>
+                  </div>
+                  <div className={`${navbar ? "" : "flex "}`}>
+                     {/* column2 */}
+                    <div className={`${navbar ? "" : "bg-red-200 w-[300px]"}`}>
+                      <div>item</div>
+                      <div>item</div>
+                      <div>item</div>
+                      <div>item</div>
+                      <div>item</div>
+                      <div>item</div>
+                      <div>item</div>
+                      <div>item</div>
+                    </div>
+                    {/* column3 */}
+                     <div className={`${navbar ? "" : "bg-blue-200 w-[300px]"}`}>
+                      <div>item</div>
+                      <div>item</div>
+                      <div>item</div>
+                      <div>item</div>
+                      <div>item</div>
+                      <div>item</div>
+                      <div>item</div>
+                      <div>item</div>
+                    </div>
+                    {/* column4 */}
+                      <div className={`${navbar ? "" : "bg-green-200 w-[300px]"}`}>
+                      <div>item</div>
+                      <div>item</div>
+                      <div>item</div>
+                      <div>item</div>
+                      <div>item</div>
+                      <div>item</div>
+                      <div>item</div>
+                      <div>item</div>
+                    </div>
+                  </div>
+                 
+              </div> 
+            {/* end container mega menu */}
             </div>
-            
-            
-
-
-            <div className={`${navbar ? " group border-b-1!  border-b-gray-100! hover:bg-sky-100 flex items-center justify-between px-3 cursor-pointer hover:text-gray-950!" : ""}`}>
-              <div className=" group-hover:text-black bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer max-md:border-0! max-md:text-gray-500 ">item</div>
-              <i className="bi bi-chevron-down text-gray-300 hover:text-red-500 md:hidden group-hover:rotate-180 duration-300 group-hover:text-red-500"></i>
-            </div>
-            
-            <div className={`${navbar ? " group border-b-1!  border-b-gray-100! hover:bg-sky-100 flex items-center justify-between px-3 cursor-pointer hover:text-gray-950!" : ""}`}>
-              <div className=" group-hover:text-black bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer max-md:border-0! max-md:text-gray-500 ">item</div>
-              <i className="bi bi-chevron-down text-gray-300 hover:text-red-500 md:hidden group-hover:rotate-180 duration-300 group-hover:text-red-500"></i>
-            </div>
-            
-            <div className={`${navbar ? " group border-b-1!  border-b-gray-100! hover:bg-sky-100 flex items-center justify-between px-3 cursor-pointer hover:text-gray-950!" : ""}`}>
-              <div className=" group-hover:text-black bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer max-md:border-0! max-md:text-gray-500 ">item</div>
-              <i className="bi bi-chevron-down text-gray-300 hover:text-red-500 md:hidden group-hover:rotate-180 duration-300 group-hover:text-red-500"></i>
-            </div>
-            
-            <div className={`${navbar ? " group border-b-1!  border-b-gray-100! hover:bg-sky-100 flex items-center justify-between px-3 cursor-pointer hover:text-gray-950!" : ""}`}>
-              <div className=" group-hover:text-black bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer max-md:border-0! max-md:text-gray-500 ">item</div>
-              <i className="bi bi-chevron-down text-gray-300 hover:text-red-500 md:hidden group-hover:rotate-180 duration-300 group-hover:text-red-500"></i>
-            </div>
-            
+       
           
-
           </div>
 
 
