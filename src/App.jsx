7fc7bd9d3fe7fa06530b1523fa2a72eb7b8b8687
amir <amirtaki623@gmail.com>
2025-09-01 03:44:27 +0330

    part 55

--- src/App.jsx
+++ src/App.jsx
@@ -42,6 +42,9 @@ window.addEventListener('resize',handerResize )
             <img src={logo} alt="" className='w-20' />
           </div>
           <div className= {navbar ? "bg-green-500 fixed w-full top-0 left-0" : "flex gap-7"} >
+            <div className={navbar ? "flex bg-pink-500" : "hidden"}>
+
+            </div>
             <div className="bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer">item</div>
             <div className="bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer">item</div>
             <div className="bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer">item</div>
