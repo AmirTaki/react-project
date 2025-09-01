c5e64f29952119d20489e641f8ddc06447af9a2a
amir <amirtaki623@gmail.com>
2025-09-01 17:29:38 +0330

    day 2 part 1

--- src/App.jsx
+++ src/App.jsx
@@ -67,8 +67,13 @@ function App() {
             <div className={`${navbar ? " group border-b-1!  border-b-gray-100! hover:bg-sky-100 flex items-center justify-between px-3 cursor-pointer hover:text-gray-950!" : ""}`}>
               <div className=" group-hover:text-black bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer max-md:border-0! max-md:text-gray-500 ">item</div>
               <i className="bi bi-chevron-down text-gray-300 hover:text-red-500 md:hidden group-hover:rotate-180 duration-300 group-hover:text-red-500"></i>
+              {/* MegaMenu */}
+              <div className={`${navbar ? "fixed top-0 w-full h-[500px] bg-amber-300" : ""   }`} ></div>
+
             </div>
             
+
+
             <div className={`${navbar ? " group border-b-1!  border-b-gray-100! hover:bg-sky-100 flex items-center justify-between px-3 cursor-pointer hover:text-gray-950!" : ""}`}>
               <div className=" group-hover:text-black bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer max-md:border-0! max-md:text-gray-500 ">item</div>
               <i className="bi bi-chevron-down text-gray-300 hover:text-red-500 md:hidden group-hover:rotate-180 duration-300 group-hover:text-red-500"></i>
