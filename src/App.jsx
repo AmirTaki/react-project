f88f197caa8affaf0e3f81c37299b7de83f179f9
amir <amirtaki623@gmail.com>
2025-09-01 17:31:53 +0330

    day 2 part 2

--- src/App.jsx
+++ src/App.jsx
@@ -68,7 +68,7 @@ function App() {
               <div className=" group-hover:text-black bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer max-md:border-0! max-md:text-gray-500 ">item</div>
               <i className="bi bi-chevron-down text-gray-300 hover:text-red-500 md:hidden group-hover:rotate-180 duration-300 group-hover:text-red-500"></i>
               {/* MegaMenu */}
-              <div className={`${navbar ? "fixed top-0 w-full h-[500px] bg-amber-300" : ""   }`} ></div>
+              <div className={`${navbar ? "" : "fixed top-[101px] left-0 w-[100%] h-[500px] bg-gray-100 z-50"   }`} ></div>
 
             </div>
             
