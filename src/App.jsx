78e9dee4307203723586c21bb709189e2e19b62e
amir <amirtaki623@gmail.com>
2025-09-01 05:07:16 +0330

    part 75

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,8 @@ function App() {
             
             </div>
             {/* row 2 */}
-            <div className="bg-transparent flex items-center justify-between px-3 max-md:cursor-pointer  max-md:hover:text-gray-950!">
+        
+            <div className={`${navbar ? "border-b-1! border-b-gray-500! bg-amber-400  flex items-center justify-between px-3 cursor-pointer hover:text-gray-950!" : ""}`}>
               <div className=" bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer max-md:border-0! max-md:text-gray-500 ">item</div>
               <i className="bi bi-chevron-right text-gray-300 hover:text-red-500 md:hidden "></i>
             </div>
