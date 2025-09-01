8de69e470684a3d7bc1623e3abe8c574b63f49a5
amir <amirtaki623@gmail.com>
2025-09-01 17:34:06 +0330

    day 2 part 3

--- src/App.jsx
+++ src/App.jsx
@@ -64,13 +64,17 @@ function App() {
             
             </div>
             {/* row 2 */}
-            <div className={`${navbar ? " group border-b-1!  border-b-gray-100! hover:bg-sky-100 flex items-center justify-between px-3 cursor-pointer hover:text-gray-950!" : ""}`}>
-              <div className=" group-hover:text-black bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer max-md:border-0! max-md:text-gray-500 ">item</div>
-              <i className="bi bi-chevron-down text-gray-300 hover:text-red-500 md:hidden group-hover:rotate-180 duration-300 group-hover:text-red-500"></i>
+            <div className="">
+              <div className={`${navbar ? " group border-b-1!  border-b-gray-100! hover:bg-sky-100 flex items-center justify-between px-3 cursor-pointer hover:text-gray-950!" : ""}`}>
+                <div className=" group-hover:text-black bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer max-md:border-0! max-md:text-gray-500 ">item</div>
+                <i className="bi bi-chevron-down text-gray-300 hover:text-red-500 md:hidden group-hover:rotate-180 duration-300 group-hover:text-red-500"></i>
+              </div>
+              
               {/* MegaMenu */}
               <div className={`${navbar ? "" : "fixed top-[101px] left-0 w-[100%] h-[500px] bg-gray-100 z-50"   }`} ></div>
-
+            
             </div>
+            
             
 
 
