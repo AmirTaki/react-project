c730eb0e56a5fd627574f1b2bd5e7abab154ddb5
amir <amirtaki623@gmail.com>
2025-09-02 20:55:48 +0330

    day 3 part 1

--- src/Compnonet/headerBottom.jsx
+++ src/Compnonet/headerBottom.jsx
@@ -6,17 +6,11 @@ import { createContext, useEffect, useState } from 'react';
 
 
 const  HeaderBottom = () => {
-    
+ 
     const [search, setSecarch] =  useState(false);
     const [navbar, setNavbar] = useState(false);
     const [moveNavbar, setMoveNavbar] =  useState(false)
-    const [activeSlideBar, setActiveSlideBar] =  useState(null)
-    const [listItems, setListItems] =  useState([])
-    
-    const n = document.querySelectorAll('.list').length
-    const list = []
-    for (let i = 1 ; i < n + 1 ; i++)    list.push('item'+i)
-    
+   
     
     
 
@@ -41,10 +35,6 @@ const  HeaderBottom = () => {
 
     }
   
-
-    const HandlerOpenSlider = (index) => {
-        setActiveSlideBar(index)
-    }
     return(
         <div className="h-[60px] bg-white- flex items-center justify-between shadow-lg shadow-gray-600 px-6">
             {/* LEFT */}
@@ -57,175 +47,11 @@ const  HeaderBottom = () => {
             </div>
 
             {/* MEDDLE */}
-            <div className= {`${navbar ? "bg-white fixed w-full top-0  -left-full z-30! ": "flex gap-7"} ${moveNavbar ? "-left-0! duration-800": "-left-full! duration-800"}` }>
-                {/* row 1 */}
-                <div className={navbar ? "flex justify-between items-center p-3 shadow-gray-300 border border-b-1 border-b-gray-300 h-[50px] " : "hidden"}>
-                    <i className="bi bi-x-lg text-gray-300  hover:text-gray-700 cursor-pointer  duration-200 hover:scale-130" onClick={menuHandler} ></i>
-                    <img src={logo} alt="" className='w-10' />
-                    <div className="relative">
-                        <input type="search" placeholder='search' className={search ? 'w-[200px] duration-900 ease-in bg-gray-100 px-3 focus:outline-hidden rounded-md h-[30px]' : 'w-[0px] duration-900 ease-out'}    />
-                        <i className="bi bi-search text-gray-400 cursor-pointer text-lg hover:scale-130    max-md:right-[10px] absolute hover:text-gray-900 duration-300"   onClick={handlerSerch}></i>
-                    </div>
-                </div>
-
-            
-                {/* container mega menu */}
-                   {/* row 2 */}
-                    <div   className={` ${navbar ? "  group border-b-1! bg-yellow-500  border-b-gray-100! hover:bg-sky-100 flex items-center justify-between px-3 cursor-pointer hover:text-gray-950!" : "group bg-red-500 "}`}>
-                        <div className=" group-hover:text-black bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer max-md:border-0! max-md:text-gray-500 ">
-                            item
-                        </div>
-                        <i className="bi bi-chevron-down text-gray-300 hover:text-red-500 md:hidden group-hover:rotate-180 duration-300 group-hover:text-red-500"></i>
-
-                        {/* MegaMenu  => group-hover:flex!*/}
-                        <div className={` ${navbar ? " fixed" : " flex!  hidden fixed top-[101px] left-0 w-[100%] h-[500px] bg-gray-100 z-50   justify-center  "   } `   } >
-                            {/* column1 */}
-                            <div className={`${navbar ? " bg-white fixed  left-0 top-[110px] h-0  group-hover:h-[400px]! group-active:bg-gray-100!  duration-300  overflow-hidden w-full" :"bg-yellow-200 w-[300px] border-r-1 border-gray-200"}`}>
-                              
-                                <div className={`${navbar ? "hidden" :"bg-white text-gray-300 h-[50px] flex items-center p-3"}`}> LIST ITEM</div>
-                              
-                                <div className={`list ${navbar ? "hover:bg-sky-100! bg-gray-100 cursor-pointer flex justify-between items-center  h-[50px] p-4" : "cursor-context-menu"}`} onClick={()=> {HandlerOpenSlider(1)}}>
-                                    <div className={` ${navbar ? "text-gray-600" :"  bg-white h-[40px] flex items-center p-3 text-gray-400 hover:bg-gray-100 duration-75 hover:text-blue-600 "}`} >For Home</div>
-                                    <i className={`bi bi-chevron-right ${navbar ? "block text-gray-600" : "hidden"  } `}></i>
-                                </div>
-                              
-                                <div className={`list ${navbar ? "hover:bg-sky-100! bg-gray-100 cursor-pointer flex justify-between items-center  h-[50px] p-4" : "cursor-context-menu"}`} onClick={()=> {HandlerOpenSlider(2)}}>
-                                    <div className={` ${navbar ? "text-gray-600" :"  bg-white h-[40px] flex items-center p-3 text-gray-400 hover:bg-gray-100 duration-75 hover:text-blue-600 "}`} >For Work</div>
-                                    <i className={`bi bi-chevron-right ${navbar ? "block text-gray-600" : "hidden"  } `}></i>
-                                </div>
-                                
-                                <div className={`list ${navbar ? "hover:bg-sky-100! bg-gray-100 cursor-pointer flex justify-between items-center  h-[50px] p-4" : "cursor-context-menu"}`} onClick={()=> {HandlerOpenSlider(3)}}>
-                                    <div className={` ${navbar ? "text-gray-600" :"  bg-white h-[40px] flex items-center p-3 text-gray-400 hover:bg-gray-100 duration-75 hover:text-blue-600 "}`} >For Creators</div>
-                                    <i className={`bi bi-chevron-right ${navbar ? "block text-gray-600" : "hidden"  } `}></i>
-                                </div>
-                                
-                                <div className={`list ${navbar ? "hover:bg-sky-100! bg-gray-100 cursor-pointer flex justify-between items-center  h-[50px] p-4" : "cursor-context-menu"}`} onClick={()=> {HandlerOpenSlider(4)}}>
-                                    <div className={` ${navbar ? "text-gray-600" :"  bg-white h-[40px] flex items-center p-3 text-gray-400 hover:bg-gray-100 duration-75 hover:text-blue-600 "}`} >For Student</div>
-                                    <i className={`bi bi-chevron-right ${navbar ? "block text-gray-600" : "hidden"  } `}></i>
-                                </div>
-                                
-                                <div className={`list ${navbar ? "hover:bg-sky-100! bg-gray-100 cursor-pointer flex justify-between items-center  h-[50px] p-4" : "cursor-context-menu"}`} onClick={()=> {HandlerOpenSlider(5)}}>
-                                    <div className={` ${navbar ? "text-gray-600" :"  bg-white h-[40px] flex items-center p-3 text-gray-400 hover:bg-gray-100 duration-75 hover:text-blue-600 "}`} >For Gaming</div>
-                                    <i className={`bi bi-chevron-right ${navbar ? "block text-gray-600" : "hidden"  } `}></i>
-                                </div>
-                                
-                                <div className={`list ${navbar ? "hover:bg-sky-100! bg-gray-100 cursor-pointer flex justify-between items-center  h-[50px] p-4" : "cursor-context-menu"}`} onClick={()=> {HandlerOpenSlider(6)}}>
-                                    <div className={` ${navbar ? "text-gray-600" :"  bg-white h-[40px] flex items-center p-3 text-gray-400 hover:bg-gray-100 duration-75 hover:text-blue-600 "}`} >Software</div>
-                                    <i className={`bi bi-chevron-right ${navbar ? "block text-gray-600" : "hidden"  } `}></i>
-                                </div>
-                                
-                                <div className={`list ${navbar ? "hover:bg-sky-100! bg-gray-100 cursor-pointer flex justify-between items-center  h-[50px] p-4" : "cursor-context-menu"}`} onClick={()=> {HandlerOpenSlider(7)}}>
-                                    <div className={` ${navbar ? "text-gray-600" :"  bg-white h-[40px] flex items-center p-3 text-gray-400 hover:bg-gray-100 duration-75 hover:text-blue-600 "}`} >Technology / Inovations</div>
-                                    <i className={`bi bi-chevron-right ${navbar ? "block text-gray-600" : "hidden"  } `}></i>
-                                </div>    
-                            </div>
-
-                            <div className={`${navbar ? "hidden" : "flex "} ${activeSlideBar === 1 && navbar ?"flex! fixed bg-green-900 w-full left-0 top-0" : ""}` }>
-                                {/* column2 */}
-                                <div className={`${navbar ? "" : "bg-red-200 w-[300px]"}`}>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                </div>
-                                {/* column3 */}
-                                <div className={`${navbar ? "" : "bg-blue-200 w-[300px]"}`}>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                </div>
-                                {/* column4 */}
-                                <div className={`${navbar ? "" : "bg-green-200 w-[300px]"}`}>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                </div>
-                            </div>
-                        
-                        </div>
-                    </div>         
-                {/* end container mega menu */}
-                {/* ------------- */}
-                {/* container mega menu */}
-                 {/* row 2 */}
-                    <div   className={` ${navbar ? "  group border-b-1!  border-b-gray-100! hover:bg-sky-100 flex items-center justify-between px-3 cursor-pointer hover:text-gray-950!" : "group bg-amber-500 "}`}>
-                        <div className=" group-hover:text-black bg-transparent flex items-center justify-center h-[60px] border-b-white! border-b-2 hover:border-b-2 hover:border-b-gray-900! cursor-pointer max-md:border-0! max-md:text-gray-500 ">
-                            item
-                        </div>
-                        <i className="bi bi-chevron-down text-gray-300 hover:text-red-500 md:hidden group-hover:rotate-180 duration-300 group-hover:text-red-500"></i>
-
-                        {/* MegaMenu */}
-                        <div className={` ${navbar ? "fixed hidden" : " group-hover:flex! hidden  fixed top-[101px] left-0 w-[100%] h-[500px] bg-gray-100 z-50   justify-center  "   } `   } >
-                            {/* column1 */}
-                            <div className={`${navbar ? "" :"bg-green-200 w-[300px]"}`}>
-                                <div>item</div>
-                                <div>item</div>
-                                <div>item</div>
-                                <div>item</div>
-                                <div>item</div>
-                                <div>item</div>
-                                <div>item</div>
-                                <div>item</div>
-                            </div>
-                            <div className={`${navbar ? "" : "flex "}`}>
-                                {/* column2 */}
-                                <div className={`${navbar ? "" : "bg-red-200 w-[300px]"}`}>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                </div>
-                                {/* column3 */}
-                                <div className={`${navbar ? "" : "bg-blue-200 w-[300px]"}`}>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                </div>
-                                {/* column4 */}
-                                <div className={`${navbar ? "" : "bg-green-200 w-[300px]"}`}>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                    <div>item</div>
-                                </div>
-                            </div>
-                        
-                        </div>
-                    </div>      
-                {/* end container mega menu */}
-  
-            
-
-      
-       
-          
+            <div className="menuContainer">
+                <div className="menu">menu 1</div>
+                <div className="menu">menu 2</div>
+                <div className="menu">menu 3</div>
+                <div className="menu">menu 4</div>
             </div>
 
             {/* RIGHT  */}
