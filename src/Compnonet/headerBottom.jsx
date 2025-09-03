4d12d8674fda0f49a893a24a9977ff4e546fb0d2
amir <amirtaki623@gmail.com>
2025-09-03 18:52:27 +0330

    day 3  part 58

--- src/Compnonet/headerBottom.jsx
+++ src/Compnonet/headerBottom.jsx
@@ -79,7 +79,7 @@ const  HeaderBottom = () => {
                       <div className = {`${navbar ? "hidden" : "h-[30px] flex items-center text-gray-900/30 w-[300px]"}`}>Category Tilte </div>
                       
                       {/* title List */}
-                      <div className={`${navbar ? "flex justify-between px-4 bg-white text-gray-400 hover:text-gray-700 cursor-pointer hover:bg-sky-100! duration-150 h-[50px] items-center" : " h-[40px] text-xl flex flex-col  pl-[2px] text-gray-600   hover:text-blue-600 bg-white hover:bg-gray-100 duration-300 "}`}>
+                      <div className={`${navbar ? "flex justify-between px-4 bg-white text-gray-400 hover:text-gray-700 cursor-pointer hover:bg-sky-100! duration-150 h-[50px] items-center" : " h-[40px]  justify-center text-xl flex flex-col  pl-[2px] text-gray-600   hover:text-blue-600 bg-white hover:bg-gray-100 duration-300 "}`}>
                           <div className={`${navbar ? "" : ""}`}>list 1</div>
                           <div className={`${navbar ? "" : "hidden"}`}><i class="bi bi-chevron-right"></i></div>
                       </div>
