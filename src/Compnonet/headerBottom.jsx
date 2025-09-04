960088c88b2383787c1f18d0a1cf62ad9cd462b1
amir <amirtaki623@gmail.com>
2025-09-04 16:52:58 +0330

    day 4 part 27

--- src/Compnonet/headerBottom.jsx
+++ src/Compnonet/headerBottom.jsx
@@ -68,7 +68,7 @@ const  HeaderBottom = () => {
                 </div>
                 {/* megaMenu */}
                                                                                                                                         {/* hiiden */}
-                <div className = {`${navbar ? "bg-blue-500 h-0 overflow-hidden group-hover/menu:h-[400px]! duration-500! " : " fixed group-hover/menu:flex!  bg-amber-400 w-[30%] left-0 top-[101px]   m-10   h-[600px]   "}`}>
+                <div className = {`${navbar ? "bg-blue-500 h-0 overflow-hidden group-hover/menu:h-[400px]! duration-500! " : " fixed group-hover/menu:flex!  bg-amber-400 w-[20%]  left-0 top-[101px]   my-5  ml-[10%] h-[500px]   "}`}>
                   
                   {/* List */}
                   <div className={` ${navbar ? "list": "list flex  bg-transparent h-[600px] flex-col w-[100%]   "}`}>
@@ -83,9 +83,9 @@ const  HeaderBottom = () => {
                       </div>
                       
                       {/* container product ============================================================> Hidden  =>   */}
-                      <div className={`${navbar ? "bg-yellow-500 fixed h-0 overflow-hidden containerProduct" : "  fixed flex w-[70%] left-[30%] top-[101px] bg-red-500 border-l-1!  h-[600px] ml-0 m-10  border-gray-300! flex-row!    group-hover/list:flex! "}`}>
+                      <div className={`${navbar ? "bg-yellow-500 fixed h-0 overflow-hidden containerProduct" : "  fixed flex w-[60%] left-[30%] mr-[10%] top-[101px] bg-red-500 border-l-1!  h-[500px] ml-0 m-5  border-gray-300! flex-row!    group-hover/list:flex! "}`}>
                         {/* category */}
-                        <div className=" flex flex-col  w-[23.3%]  overflow-hidden border-l-gray-100 border-r-1!" >
+                        <div className=" flex flex-col  w-[33.3%]  overflow-hidden border-l-gray-100 border-r-1!" >
                             <div className = "h-[50px] flex items-center text-gray-900/20 pl-[20px]  font-bold ">BY CATEGORY</div>
                             {/* category item */}
                             <div className=" flex bg-white h-[50px]  items-center!  pl-[10px] text-gray-500 cursor-pointer hover:bg-gray-100 duration-300 hover:text-blue-500   group/category">
@@ -94,7 +94,7 @@ const  HeaderBottom = () => {
                             </div>
                         </div>
                         {/* serics */}
-                        <div className=" flex flex-col  w-[23.3%] overflow-hidden border-l-gray-100 border-r-1!">
+                        <div className=" flex flex-col  w-[33.3%] overflow-hidden border-l-gray-100 border-r-1!">
                           <div className = "h-[50px] flex items-center text-gray-900/20 pl-[20px]  font-bold ">BY SERYS</div>
                           {/* serics item */}
                           <div className=" flex bg-white h-[50px] items-center!  pl-[10px] text-gray-500 cursor-pointer hover:bg-gray-100 duration-300 hover:text-blue-500  group/category">
@@ -102,18 +102,19 @@ const  HeaderBottom = () => {
                           </div>
                         </div>
                         {/* image */}
-                        <div className="flex flex-col  w-[23.3%] overflow-hidden  justify-center items-center gap-2">
+                        <div className="flex flex-col  w-[33.3%] overflow-hidden  justify-center items-center gap-2">
                           {/* itemImages */}
-                          <div className="w-[230px] h-[230px] bg-gray-100 flex flex-col justify-center items-center">
+                          <div className="w-[200px] h-[200px] bg-gray-100 flex flex-col justify-center items-center">
+                            <img src={labtop} alt=""className="w-[150px] h-[150px]  "/>
+                            <div className=" w-[150px] h-[150px] text-center flex items-center text-gray-600 hover:text-blue-500 cursor-pointer duration-200">ASUS Zenbook Duo (2024) UX8406</div>
+                          </div>
+                          {/* itemImages */}
+                          <div className="w-[200px] h-[200px] bg-gray-100 flex flex-col justify-center items-center">
                             <img src={labtop} alt=""className="w-[150px] h-[150px]  "/>
                             <div className=" w-[150px] h-[150px] text-center flex items-center text-gray-600 hover:text-blue-500 cursor-pointer duration-200">ASUS Zenbook Duo (2024) UX8406</div>
                           </div>
                      
-                          {/* itemImages */}
-                          <div className="w-[230px]  bg-gray-100 flex flex-col justify-center items-center">
-                            <img src={labtop} alt=""className="w-[150px] h-[150px]  "/>
-                            <div className=" w-[150px] h-[150px] text-center flex items-center text-gray-600 hover:text-blue-500 cursor-pointer duration-200">ASUS Zenbook Duo (2024) UX8406</div>
-                          </div>
+                          
                      
                         </div>
                       </div>
