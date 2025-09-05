843424025162512aec267b50bb3cb4bab32896dd
amir <amirtaki623@gmail.com>
2025-09-05 21:36:07 +0330

    day 5 part 46

--- src/Compnonet/headerTop.jsx
+++ src/Compnonet/headerTop.jsx
@@ -21,7 +21,7 @@ const HeaderTop = () => {
     
     return(
    
-        <div className="bg-gray-100 flex items-center justify-end h-[40px] border-b-[2px] border-amber-300 fixed! w-full! top-0 left-0"  >
+        <div className={`bg-gray-100 flex items-center justify-end h-[40px] border-b-[2px] border-amber-300 fixed! w-full! top-0 left-0 ${scrollTop > 50 ? "hidden" : ""}`}  >
                  {scrollTop}
             <div className="flex gap-4 p-5 justify-between items-center">
               <a className='cursor-pointer text-gray-500 hover:text-gray-950! hover:underline  '>help</a>
