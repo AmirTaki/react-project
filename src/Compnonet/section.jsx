e6c14b70f30faf70030a3ba4ac4b3156a255cc12
amir <amirtaki623@gmail.com>
2025-09-05 21:15:28 +0330

    day 5 part 38

--- src/Compnonet/section.jsx
+++ src/Compnonet/section.jsx
@@ -1,7 +1,9 @@
 const Section = () => {
     return(
         <>
-            section
+            <div className="bg-blue-500 mt-15">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit delectus ea eius obcaecati nihil, similique aliquam, molestiae quaerat quo nemo soluta commodi exercitationem repellat deserunt eaque est! Fugiat, voluptas iusto.
+            </div>
         </>
     )
 }
