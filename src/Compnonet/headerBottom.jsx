8b2864ecf74ff6867440233d30d70ecc9ebd2f6b
amir <amirtaki623@gmail.com>
2025-09-05 16:56:17 +0330

    day 5 part 22

--- src/Compnonet/headerBottom.jsx
+++ src/Compnonet/headerBottom.jsx
@@ -122,9 +122,12 @@ const  HeaderBottom = () => {
                           {/* image */}
                           <div className={`${navbar ? "flex flex-col justify-center h-[100%] bg-red-500 w-[50%] items-center gap-10 " : "flex flex-col w-[300px]    overflow-hidden  justify-center items-center gap-1"}`}>
                             {/* itemImages */} 
-                            <div className="w-[200px] h-[230px] bg-gray-100 flex flex-col justify-center items-center">
+                            <div className={`${navbar ? "bg-gray-100 flex flex-col justify-center items-center h-[300px] w-[250px]" :
+                              "w-[200px] h-[230px] bg-gray-100 flex flex-col justify-center items-center"
+                            }`}>
                               <img src={labtop} alt=""className="w-[150px] h-[150px]  "/>
                               <div className=" w-[150px] h-[150px] text-center flex items-center text-gray-600 hover:text-blue-500 cursor-pointer duration-200">ASUS Zenbook Duo (2024) UX8406</div>
+                            
                             </div>
                             {/* itemImages */}
                             <div className="w-[200px] h-[230px] bg-gray-100 flex flex-col justify-center items-center">
