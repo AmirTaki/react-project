9ccffb716870b48eff67b41e293f25bb5cd12724
amir <amirtaki623@gmail.com>
2025-09-05 16:10:01 +0330

    day 5 part 18

--- src/Compnonet/headerBottom.jsx
+++ src/Compnonet/headerBottom.jsx
@@ -103,7 +103,7 @@ const  HeaderBottom = () => {
                         <div className={`${navbar ? "bg-yellow-500 top-0 w-full h-full   overflow-hidden containerProduct fixed! -left-300 " : "  z-10!  fixed!  w-[900px] top-0 -left-[0px]! bg-white shadow-2xl rounded-2xl border-l-1! hidden  h-[500px] ml-0 p-5  border-gray-300!  flex-row!  "}  ${productMenuNavbar ? "left-0! duration-300!" : ""} ${ productMenu  ? "flex! animate-productMenu!":"" } ${animationProductMenu & navbar === false? "animate-productMenuReverse!" : ""}` }>
                           <i onClick = {(e)=>{handlerProductMenu()}} className="text-red-200 hover:text-red-500 duration-300 cursor-pointer font-bold right-3 text-2xl absolute bi bi-x-circle"></i>
                           {/* category */}
-                          <div className={`${navbar ? " flex flex-col w-[50%] bg-red-600" :" flex flex-col  w-[300px]  overflow-hidden border-l-gray-100 border-r-1!"}`} >
+                          <div className={`${navbar ? " flex flex-col w-[50%] h-[50%] bg-red-600" :" flex flex-col  w-[300px]  overflow-hidden border-l-gray-100 border-r-1!"}`} >
                               <div className = "h-[50px] flex items-center text-gray-900/20 pl-[20px]  font-bold ">BY CATEGORY</div>
                               {/* category item */}
                               <div className=" flex bg-white h-[50px]  items-center!  pl-[10px] text-gray-500 cursor-pointer hover:bg-gray-100 duration-300 hover:text-blue-500   group/category">
