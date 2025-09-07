d3f7a9e2868bd34d1a0fba2e3daa5fdd5a250b3d
amir <amirtaki623@gmail.com>
2025-09-07 17:17:37 +0330

    day 6 part 22

--- src/Compnonet/section.jsx
+++ src/Compnonet/section.jsx
@@ -1,16 +1,15 @@
 import { useContext } from "react";
 
-
-
+import { GlobalHeaderScroll } from "../App";
 
 const Section = () => {
-    // const {scrollTop} = useContext(GlobalHeaderScroll)
+    const {scrollTop} = useContext(GlobalHeaderScroll)
     return(
-        <>
-            <div className = {`${scrollTop ? "" : ""} bg-blue-500 mt-15"`}>
+        <div >
+            <div>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit delectus ea eius obcaecati nihil, similique aliquam, molestiae quaerat quo nemo soluta commodi exercitationem repellat deserunt eaque est! Fugiat, voluptas iusto.
             </div>
-        </>
+        </div>
     )
 }
 
