fa38162fd736db845382bef9e2757529b73f4f33
amir <amirtaki623@gmail.com>
2025-09-07 17:31:40 +0330

    day 6 part 25

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,13 @@
 import './App.css'
 import Header from './Compnonet/header'
 import Section from './Compnonet/section'
-import { createContext, useEffect, useState } from 'react';
-
-
-export const GlobalHeaderScroll = createContext();
-
 
 function App() {
-  const [scrollTop, setScrollTop]  =  useState(0);
-    const handleScroll = () => {
-        setScrollTop(window.scrollY);
-    }
-   
-    useEffect(() => {
-       
-        window.addEventListener('scroll', handleScroll)
- 
-        return() => {
-            window.addEventListener("scroll", handleScroll);
-            
-        }
-    },[])
-
-
   return (
-    <GlobalHeaderScroll.Provider value = {{scrollTop, setScrollTop}} >
-      <div className='h-[2000px] bg-red-500'>
-        <Header />
-        <Section />
-      </div>
-    </ GlobalHeaderScroll.Provider>
-
+    <div className='h-[2000px] bg-red-500'>
+      <Header />
+      <Section />
+    </div>
   )
 }
 
--- src/Compnonet/header.jsx
+++ src/Compnonet/header.jsx
@@ -1,12 +1,30 @@
 import HeaderTop from './headerTop';
 import HeaderBottom from './headerBottom';
+import { createContext, useEffect, useState } from 'react';
+export const GlobalHeaderScroll = createContext();
 
 const Header = () => {
+    const [scrollTop, setScrollTop]  =  useState(0);
+    const handleScroll = () => {
+        setScrollTop(window.scrollY);
+    }
+   
+    useEffect(() => {
+       
+        window.addEventListener('scroll', handleScroll)
+ 
+        return() => {
+            window.addEventListener("scroll", handleScroll);
+            
+        }
+    },[])
     return(
-        <header>
-            <HeaderTop />
-            <HeaderBottom />
-        </header>
+        <GlobalHeaderScroll.Provider value = {{scrollTop, setScrollTop}} >
+            <header>
+                <HeaderTop />
+                <HeaderBottom />
+            </header>
+        </GlobalHeaderScroll.Provider>
     )
 }
 export default Header;
