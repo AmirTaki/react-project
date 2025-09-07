6e6163afc56ca8625e15beb186b7004019ecca72
amir <amirtaki623@gmail.com>
2025-09-07 16:59:13 +0330

    day 6 part 7

--- src/Compnonet/headerTop.jsx
+++ src/Compnonet/headerTop.jsx
@@ -6,12 +6,12 @@ import { GlobalHeaderScroll } from "./header";
 const HeaderTop = () => {
 
     
-     const {scrollTop}  = useContext(GlobalHeaderScroll)
+    const {scrollTop}  = useContext(GlobalHeaderScroll)
     return(
         
         <div className={`bg-gray-100 flex items-center justify-end h-[40px] border-b-[2px] border-amber-300 fixed! w-full! top-0 left-0 
         ${scrollTop > 50 ? "hidden" : ""}   `}  >
-           
+           {scrollTop}
             <div className="flex gap-4 p-5 justify-between items-center">
               <a className='cursor-pointer text-gray-500 hover:text-gray-950! hover:underline  '>help</a>
               <a className='cursor-pointer text-gray-500 hover:text-gray-950! hover:underline'>orders and returns</a>
