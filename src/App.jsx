793849c5ee208e8cccae5400dab50043c894c557
amir <amirtaki623@gmail.com>
2025-09-07 17:12:36 +0330

    day 6 part 18

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-
-
 import './App.css'
 import Header from './Compnonet/header'
 import Section from './Compnonet/section'
-
+import { createContext, useEffect, useState } from 'react';
+export const GlobalHeaderScroll = createContext();
 
 
 function App() {
+  const [scrollTop, setScrollTop]  =  useState(0);
+    const handleScroll = () => {
+        setScrollTop(window.scrollY);
+    }
+   
+    useEffect(() => {
+       
+        window.addEventListener('scroll', handleScroll)
+ 
+        return() => {
+            window.addEventListener("scroll", handleScroll);
+            
+        }
+    },[])
 
 
   return (
+    
     <div className='h-[2000px] bg-red-500'>
       <Header />
       <Section />
--- src/Compnonet/header.jsx
+++ src/Compnonet/header.jsx
@@ -1,25 +1,8 @@
-
 import HeaderTop from './headerTop';
 import HeaderBottom from './headerBottom';
-import { createContext, useEffect, useState } from 'react';
-
-export const GlobalHeaderScroll = createContext();
 
 const Header = () => {
-    const [scrollTop, setScrollTop]  =  useState(0);
-    const handleScroll = () => {
-        setScrollTop(window.scrollY);
-    }
-   
-    useEffect(() => {
-       
-        window.addEventListener('scroll', handleScroll)
- 
-        return() => {
-            window.addEventListener("scroll", handleScroll);
-            
-        }
-    },[])
+    
 
     return(
         <GlobalHeaderScroll.Provider value = {{scrollTop, setScrollTop}} >
