b74a10af2310d0412c8ad9c1681a3f1502566e9c
amir <amirtaki623@gmail.com>
2025-09-07 17:11:38 +0330

    day 6 part 17

--- src/Compnonet/section.jsx
+++ src/Compnonet/section.jsx
@@ -7,7 +7,7 @@ const Section = () => {
     const {scrollTop} = useContext(GlobalHeaderScroll)
     return(
         <>
-            <div className = {`${} bg-blue-500 mt-15"`}>
+            <div className = {`${scrollTop ? "" : ""} bg-blue-500 mt-15"`}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit delectus ea eius obcaecati nihil, similique aliquam, molestiae quaerat quo nemo soluta commodi exercitationem repellat deserunt eaque est! Fugiat, voluptas iusto.
             </div>
         </>
