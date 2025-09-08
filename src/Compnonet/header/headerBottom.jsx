52a19c88ffbf3148e1a82a2c43ba16244d1eb787
amir <amirtaki623@gmail.com>
2025-09-08 22:44:25 +0330

    day 7 part 26

--- src/Compnonet/header/headerBottom.jsx
+++ src/Compnonet/header/headerBottom.jsx
@@ -100,7 +100,6 @@ const  HeaderBottom = () => {
       setProductMenu((product) => {
         const newProduct = {};
         for (const item in product){
-          console.log(product[item])
           product[item] = false
         }
         return newProduct;
@@ -131,10 +130,10 @@ const  HeaderBottom = () => {
     const listHandler = (id)  => {
       window.innerWidth <= 768 ?  setProductMenuNavbar(!productMenuNavbar) : setProductMenu((product) => ({...product, [id] : !product[id] }))
     }
-    const handlerProductMenu = (e) => {
+    const handlerProductMenu = (index) => {
       setAnimationProductMenu(true)
       setTimeout(() => {
-        setProductMenu(false)
+        setProductMenu((product) => ({...product, [index] : !product[index]}))
         setAnimationProductMenu(false)
       }, 500);
     }
@@ -201,7 +200,7 @@ const  HeaderBottom = () => {
 
                   {/* container product ============================================================> Hidden  =>   */}
                   <div className={`${navbar ? "bg-white shadow-2xl top-0 w-full h-full flex-wrap! flex-col flex!   overflow-hidden containerProduct fixed! -left-300 " : "  z-10!  fixed!  w-[900px] top-0 -left-[0px]! bg-white shadow-2xl rounded-2xl border-l-1! hidden  h-[500px] ml-0 p-5  border-gray-300!  flex-row!  "}  ${productMenuNavbar ? "left-0! duration-500!" : "-left-300! duration-700!"} ${ productMenu[0] ? "flex! animate-productMenu!":"" } ${state.animationProductMenu & navbar === false? "animate-productMenuReverse!" : ""}` }>
-                    <i onClick = {(e)=>{handlerProductMenu()}} className={`${navbar ? "hidden": "text-red-200 hover:text-red-500 duration-300 cursor-pointer font-bold right-3 text-2xl absolute bi bi-x-circle"}`}></i>
+                    <i onClick = {()=>{handlerProductMenu(0)}} className={`${navbar ? "hidden": "text-red-200 hover:text-red-500 duration-300 cursor-pointer font-bold right-3 text-2xl absolute bi bi-x-circle"}`}></i>
                   
                     <div className={`${navbar ? "bg-transparent h-[6%] w-full border-b-1! border-b-gray-100! fixed! flex justify-between items-center px-5": "hidden"}`}>
                       <div onClick={listHandler} className="text-gray-100 hover:scale-125 cursor-pointer duration-200 hover:text-gray-400"><i className="bi bi-chevron-left pr-2"></i>LEFT</div>
@@ -257,7 +256,7 @@ const  HeaderBottom = () => {
 
                   {/* container product ============================================================> Hidden  =>   */}
                   <div className={`${navbar ? "bg-white shadow-2xl top-0 w-full h-full flex-wrap! flex-col flex!   overflow-hidden containerProduct fixed! -left-300 " : "  z-10!  fixed!  w-[900px] top-0 -left-[0px]! bg-yellow-500 shadow-2xl rounded-2xl border-l-1! hidden  h-[500px] ml-0 p-5  border-gray-300!  flex-row!  "}  ${productMenuNavbar ? "left-0! duration-500!" : "-left-300! duration-700!"} ${ productMenu[1] ? "flex! animate-productMenu!":"" } ${state.animationProductMenu & navbar === false? "animate-productMenuReverse!" : ""}` }>
-                    <i onClick = {(e)=>{handlerProductMenu()}} className={`${navbar ? "hidden": "text-red-200 hover:text-red-500 duration-300 cursor-pointer font-bold right-3 text-2xl absolute bi bi-x-circle"}`}></i>
+                    <i onClick = {(e)=>{handlerProductMenu(1)}} className={`${navbar ? "hidden": "text-red-200 hover:text-red-500 duration-300 cursor-pointer font-bold right-3 text-2xl absolute bi bi-x-circle"}`}></i>
                   
                     <div className={`${navbar ? "bg-transparent h-[6%] w-full border-b-1! border-b-gray-100! fixed! flex justify-between items-center px-5": "hidden"}`}>
                       <div onClick={listHandler} className="text-gray-100 hover:scale-125 cursor-pointer duration-200 hover:text-gray-400"><i className="bi bi-chevron-left pr-2"></i>LEFT</div>
