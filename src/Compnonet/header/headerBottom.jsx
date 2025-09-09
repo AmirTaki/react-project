3f7ddd54e37dae44164da3aaa50ef27290605f12
amir <amirtaki623@gmail.com>
2025-09-10 03:15:14 +0330

    day 8 part 10

--- src/Compnonet/header/headerBottom.jsx
+++ src/Compnonet/header/headerBottom.jsx
@@ -5,14 +5,7 @@ import { GlobalHeaderScroll } from "./header";
 
 
 
-const initialStateHeader = {
-  search : false,
-  moveNavbar : false,
-  productMenu : {},
-  productMenuNavbar : {},
-  animationProductMenu : {},
-  heightNavbar : {}
-}
+
 
 const ACTIONTYPE = {
   search : "search",
@@ -52,85 +45,7 @@ const  HeaderBottom = () => {
        
       }
 
-    const reducer = (state, action) => {
-      // console.log(state)
-      switch(action.type){
-        // search
-        case ACTIONTYPE.search :
-          return {...state, search : !state.search };
-
-        case ACTIONTYPE.searchResize : 
-          return {...state, search : false}
-        
-        // MoveNaveBar 
-        case ACTIONTYPE.moveNavbar : 
-          return {...state, moveNavbar : !state.moveNavbar};
-
-        case ACTIONTYPE.moveNavbarResize : 
-          return {...state, moveNavbar : false};
-
-        // productMenu
-        case ACTIONTYPE.productMenu : 
-          const {id} = action.payload;
-          return {...state,
-            productMenu: { ...state.productMenu, [id] : !state.productMenu[id]  }
-          }
-
-        case ACTIONTYPE.productMenuResize :
-          const newProductMenu = Object.keys(state.productMenu).reduce((acc, key) => {
-            acc[key] = false;
-            return acc;
-          }, {});
-          
-          return {...state, 
-            productMenu : newProductMenu
-          }
-
-        // productMenuNavbar 
-        case ACTIONTYPE.productMenuNavbar :
-          
-          const {index} = action.payload ;
-          return {...state, productMenuNavbar : {
-            ...state.productMenuNavbar, [index] : !state.productMenuNavbar[index]
-          }}
-
-        case ACTIONTYPE.productMenuNavbarResize : 
-          const newProductMenuNavber = Object.keys(state.productMenuNavbar).reduce((a, b) => {
-            a[b] = false;
-            return a
-          }, {})
-          return {...state,
-            productMenuNavbar : newProductMenuNavber
-          }
-
-        //animationProductMenu
-        case ACTIONTYPE.animationProductMenu : 
-          const {page}  = action.payload;
-          const {animation} = action.payload
-          return {
-            ...state,
-            animationProductMenu : {
-              ...state.animationProductMenu,
-              [page] : animation
-            }
-          }
-
-        // height 
-        case ACTIONTYPE.heightNavbar : 
-          const {height} = action.payload;
-          const {place} = action.payload;
-          return {...state,
-            heightNavbar : {
-              ...state.heightNavbar,
-              [place] : height 
-            }};
-        
-        // default 
-        default :
-          return {...state, }
-      }
     
-    }
     const [state, dispatch]  =   useReducer (reducer,initialStateHeader);
  
 
--- src/Compnonet/header/reducerHeader.jsx
+++ src/Compnonet/header/reducerHeader.jsx
@@ -1,8 +1,89 @@
-const ReducerHeader = () => {
-    return(
-        <>
-        </>
-    )
+export const initialStateHeader = {
+  search : false,
+  moveNavbar : false,
+  productMenu : {},
+  productMenuNavbar : {},
+  animationProductMenu : {},
+  heightNavbar : {}
 }
 
-export default ReducerHeader;
+
+export const reducerHeader = (state, action) => {
+ 
+    switch(action.type){
+    // search
+    case ACTIONTYPE.search :
+        return {...state, search : !state.search };
+
+    case ACTIONTYPE.searchResize : 
+        return {...state, search : false}
+    
+    // MoveNaveBar 
+    case ACTIONTYPE.moveNavbar : 
+        return {...state, moveNavbar : !state.moveNavbar};
+
+    case ACTIONTYPE.moveNavbarResize : 
+        return {...state, moveNavbar : false};
+
+    // productMenu
+    case ACTIONTYPE.productMenu : 
+        const {id} = action.payload;
+        return {...state,
+        productMenu: { ...state.productMenu, [id] : !state.productMenu[id]  }
+        }
+
+    case ACTIONTYPE.productMenuResize :
+        const newProductMenu = Object.keys(state.productMenu).reduce((acc, key) => {
+        acc[key] = false;
+        return acc;
+        }, {});
+        
+        return {...state, 
+        productMenu : newProductMenu
+        }
+
+    // productMenuNavbar 
+    case ACTIONTYPE.productMenuNavbar :
+        
+        const {index} = action.payload ;
+        return {...state, productMenuNavbar : {
+        ...state.productMenuNavbar, [index] : !state.productMenuNavbar[index]
+        }}
+
+    case ACTIONTYPE.productMenuNavbarResize : 
+        const newProductMenuNavber = Object.keys(state.productMenuNavbar).reduce((a, b) => {
+        a[b] = false;
+        return a
+        }, {})
+        return {...state,
+        productMenuNavbar : newProductMenuNavber
+        }
+
+    //animationProductMenu
+    case ACTIONTYPE.animationProductMenu : 
+        const {page}  = action.payload;
+        const {animation} = action.payload
+        return {
+        ...state,
+        animationProductMenu : {
+            ...state.animationProductMenu,
+            [page] : animation
+        }
+        }
+
+    // height 
+    case ACTIONTYPE.heightNavbar : 
+        const {height} = action.payload;
+        const {place} = action.payload;
+        return {...state,
+        heightNavbar : {
+            ...state.heightNavbar,
+            [place] : height 
+        }};
+    
+    // default 
+    default :
+        return {...state, }
+    }
+
+}
