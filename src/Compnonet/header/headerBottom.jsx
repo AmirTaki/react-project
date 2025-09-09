6560df4ddc45ca8ed7236fb12c5bc0cc067642a3
amir <amirtaki623@gmail.com>
2025-09-09 15:59:04 +0330

    day 8 part 3

--- src/Compnonet/header/headerBottom.jsx
+++ src/Compnonet/header/headerBottom.jsx
@@ -252,7 +252,7 @@ const  HeaderBottom = () => {
                   <div className={`${navbar ?  "text-gray-400 group-hover/menu:rotate-180 group-hover/menu:text-red-500! duration-300!" : "hidden"}`}><i className="bi bi-chevron-down"></i></div>
                 </div>
                 {/* megaMenu   => group-hover/menu:h-[200px]   */}
-                <div   style = {{height :  `${hight}`}}  className={` megaContainer  ${navbar ? "bg-gray-100 h-[0] overflow-hidden  duration-500!" : "bg-white h-[0] hidden   rounded-2xl shadow-2xl absolute top-[62px] -right-110 group-hover/menu:h-[500px]! group-hover/menu:flex!  duration-500!    animate-mege!  "}`}>
+                <div     className={` megaContainer  ${navbar ? "bg-gray-100 h-[0] overflow-hidden  duration-500!" : "bg-white h-[0] hidden   rounded-2xl shadow-2xl absolute top-[62px] -right-110 group-hover/menu:h-[500px]! group-hover/menu:flex!  duration-500!    animate-mege!  "}`}>
                   {/* container list */}
                   <div className={` ${navbar ? "list": "list flex  items-center bg-transparent  h-[500px] flex-col w-[900px]   "}`}>
                     {/* category */}
