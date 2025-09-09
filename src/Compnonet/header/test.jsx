051c81e942d529a4426b5fea99de1d8ab53294fe
amir <amirtaki623@gmail.com>
2025-09-09 21:15:10 +0330

    day 8 part 32

--- src/Compnonet/header/test.jsx
+++ src/Compnonet/header/test.jsx
@@ -4,18 +4,39 @@ import { act, useReducer, useState } from "react"
 const Test = () => {
 
 
-    const h = "500px"
+    const [height, setHeigt] =   useState(0)
+    const enter = (index) => {
+        const mega = document.querySelectorAll(".mega")[index]
+        const alpha = mega.querySelectorAll('.alpha').length;
+        console.log(alpha)
+        setHeigt((h) => ({}))
+        setHeigt(`${alpha*30}px`)
+    }
+    const leave = () => {
+        setHeigt("0px")
+
+    }
     return(
         <div>
-            <div className="bg-blue-500 w-[300px] group/one ">
+            <div onMouseEnter={()=> {enter(0)}} onMouseLeave={()=> {leave(0)}} className="bg-blue-500 w-[300px] group/one ">
                 <div>menu</div>
-                <div className="h-0 overflow-hidden group-hover/one:h-[100px]! duration-300">
-                    <div className="">item</div>
-                    <div className="">item</div>
-                    <div className="">item</div>
-                    <div className="">item</div>
+                <div style={{height : height}} className="h-0 overflow-hidden  duration-300 mega">
+                    <div className="alpha">item</div>
+                    <div className="alpha">item</div>
+                    <div className="alpha">item</div>
+                    <div className="alpha">item</div>
                 </div>
             </div>
+            <div onMouseEnter={()=> {enter(1)}} onMouseLeave={()=> {leave(1)}} className="bg-blue-500 w-[300px] group/one ">
+                <div>menu</div>
+                <div style={{height : height}} className="h-0 overflow-hidden  duration-300 mega">
+                    <div className="alpha">item</div>
+                    <div className="alpha">item</div>
+                    <div className="alpha">item</div>
+                    <div className="alpha">item</div>
+                </div>
+            </div>
+          
         </div>
       
     )
