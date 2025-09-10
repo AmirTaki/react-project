3ebfbe1f0e784ee292ce9b34a90e7ef5f1bd26d7
amir <amirtaki623@gmail.com>
2025-09-10 21:51:37 +0330

    day 8 part 35

--- src/Compnonet/header/menuMegaMenu.jsx
+++ src/Compnonet/header/menuMegaMenu.jsx
@@ -25,7 +25,7 @@ const MenuMegaMenu = ({menu, loc}) => {
                 style={{height: state.heightNavbar[loc]}} 
                 className={` megaContainer  h-[0]    
                     ${navbar ? "bg-gray-100  overflow-hidden  duration-500!" : "bg-red-400 h-[0] hidden!   rounded-2xl shadow-2xl left-[10%] top-[101px] fixed group-hover/menu:flex!  duration-500! group-hover/menu:h-[500px]!  overflow-hidden  animate-mege!  "}
-                    ${scrollTop > 90 ? "":""}
+                    ${scrollTop > 90 ? "top-[90px]!":"top-[101px]!"}
                     `}  
                 >
                 {/* container list */}
