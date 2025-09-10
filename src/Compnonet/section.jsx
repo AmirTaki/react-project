4e0a9f48605a716bd9c74f5ed0d61f33996c6b03
amir <amirtaki623@gmail.com>
2025-09-10 23:30:46 +0330

    day 8 part 65

--- src/Compnonet/section.jsx
+++ src/Compnonet/section.jsx
@@ -25,30 +25,34 @@ const Section = () => {
     }) 
     return(
         <div className={`bg-red-500 mt-[101px]  h-[600px]`}>
-         
-            <div className="bg-blue-400 w-[100%] h-[600px]   sliderImage overflow-x-scroll flex  flex-col flex-wrap overflow-y-hidden relative!  ">
-                <div className="bg-green-200 w-[100%]! h-[600px]!  ">
-                    <img src={img1} className="w-[100%]! h-[100%]!" alt="" />
-                </div>
-                <div className="bg-green-200 w-[100%]! h-[600px]!  ">
-                    <img src={img2} className="w-[100%]! h-[100%]!" alt="" />
-                </div>
-                <div className="bg-green-200 w-[100%]! h-[600px]!  ">
-                    <img src={img3} className="w-[100%]! h-[100%]!" alt="" />
-                </div>
-                <div className="bg-green-200 w-[100%]! h-[600px]!  ">
-                    <img src={img4} className="w-[100%]! h-[100%]!" alt="" />
-                </div>
+            
+            <div className="">
+                <div className="bg-blue-400 w-[100%] h-[600px]   sliderImage overflow-x-scroll flex  flex-col flex-wrap overflow-y-hidden relative!  ">
+                    <div className="bg-green-200 w-[100%]! h-[600px]!  ">
+                        <img src={img1} className="w-[100%]! h-[100%]!" alt="" />
+                    </div>
+                    <div className="bg-green-200 w-[100%]! h-[600px]!  ">
+                        <img src={img2} className="w-[100%]! h-[100%]!" alt="" />
+                    </div>
+                    <div className="bg-green-200 w-[100%]! h-[600px]!  ">
+                        <img src={img3} className="w-[100%]! h-[100%]!" alt="" />
+                    </div>
+                    <div className="bg-green-200 w-[100%]! h-[600px]!  ">
+                        <img src={img4} className="w-[100%]! h-[100%]!" alt="" />
+                    </div>
                
 
                {/*  */}
-               <div className="bg-white absolute! top-[50%] right-[4%]">
+               
+                </div>
+                <div className="bg-white absolute! top-[50%] right-[4%]">
                     <i class="bi bi-arrow-right-circle"></i>
-               </div>
-               <div className="bg-white absolute! top-[50%] left-[4%]">
+                </div>
+                <div className="bg-white absolute! top-[50%] left-[4%]">
                     <i class="bi bi-arrow-left-circle"></i>
-               </div>
+                </div>
             </div>
+          
 
 
           
