a877e8d5cd0fd7f9843e8503a896221c0e6cf082
amir <amirtaki623@gmail.com>
2025-09-10 23:18:23 +0330

    day 8 part 58

--- src/Compnonet/section.jsx
+++ src/Compnonet/section.jsx
@@ -5,10 +5,15 @@ import img4 from "../assets/img4.jpg"
 import img5 from "../assets/img5.jpg"
 
 const Section = () => {
+    const handler = () => {
+        const SliderImage =  document.querySelector(".sliderImage")
+        SliderImage.style.scrollBehvior = "smooth"
+    }
+
     return(
         <div className={`bg-red-500 mt-[101px] flex items-center justify-center h-[600px]`}>
          
-            <div className="bg-blue-400 w-[500px] h-[300px]    overflow-x-scroll flex  flex-col flex-wrap overflow-y-hidden   ">
+            <div className="bg-blue-400 w-[500px] h-[300px]   sliderImage overflow-x-scroll flex  flex-col flex-wrap overflow-y-hidden   ">
                 <div className="bg-green-200 w-[500px]! h-[300px]!  ">
                     <img src={img1} className="w-[100%]! h-[100%]!" alt="" />
                 </div>
