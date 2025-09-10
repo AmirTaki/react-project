147f3bd22a7bf16bd63556953973aa7863b3a864
amir <amirtaki623@gmail.com>
2025-09-10 22:10:59 +0330

    day 8 part 42

--- src/Compnonet/section.jsx
+++ src/Compnonet/section.jsx
@@ -1,10 +1,26 @@
 import img1 from "../assets/img1.webp"
+import img2 from "../assets/img2.jpg"
+import img3 from "../assets/img3.jpg"
+import img4 from "../assets/img4.jpg"
+import img5 from "../assets/img5.jpg"
 
 const Section = () => {
     return(
         <div className={`bg-red-100 mt-[101px] `}>
             {/* image slider  */}
             <div className="">
+                <div className="">
+                    <img src={img1} alt="" />
+                </div>
+                <div className="">
+                    <img src={img1} alt="" />
+                </div>
+                <div className="">
+                    <img src={img1} alt="" />
+                </div>
+                <div className="">
+                    <img src={img1} alt="" />
+                </div>
                 <div className="">
                     <img src={img1} alt="" />
                 </div>
