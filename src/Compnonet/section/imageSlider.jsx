bd36170066ffa69fdd2bc299748eff8ea1dc9db2
amir <amirtaki623@gmail.com>
2025-09-13 21:26:35 +0330

    day 10 part 4

--- src/Compnonet/section/imageSlider.jsx
+++ src/Compnonet/section/imageSlider.jsx
@@ -1,7 +1,7 @@
 const ImageSlider = () => {
     return (
-       <div className="w-[100%] h-[500px] bg-blue-500">
-
+       <div className="w-[100%] h-[600px] bg-blue-500 flex items-center justify-center">
+            <div className="w-[90%] h-[500px] bg-red-500"></div>
        </div>
     )
 }
