cb115d2094c226e8b6d2c10b31ae8548fd7cef59
amir <amirtaki623@gmail.com>
2025-09-13 21:52:40 +0330

    day 10 part 18

--- src/Compnonet/section/imageSlider.jsx
+++ src/Compnonet/section/imageSlider.jsx
@@ -5,21 +5,22 @@ const ImageSlider = () => {
                 w-[90%] h-[500px] bg-red-500 lg:bg-yellow-500 sm:bg-green-500
                 lg:flex! lg:flex-wrap lg:flex-col lg:justify-center! lg:items-center lg:overflow-x-scroll! 
                 sm:grid sm:grid-cols-2 sm:justify-items-center sm:items-center sm:overflow-hidden
+                grid grid-cols-1 justify-items-center items-center overflow-hidden
                 "
             
             >
-                <div className="w-[250px] lg:h-[400px]  sm:h-[240px] bg-sky-200 lg:mx-3 my-1"></div>
-                <div className="w-[250px] lg:h-[400px]  sm:h-[240px] bg-sky-300 lg:mx-3 my-1"></div>
-                <div className="w-[250px] lg:h-[400px]  sm:h-[240px] bg-sky-400 lg:mx-3 my-1"></div>
-                <div className="w-[250px] lg:h-[400px]  sm:h-[240px] bg-sky-500 lg:mx-3 my-1"></div>
-                <div className="w-[250px] lg:h-[400px]  sm:h-[240px] bg-sky-600 lg:mx-3 my-1"></div>
-                <div className="w-[250px] lg:h-[400px]  sm:h-[240px] bg-sky-700 lg:mx-3 my-1"></div>
-                <div className="w-[250px] lg:h-[400px]  sm:h-[240px] bg-red-200 lg:mx-3 my-1"></div>
-                <div className="w-[250px] lg:h-[400px]  sm:h-[240px] bg-red-300 lg:mx-3 my-1"></div>
-                <div className="w-[250px] lg:h-[400px]  sm:h-[240px] bg-red-400 lg:mx-3 my-1"></div>
-                <div className="w-[250px] lg:h-[400px]  sm:h-[240px] bg-red-500 lg:mx-3 my-1"></div>
-                <div className="w-[250px] lg:h-[400px]  sm:h-[240px] bg-red-600 lg:mx-3 my-1"></div>
-                <div className="w-[250px] lg:h-[400px]  sm:h-[240px] bg-red-700 lg:mx-3 my-1"></div>
+                <div className="w-[250px] lg:h-[400px]  h-[240px] bg-sky-200 lg:mx-3 my-1"></div>
+                <div className="w-[250px] lg:h-[400px]  h-[240px] bg-sky-300 lg:mx-3 my-1"></div>
+                <div className="w-[250px] lg:h-[400px]  h-[240px] bg-sky-400 lg:mx-3 my-1"></div>
+                <div className="w-[250px] lg:h-[400px]  h-[240px] bg-sky-500 lg:mx-3 my-1"></div>
+                <div className="w-[250px] lg:h-[400px]  h-[240px] bg-sky-600 lg:mx-3 my-1"></div>
+                <div className="w-[250px] lg:h-[400px]  h-[240px] bg-sky-700 lg:mx-3 my-1"></div>
+                <div className="w-[250px] lg:h-[400px]  h-[240px] bg-red-200 lg:mx-3 my-1"></div>
+                <div className="w-[250px] lg:h-[400px]  h-[240px] bg-red-300 lg:mx-3 my-1"></div>
+                <div className="w-[250px] lg:h-[400px]  h-[240px] bg-red-400 lg:mx-3 my-1"></div>
+                <div className="w-[250px] lg:h-[400px]  h-[240px] bg-red-500 lg:mx-3 my-1"></div>
+                <div className="w-[250px] lg:h-[400px]  h-[240px] bg-red-600 lg:mx-3 my-1"></div>
+                <div className="w-[250px] lg:h-[400px]  h-[240px] bg-red-700 lg:mx-3 my-1"></div>
 
             </div>
        </div>
