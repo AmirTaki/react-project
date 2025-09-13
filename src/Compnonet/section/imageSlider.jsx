df3887bb89696524023e402fc86afc8f36329377
amir <amirtaki623@gmail.com>
2025-09-13 22:49:06 +0330

    day 10 part 41

--- src/Compnonet/section/imageSlider.jsx
+++ src/Compnonet/section/imageSlider.jsx
@@ -7,8 +7,9 @@ const ImageSlider = () => {
     }
     return (
        <div className="w-[100%] h-[600px] bg-blue-500 flex items-center justify-center ">
+        {/*  max-sm:bg-green-500! max-md:bg-amber-300! bg-red-500 */}
             <div className="
-                w-[90%] h-[520px]  max-sm:bg-green-500! max-md:bg-amber-300! bg-red-500
+                w-[90%] h-[520px] bg-white
                 flex! flex-wrap flex-col justify-center! items-center overflow-x-scroll 
                 max-md:grid! max-md:grid-cols-2! max-md:justify-items-center max-md:items-center max-md:overflow-hidden! 
                 max-sm:grid! max-sm:grid-cols-1! max-sm:justify-items-center max-sm:items-center max-sm:overflow-hidden! 
