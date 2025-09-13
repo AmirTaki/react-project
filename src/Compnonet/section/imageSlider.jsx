394d9cf12935fbef7d254cea6da645e9d4b4b6b9
amir <amirtaki623@gmail.com>
2025-09-13 21:36:42 +0330

    day 10 part 5

--- src/Compnonet/section/imageSlider.jsx
+++ src/Compnonet/section/imageSlider.jsx
@@ -3,7 +3,7 @@ const ImageSlider = () => {
        <div className="w-[100%] h-[600px] bg-blue-500 flex items-center justify-center">
             <div className="
                 w-[90%] h-[500px] bg-red-500 lg:bg-yellow-500 sm:bg-green-500
-                lg:flex! lg:justify-center! lg:items-center
+                lg:flex! lg:flex-wrap lg:flex-col lg:justify-center! lg:items-center lg:overflow-x-scroll! lg:gap-3
                 "
             
             >
@@ -13,6 +13,12 @@ const ImageSlider = () => {
                 <div className="w-[200px] h-[400px] bg-sky-500"></div>
                 <div className="w-[200px] h-[400px] bg-sky-600"></div>
                 <div className="w-[200px] h-[400px] bg-sky-700"></div>
+                <div className="w-[200px] h-[400px] bg-red-200"></div>
+                <div className="w-[200px] h-[400px] bg-red-300"></div>
+                <div className="w-[200px] h-[400px] bg-red-400"></div>
+                <div className="w-[200px] h-[400px] bg-red-500"></div>
+                <div className="w-[200px] h-[400px] bg-red-600"></div>
+                <div className="w-[200px] h-[400px] bg-red-700"></div>
 
             </div>
        </div>
