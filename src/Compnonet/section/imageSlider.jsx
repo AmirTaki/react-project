580ec13c7e18a2b1b6ee2cba2d42fe920549c0ec
amir <amirtaki623@gmail.com>
2025-09-13 21:37:25 +0330

    day 10 part 12

--- src/Compnonet/section/imageSlider.jsx
+++ src/Compnonet/section/imageSlider.jsx
@@ -3,22 +3,22 @@ const ImageSlider = () => {
        <div className="w-[100%] h-[600px] bg-blue-500 flex items-center justify-center">
             <div className="
                 w-[90%] h-[500px] bg-red-500 lg:bg-yellow-500 sm:bg-green-500
-                lg:flex! lg:flex-wrap lg:flex-col lg:justify-center! lg:items-center lg:overflow-x-scroll! lg:gap-3
+                lg:flex! lg:flex-wrap lg:flex-col lg:justify-center! lg:items-center lg:overflow-x-scroll! 
                 "
             
             >
-                <div className="w-[200px] h-[400px] bg-sky-200"></div>
-                <div className="w-[200px] h-[400px] bg-sky-300"></div>
-                <div className="w-[200px] h-[400px] bg-sky-400"></div>
-                <div className="w-[200px] h-[400px] bg-sky-500"></div>
-                <div className="w-[200px] h-[400px] bg-sky-600"></div>
-                <div className="w-[200px] h-[400px] bg-sky-700"></div>
-                <div className="w-[200px] h-[400px] bg-red-200"></div>
-                <div className="w-[200px] h-[400px] bg-red-300"></div>
-                <div className="w-[200px] h-[400px] bg-red-400"></div>
-                <div className="w-[200px] h-[400px] bg-red-500"></div>
-                <div className="w-[200px] h-[400px] bg-red-600"></div>
-                <div className="w-[200px] h-[400px] bg-red-700"></div>
+                <div className="w-[200px] h-[400px] bg-sky-200 mx-3"></div>
+                <div className="w-[200px] h-[400px] bg-sky-300 mx-3"></div>
+                <div className="w-[200px] h-[400px] bg-sky-400 mx-3"></div>
+                <div className="w-[200px] h-[400px] bg-sky-500 mx-3"></div>
+                <div className="w-[200px] h-[400px] bg-sky-600 mx-3"></div>
+                <div className="w-[200px] h-[400px] bg-sky-700 mx-3"></div>
+                <div className="w-[200px] h-[400px] bg-red-200 mx-3"></div>
+                <div className="w-[200px] h-[400px] bg-red-300 mx-3"></div>
+                <div className="w-[200px] h-[400px] bg-red-400 mx-3"></div>
+                <div className="w-[200px] h-[400px] bg-red-500 mx-3"></div>
+                <div className="w-[200px] h-[400px] bg-red-600 mx-3"></div>
+                <div className="w-[200px] h-[400px] bg-red-700 mx-3"></div>
 
             </div>
        </div>
