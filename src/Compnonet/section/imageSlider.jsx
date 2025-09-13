9e18032ffbf5d511611be2a988e4c5265a3b48b9
amir <amirtaki623@gmail.com>
2025-09-13 22:25:13 +0330

    day 10 part 32

--- src/Compnonet/section/imageSlider.jsx
+++ src/Compnonet/section/imageSlider.jsx
@@ -15,8 +15,8 @@ const ImageSlider = () => {
                 "
             
             >
-                <div className="w-[250px] lg:h-[420px]  h-[240px] bg-sky-200 lg:mx-3 my-1 overflow-hidden rounded-2xl">
-                    <div className="relative">
+                <div className="w-[250px] lg:h-[430px]  h-[240px] bg-white lg:mx-3 my-1 overflow-hidden rounded-2xl">
+                    <div className="relative ">
                         <div onClick = {handlerHeart} 
                         className={`absolute right-2 top-2 cursor-pointer hover:text-gray-900  duration-300 
                                     ${heart === 'bi bi-heart' ? "text-gray-500" : "text-red-600!"}
@@ -25,14 +25,16 @@ const ImageSlider = () => {
                         </div>
                         <img src={IMG} alt="" className="w-[100%] h-[100%] object-cover" />
                     </div>
-                    <div className="bg-white flex flex-col items-center ">
+                    <div className="bg-white flex flex-col  items-center ">
                         <div className="bg-white w-[100%] p-2 font-bold text-gray-700">
                             30$
                         </div>
                         <div className=" w-[95%]  p-1 text-gray-400 text-[16px]  hover:text-gray-800 duration-300">
                             Texas Tech Red Raiders Women's  College World Series Participant
                         </div>
-                        <div className="bg-blue-400"></div>
+                        <div className="bg-b w-[100%] px-2 my-1 text-gray-500  text-[14px]">
+                            Performance
+                        </div>
                     </div>
                 </div>
                 
