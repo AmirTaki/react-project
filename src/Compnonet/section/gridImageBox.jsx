e38aed79f72d6058b33c556f95922ca40423b005
amir <amirtaki623@gmail.com>
2025-09-14 21:45:22 +0330

    day 11 part 30

--- src/Compnonet/section/gridImageBox.jsx
+++ src/Compnonet/section/gridImageBox.jsx
@@ -4,17 +4,17 @@ import img10 from "../../assets/img10.jpg"
 import img11 from "../../assets/img11.jpg"
 const GridImageBox = () => {
     return(
-        <div className="w-[100%]  bg-red-400 grid grid-cols-2! max-lg:grid-cols-1!">
-            <div className="h-[600px] bg-blue-200">
+        <div className="w-[100%]  bg-red-400 grid grid-cols-2! max-lg:grid-cols-1! ">
+            <div className=" bg-blue-200">
                 <img src={img8} className="w-[100%]  h-[100%] " alt="" />
             </div>
-            <div className="h-[600px] bg-blue-200">
+            <div className=" bg-blue-200">
                 <img src={img9} className="w-[100%]  h-[100%]" alt="" />
             </div>
-            <div className="h-[600px] bg-blue-200">
+            <div className=" bg-blue-200">
                 <img src={img10} className="w-[100%]  h-[100%] " alt="" />
             </div>
-            <div className="h-[600px] bg-blue-200">
+            <div className=" bg-blue-200">
                 <img src={img11} className="w-[100%]  h-[100%]" alt="" />
             </div>
             
