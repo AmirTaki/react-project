979d4d4339e5dccae9fb9402c2efae561064e141
amir <amirtaki623@gmail.com>
2025-09-14 21:17:28 +0330

    day 11 part 26

--- src/Compnonet/section/gridImageBox.jsx
+++ src/Compnonet/section/gridImageBox.jsx
@@ -1,6 +1,6 @@
 const GridImageBox = () => {
     return(
-        <div className="w-[100%]  bg-red-400 grid grid-cols-2">
+        <div className="w-[100%]  bg-red-400 grid grid-cols-2! max-lg:grid-cols-1!">
             <div className="h-[300px] bg-blue-200"></div>
             <div className="h-[300px] bg-blue-400"></div>
             <div className="h-[300px] bg-yellow-200"></div>
