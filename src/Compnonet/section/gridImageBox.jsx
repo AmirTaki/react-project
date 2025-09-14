793808fdf2654ac98ddf4f26baef5df8ffb046cd
amir <amirtaki623@gmail.com>
2025-09-14 21:40:14 +0330

    day 11 part 28

--- src/Compnonet/section/gridImageBox.jsx
+++ src/Compnonet/section/gridImageBox.jsx
@@ -1,3 +1,7 @@
+import img8 from "../../assets/img8.jpg"
+import img9 from "../../assets/img9.jpg"
+import img10 from "../../assets/img10.jpg"
+import img11 from "../../assets/img11.jpg"
 const GridImageBox = () => {
     return(
         <div className="w-[100%]  bg-red-400 grid grid-cols-2! max-lg:grid-cols-1!">
