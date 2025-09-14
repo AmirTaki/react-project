c0098469f52ee41799a18ec53f9e18d2e04d884e
amir <amirtaki623@gmail.com>
2025-09-14 21:52:36 +0330

    day 11 part 334

--- src/Compnonet/section/gridImageBox.jsx
+++ src/Compnonet/section/gridImageBox.jsx
@@ -7,7 +7,9 @@ const GridImageBox = () => {
         <div className="w-[100%]  grid grid-cols-2! max-lg:grid-cols-1! ">
             <div className=" relative">
                 <img src={img8} className="w-[100%]  max-lg:h-[600px]! object-cover!  " alt="" />
-                <div className="w-[200px] h-[400px] bg-yellow-300 absolute right-0 top-0"></div>
+                <div className="w-[300px] h-[400px] bg-yellow-300 absolute right-0 top-0 flex flex-col">
+                    <div className="">For Rain or Shine</div>
+                </div>
             </div>
             <div className=" bg-blue-200">
                 <img src={img9} className="w-[100%]  max-lg:h-[600px]! object-cover! " alt="" />
