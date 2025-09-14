cd915dd0b45d412727d4a758da9f3dc1c274b974
amir <amirtaki623@gmail.com>
2025-09-14 22:20:45 +0330

    day 11 part 41

--- src/Compnonet/section/gridImageBox.jsx
+++ src/Compnonet/section/gridImageBox.jsx
@@ -7,10 +7,10 @@ const GridImageBox = () => {
         <div className="w-[100%]  grid grid-cols-2! max-lg:grid-cols-1! ">
             <div className=" relative">
                 <img src={img8} className="w-[100%]  max-lg:h-[600px]! object-cover!  " alt="" />
-                <div className="w-[300px] h-[400px] bg-yellow-300 absolute right-0 top-0 flex flex-col items-center justify-center">
+                <div className="w-[300px] h-[400px] bg-yellow-300 absolute right-0 top-0 flex flex-col items-center justify-center gap-4">
                     <div className="bg-white text-gray-300 hover:text-gray-700 hover:duration-600 duration-600 rounded-md hover:scale-110  w-[50%]  text-center flex items-center justify-center h-[40px] ">For Rain or Shine</div>
-                    <div className="group  w-[80%] h-[50px] bg-black flex text-white items-center">tick
-                       <i className="bi bi-arrow-right group-hover:translate-x-22! group-hover:duration-500!    transform  group-[:not(:hover)]:translate-x-0! group-[:not(:hover)]:duration-500! "></i>
+                    <div className="group hover:cursor-pointer p-2 w-[80%] h-[50px] bg-amber-400 rounded-md flex text-white items-center">tick
+                       <i className=" pl-2 bi bi-arrow-right group-hover:translate-x-21! group-hover:duration-900!    transform  group-[:not(:hover)]:translate-x-0! group-[:not(:hover)]:duration-900! "></i>
                     </div>
                 </div>
             </div>
