e360d0bc5ba0a89fee520376839b4c2425d2d128
amir <amirtaki623@gmail.com>
2025-09-15 19:54:05 +0330

    day 12 part 13

--- src/Compnonet/section/sliderPage.jsx
+++ src/Compnonet/section/sliderPage.jsx
@@ -2,27 +2,40 @@ import { useRef } from "react"
 
 const SliderPage = () => {
     const refSlide =    useRef()
-    const left = () => {
+    const right = () => {
        if (refSlide.current){
         refSlide.current.style.scrollBehavior = "smooth"
         refSlide.current.scrollLeft += refSlide.current.offsetWidth
 
        }
     }
+    const left = () => {
+       if (refSlide.current){
+        refSlide.current.style.scrollBehavior = "smooth"
+        refSlide.current.scrollLeft -= refSlide.current.offsetWidth
+
+       }
+    }
     return(
         <div  className="w-[100%] h-[700px] bg-gray-600 flex items-center justify-center">
-            <div ref = {refSlide} className="w-[70%] h-[500px] bg-blue-200 flex flex-col flex-wrap overflow-scroll">
+            <div ref = {refSlide} className="w-[70%] h-[500px] bg-blue-200 flex flex-col flex-wrap  overflow-hidden!">
 
-                <div className="w-[33.3%] h-[500px] bg-amber-400"></div>
-                <div className="w-[33.3%] h-[500px] bg-red-400"></div>
-                <div className="w-[33.3%] h-[500px] bg-blue-400"></div>
-                <div className="w-[33.3%] h-[500px] bg-green-400"></div>
-                <div className="w-[33.3%] h-[500px] bg-yellow-400"></div>
+                <div className="w-[33.2%] h-[500px] bg-amber-400"></div>
+                <div className="w-[33.2%] h-[500px] bg-red-400"></div>
+                <div className="w-[33.2%] h-[500px] bg-blue-400"></div>
+                <div className="w-[33.2%] h-[500px] bg-green-400"></div>
+                <div className="w-[33.2%] h-[500px] bg-yellow-400"></div>
+                <div className="w-[33.2%] h-[500px] bg-green-400"></div>
+                <div className="w-[33.2%] h-[500px] bg-red-400"></div>
+                <div className="w-[33.2%] h-[500px] bg-blue-400"></div>
+                <div className="w-[33.2%] h-[500px] bg-green-400"></div>
+                <div className="w-[33.2%] h-[500px] bg-yellow-400"></div>
 
             </div>
 
 
-            <div onClick={left} className="">click right</div>
+            <div onClick={right} className="">click right</div>
+            <div onClick={left} className="">click left</div>
         </div>
     )
 }
