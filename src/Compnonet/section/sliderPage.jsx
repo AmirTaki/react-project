49ccc90971a1b3ab4f979dad77bfc5e773ecb93e
amir <amirtaki623@gmail.com>
2025-09-15 20:32:01 +0330

    day 12 part 17

--- src/Compnonet/section/sliderPage.jsx
+++ src/Compnonet/section/sliderPage.jsx
@@ -1,4 +1,18 @@
 import { useRef } from "react"
+import slide1 from "../../assets/slide1.webp"
+import slide2 from "../../assets/slide2.webp"
+import slide3 from "../../assets/slide3.webp"
+import slide4 from "../../assets/slide4.webp"
+import slide5 from "../../assets/slide5.webp"
+import slide6 from "../../assets/slide6.webp"
+import slide7 from "../../assets/slide7.webp"
+import slide8 from "../../assets/slide8.webp"
+import slide9 from "../../assets/slide9.webp"
+import slide10 from "../../assets/slide10.webp"
+import slide11 from "../../assets/slide11.webp"
+import slide12 from "../../assets/slide12.webp"
+import slide13 from "../../assets/slide13.webp"
+import slide14 from "../../assets/slide14.webp"
 
 const SliderPage = () => {
     const refSlide =    useRef()
