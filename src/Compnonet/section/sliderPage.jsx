a29acb7615a0486abb89a65551e216cb8334910c
amir <amirtaki623@gmail.com>
2025-09-15 19:41:49 +0330

    day 12 part 4

--- src/Compnonet/section/sliderPage.jsx
+++ src/Compnonet/section/sliderPage.jsx
@@ -1,7 +1,13 @@
 const SliderPage = () => {
     return(
         <div className="w-[100%] h-[700px] bg-gray-600 flex items-center justify-center">
-            <div className="w-[60%] h-[500px] bg-blue-200"></div>
+            <div className="w-[70%] h-[500px] bg-blue-200 flex flex-col overflow-scroll">
+
+                <div className="w-[25%] h-[500px] bg-amber-400"></div>
+                <div className="w-[25%] h-[500px] bg-red-400"></div>
+                <div className="w-[25%] h-[500px] bg-amber-400"></div>
+
+            </div>
         </div>
     )
 }
