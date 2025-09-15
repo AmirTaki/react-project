449bd4c840d1f997af543e29d91692f6f5d2f304
amir <amirtaki623@gmail.com>
2025-09-15 19:46:29 +0330

    day 12 part 9

--- src/Compnonet/section/sliderPage.jsx
+++ src/Compnonet/section/sliderPage.jsx
@@ -3,11 +3,13 @@ import { useRef } from "react"
 const SliderPage = () => {
     const refSlide =    useRef()
     const left = () => {
-
+       if (refSlide.current){
+        
+       }
     }
     return(
-        <div className="w-[100%] h-[700px] bg-gray-600 flex items-center justify-center">
-            <div className="w-[70%] h-[500px] bg-blue-200 flex flex-col flex-wrap overflow-scroll">
+        <div  className="w-[100%] h-[700px] bg-gray-600 flex items-center justify-center">
+            <div ref = {refSlide} className="w-[70%] h-[500px] bg-blue-200 flex flex-col flex-wrap overflow-scroll">
 
                 <div className="w-[33.3%] h-[500px] bg-amber-400"></div>
                 <div className="w-[33.3%] h-[500px] bg-red-400"></div>
