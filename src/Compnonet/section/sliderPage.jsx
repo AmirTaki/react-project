3c534e9ebe90d937e7b0301e8cc15a44e3aac085
amir <amirtaki623@gmail.com>
2025-09-15 19:43:51 +0330

    day 12 part 6

--- src/Compnonet/section/sliderPage.jsx
+++ src/Compnonet/section/sliderPage.jsx
@@ -3,13 +3,16 @@ const SliderPage = () => {
         <div className="w-[100%] h-[700px] bg-gray-600 flex items-center justify-center">
             <div className="w-[70%] h-[500px] bg-blue-200 flex flex-col flex-wrap overflow-scroll">
 
-                <div className="w-[25%] h-[500px] bg-amber-400"></div>
-                <div className="w-[25%] h-[500px] bg-red-400"></div>
-                <div className="w-[25%] h-[500px] bg-blue-400"></div>
-                <div className="w-[25%] h-[500px] bg-green-400"></div>
-                <div className="w-[25%] h-[500px] bg-yellow-400"></div>
+                <div className="w-[33.3%] h-[500px] bg-amber-400"></div>
+                <div className="w-[33.3%] h-[500px] bg-red-400"></div>
+                <div className="w-[33.3%] h-[500px] bg-blue-400"></div>
+                <div className="w-[33.3%] h-[500px] bg-green-400"></div>
+                <div className="w-[33.3%] h-[500px] bg-yellow-400"></div>
 
             </div>
+
+
+            <div className="">click right</div>
         </div>
     )
 }
