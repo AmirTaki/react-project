918264b998ba37e585f1604cd590aac5a68aaf50
amir <amirtaki623@gmail.com>
2025-09-15 21:04:18 +0330

    day 12 part 28

--- src/Compnonet/section/sliderPage.jsx
+++ src/Compnonet/section/sliderPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import slide1 from "../../assets/slide1.webp"
 import slide2 from "../../assets/slide2.webp"
 import slide3 from "../../assets/slide3.webp"
@@ -16,11 +16,15 @@ import slide14 from "../../assets/slide14.webp"
 
 const SliderPage = () => {
     const refSlide =    useRef()
+    const [conter, setConter] = useState(1)
     const listImg = [slide1, slide2, slide3, slide4, slide5, slide6, slide7, slide8, slide9, slide10, slide11, slide12, slide13, slide14 ]
+    
     const right = () => {
        if (refSlide.current){
         refSlide.current.style.scrollBehavior = "smooth"
-        refSlide.current.scrollLeft += refSlide.current.offsetWidth
+        // refSlide.current.scrollLeft += refSlide.current.offsetWidth
+        (setConter((conter) => (conter + 1)))
+        refSlide.current.scrollLeft = refSlide.current.offsetWidth * conter
 
        }
     }
@@ -43,12 +47,13 @@ const SliderPage = () => {
             window.addEventListener('resize', handleImage )
         }
     }, [])
+
     return(
         <div  className="w-[100%] h-[700px] bg-gray-500 flex items-center justify-center relative">
             <div ref = {refSlide} className="w-[80%] h-[500px] bg-white flex flex-col flex-wrap  overflow-hidden! ">
 
                 {listImg.map((item, index) => (
-                    <div className="w-[33.35%] p-3 max-lg:w-[50%] flex-col flex justify-center max-sm:w-[100%] h-[500px]  bg-white">
+                    <div key = {index} className="w-[33.35%] p-3 max-lg:w-[50%] flex-col flex justify-center max-sm:w-[100%] h-[500px]  bg-white">
                         <img src={item} className="w-[100%] h-[100%]" alt="" />
                         <div className="bg-gray-300 h-[60px]  flex justify-center items-center text-center text-sm">AI-powered Everyday with AI PCs from ASUS</div>
                     </div>
