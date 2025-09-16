87ebd8a24674739bceaf398f77a1afd1dcf8e707
amir <amirtaki623@gmail.com>
2025-09-16 22:35:38 +0330

    day 13 part 26

--- src/Compnonet/section/imageAdvert.jsx
+++ src/Compnonet/section/imageAdvert.jsx
@@ -8,6 +8,8 @@ const ImageAdvert = () => {
     return(
         <>
             <div className="w-[100%] h-[500px] bg-red-500 flex flex-col flex-wrap overflow-x-scroll ">
+                <div className="bg-pink-500 w-[50%] h-[100%] max-lg:hidden "></div>
+                <div className="bg-orange-500 max-lg:flex! max-sm:flex! max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%] hidden "></div>
                 <div className="w-[25%] h-[50%] bg-blue-300  max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%]"></div>
                 <div className="w-[25%] h-[50%] bg-blue-400  max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%]"></div>
                 <div className="w-[25%] h-[50%] bg-blue-500  max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%]"></div>
