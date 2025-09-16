0b54127cec26eb3baf09c583357556f9f5643d43
amir <amirtaki623@gmail.com>
2025-09-16 23:04:06 +0330

    day 13 part 40

--- src/Compnonet/section/imageAdvert.jsx
+++ src/Compnonet/section/imageAdvert.jsx
@@ -3,9 +3,15 @@ import img2 from "../../assets/advert2.jpg"
 import img3 from "../../assets/advert3.jpg"
 import img4 from "../../assets/advert4.jpg"
 import img5 from "../../assets/advert5.jpg"
+import img6 from  "../../assets/img2.jpg"
+import img7 from  "../../assets/img3.jpg"
+import img8 from  "../../assets/img4.jpg"
+import img9 from  "../../assets/img5.jpg"
+import img10 from  "../../assets/img7.jpg"
+import img11 from  "../../assets/img9.jpg"
 
 const ImageAdvert = () => {
-    const listImg = [img2, img3, img4, img5]
+    const listImg = [img2, img3, img4, img5, img6, img7, img8, img9, img10, img11]
     return(
         <>
             <div className="w-[100%] h-[500px] bg-white flex flex-col flex-wrap overflow-x-scroll ">
