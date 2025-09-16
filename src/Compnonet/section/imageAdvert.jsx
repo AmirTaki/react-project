f9686f4aaffea33c163b5e727efa7392d9701cd3
amir <amirtaki623@gmail.com>
2025-09-16 22:31:39 +0330

    day 13 part 24

--- src/Compnonet/section/imageAdvert.jsx
+++ src/Compnonet/section/imageAdvert.jsx
@@ -7,17 +7,17 @@ import img5 from "../../assets/advert5.jpg"
 const ImageAdvert = () => {
     return(
         <>
-            <div className="w-[100%] h-[500px] bg-red-500 flex flex-col flex-wrap ">
-                <div className="w-[25%] h-[50%] bg-blue-300  max-lg:w-[50%] max-lg:h-[100%]"></div>
-                <div className="w-[25%] h-[50%] bg-blue-400  max-lg:w-[50%] max-lg:h-[100%]"></div>
-                <div className="w-[25%] h-[50%] bg-blue-500  max-lg:w-[50%] max-lg:h-[100%]"></div>
-                <div className="w-[25%] h-[50%] bg-blue-600  max-lg:w-[50%] max-lg:h-[100%]"></div>
-                <div className="w-[25%] h-[50%] bg-blue-700  max-lg:w-[50%] max-lg:h-[100%]"></div>
-                <div className="w-[25%] h-[50%] bg-green-300 max-lg:w-[50%] max-lg:h-[100%]"></div>
-                <div className="w-[25%] h-[50%] bg-green-400 max-lg:w-[50%] max-lg:h-[100%]"></div>
-                <div className="w-[25%] h-[50%] bg-green-500 max-lg:w-[50%] max-lg:h-[100%]"></div>
-                <div className="w-[25%] h-[50%] bg-green-600 max-lg:w-[50%] max-lg:h-[100%]"></div>
-                <div className="w-[25%] h-[50%] bg-green-700 max-lg:w-[50%] max-lg:h-[100%]"></div>
+            <div className="w-[100%] h-[500px] bg-red-500 flex flex-col flex-wrap overflow-scroll ">
+                <div className="w-[25%] h-[50%] bg-blue-300  max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%]"></div>
+                <div className="w-[25%] h-[50%] bg-blue-400  max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%]"></div>
+                <div className="w-[25%] h-[50%] bg-blue-500  max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%]"></div>
+                <div className="w-[25%] h-[50%] bg-blue-600  max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%]"></div>
+                <div className="w-[25%] h-[50%] bg-blue-700  max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%]"></div>
+                <div className="w-[25%] h-[50%] bg-green-300 max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%]"></div>
+                <div className="w-[25%] h-[50%] bg-green-400 max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%]"></div>
+                <div className="w-[25%] h-[50%] bg-green-500 max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%]"></div>
+                <div className="w-[25%] h-[50%] bg-green-600 max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%]"></div>
+                <div className="w-[25%] h-[50%] bg-green-700 max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%]"></div>
             </div>
         </>
         // flex   flex-col flex-wrap overflow-x-scroll
