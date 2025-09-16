d0b8f7f882cde310221871a400d9d6d83fef4d1b
amir <amirtaki623@gmail.com>
2025-09-16 20:26:41 +0330

    day 13 part 17

--- src/Compnonet/section/imageAdvert.jsx
+++ src/Compnonet/section/imageAdvert.jsx
@@ -7,12 +7,13 @@ import img5 from "../../assets/advert5.jpg"
 const ImageAdvert = () => {
     return(
         <>
+            <div className="bg-red-500 h-[500px] w-[50%] max-lg:w-[100%] block  "></div>
+            <div className="bg-green-500 h-[50%] w-[25%] max-lg:w-[100%]  inline-block "></div>
+            <div className="bg-pink-500 h-[50%] w-[25%] max-lg:w-[100%]  block   "></div>
+            <div className="bg-amber-500 h-[50%] w-[25%] max-lg:w-[100%]    block "></div>
         </>
         // flex   flex-col flex-wrap overflow-x-scroll
-            <div className="bg-red-500 h-[500px] w-[50%] max-lg:w-[100%] inline-flex  "></div>
-            <div className="bg-green-500 h-[50%] w-[25%] max-lg:w-[100%]   inline-flex "></div>
-            <div className="bg-pink-500 h-[50%] w-[25%] max-lg:w-[100%]  inline-flex   "></div>
-            <div className="bg-amber-500 h-[50%] w-[25%] max-lg:w-[100%]    inline-flex "></div>
+     
 
     )
 }
