777b6c35a9504411b07a334c49cefe51c7154891
amir <amirtaki623@gmail.com>
2025-09-16 19:32:19 +0330

    day 13 part 6

--- src/Compnonet/section/imageAdvert.jsx
+++ src/Compnonet/section/imageAdvert.jsx
@@ -1,7 +1,13 @@
+import img1 from "../../assets/advert1.jpg"
+import img2 from "../../assets/advert2.jpg"
+import img3 from "../../assets/advert3.jpg"
+import img4 from "../../assets/advert4.jpg"
+import img5 from "../../assets/advert5.jpg"
+
 const ImageAdvert = () => {
     return(
         <div className="w-[100%] h-[500px] bg-blue-500">
-            
+
         </div>
     )
 }
