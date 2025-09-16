cffdb4ebeaf4e044e069be065690aceafc4dee1d
amir <amirtaki623@gmail.com>
2025-09-16 19:18:23 +0330

    day 13 part 1

--- src/Compnonet/section/sliderPage.jsx
+++ src/Compnonet/section/sliderPage.jsx
@@ -50,11 +50,12 @@ const SliderPage = () => {
     }
         const [state, dispatch] = useReducer (reduce, {
             conter : 0,
-            listImg : [slide1, slide2, slide3, slide4, slide5, slide6, slide7, slide8, slide9, slide10, slide11, slide12, slide13, slide14 ],
+            listImg : [slide1, slide2, slide3, slide4, slide5, slide6, slide7, slide8, slide9 , slide10, slide11, slide12, slide13, slide14],
             changeColor : {}
         })
 
-        const listItemsTwo = Array.from({length : Math.round (state.listImg.length) / 2}, (_) => `${_}`)
+        const listItemsTwo = Array.from({length : Math.round (state.listImg.length / 2) }, (_) => `${_}`)
+
    
    
     const right = () => {
