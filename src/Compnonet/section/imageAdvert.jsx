e85cabdc05f11c379f99feb15d9fdc794ef68ecd
amir <amirtaki623@gmail.com>
2025-09-16 23:52:06 +0330

    day 13 part 48

--- src/Compnonet/section/imageAdvert.jsx
+++ src/Compnonet/section/imageAdvert.jsx
@@ -52,33 +52,33 @@ const ImageAdvert = () => {
     return(
         <>
         <div className="relative">
-                <div ref = {refSlider} className="w-[100%] h-[500px] bg-white flex flex-col flex-wrap overflow-x-scroll  ">
+            <div ref = {refSlider} className="w-[100%] h-[500px] bg-white flex flex-col flex-wrap overflow-x-scroll  ">
                 
-                    <div className=" w-[50%] h-[100%]   relative max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%] p-1 max-sm:p-0!">
-                        <img src={img1} className="w-[100%] h-[100%]! object-fill" alt="" />
-                        <div className="bg-black bg-opacity-20 absolute bottom-1 w-[90%] text-white p-3 max-sm:w-[100%]!" >
-                            <h4 className="text-gray-400">0-The ROG Pelta wireless gaming headset :</h4>
+                <div className=" w-[50%] h-[100%]   relative max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%] p-1 max-sm:p-0!">
+                    <img src={img1} className="w-[100%] h-[100%]! object-fill" alt="" />
+                    <div className="bg-black bg-opacity-20 absolute bottom-1 w-[90%] text-white p-3 max-sm:w-[100%]!" >
+                        <h4 className="text-gray-400">0-The ROG Pelta wireless gaming headset :</h4>
+                        <p className="text-gray-200 text-sm">The ROG Pelta's ergomomic, comfort-focused design, high-end audio spaecs, and stunning battery life make it an ideal match for any gamer.</p>
+                    </div>
+                </div>
+
+                {listImg.map((item, index) => (
+                    <div key = {index} className="w-[25%] h-[50%]   max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%] relative p-1 max-sm:p-0!">
+                        <img src={item} className="w-[100%] h-[100%]! object-fill" alt="" />
+                        <div className="bg-black bg-opacity-20 absolute bottom-1 text-white p-3 w-[98%] max-sm:w-[100%]!  " >
+                            <h4 className="text-gray-400">{index + 1}-The ROG Pelta wireless gaming headset :</h4>
                             <p className="text-gray-200 text-sm">The ROG Pelta's ergomomic, comfort-focused design, high-end audio spaecs, and stunning battery life make it an ideal match for any gamer.</p>
                         </div>
                     </div>
-
-                    {listImg.map((item, index) => (
-                        <div key = {index} className="w-[25%] h-[50%]   max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%] relative p-1 max-sm:p-0!">
-                            <img src={item} className="w-[100%] h-[100%]! object-fill" alt="" />
-                            <div className="bg-black bg-opacity-20 absolute bottom-1 text-white p-3 w-[98%] max-sm:w-[100%]!  " >
-                                <h4 className="text-gray-400">{index + 1}-The ROG Pelta wireless gaming headset :</h4>
-                                <p className="text-gray-200 text-sm">The ROG Pelta's ergomomic, comfort-focused design, high-end audio spaecs, and stunning battery life make it an ideal match for any gamer.</p>
-                            </div>
-                        </div>
-                    ))}
+                ))}
                
             </div>
-                <div className="absolute top-[50%] right-3 hover:scale-150 duration-300 text-white hover:text-gray-400 cursor-pointer">
-                    <i onClick = {RightButton} className="bi bi-caret-right text-2xl"></i>
-                </div>
-                <div className=" absolute top-[50%] left-3  hover:scale-150 duration-300 text-white hover:text-gray-400 cursor-pointer">
-                    <i onClick={LeftButton} className="bi bi-caret-left  text-2xl"></i>
-                </div>
+            <div className="absolute top-[50%] right-3 hover:scale-150 duration-300 text-white hover:text-gray-400 cursor-pointer">
+                <i onClick = {RightButton} className="bi bi-caret-right text-2xl"></i>
+            </div>
+            <div className={`${state.conter <= 0 ? "hidden!" : "flex!"} absolute top-[50%] left-3  hover:scale-150 duration-300 text-white hover:text-gray-400 cursor-pointer`}>
+                <i onClick={LeftButton} className="bi bi-caret-left  text-2xl"></i>
+            </div>
         </div>
        
            
