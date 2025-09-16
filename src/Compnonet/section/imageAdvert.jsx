c0a63f83da810509bb8e5b555fa2b4894c563c83
amir <amirtaki623@gmail.com>
2025-09-16 22:29:22 +0330

    day 13 part 22

--- src/Compnonet/section/imageAdvert.jsx
+++ src/Compnonet/section/imageAdvert.jsx
@@ -9,15 +9,15 @@ const ImageAdvert = () => {
         <>
             <div className="w-[100%] h-[500px] bg-red-500 flex flex-col flex-wrap ">
                 <div className="w-[25%] h-[50%] bg-blue-300 max-md:w-[50%] max-md:h-[100%]"></div>
-                <div className="w-[25%] h-[50%] bg-blue-400"></div>
-                <div className="w-[25%] h-[50%] bg-blue-500"></div>
-                <div className="w-[25%] h-[50%] bg-blue-600"></div>
-                <div className="w-[25%] h-[50%] bg-blue-700"></div>
-                <div className="w-[25%] h-[50%] bg-green-300"></div>
-                <div className="w-[25%] h-[50%] bg-green-400"></div>
-                <div className="w-[25%] h-[50%] bg-green-500"></div>
-                <div className="w-[25%] h-[50%] bg-green-600"></div>
-                <div className="w-[25%] h-[50%] bg-green-700"></div>
+                <div className="w-[25%] h-[50%] bg-blue-400 max-md:w-[50%] max-md:h-[100%]"></div>
+                <div className="w-[25%] h-[50%] bg-blue-500 max-md:w-[50%] max-md:h-[100%]"></div>
+                <div className="w-[25%] h-[50%] bg-blue-600 max-md:w-[50%] max-md:h-[100%]"></div>
+                <div className="w-[25%] h-[50%] bg-blue-700 max-md:w-[50%] max-md:h-[100%]"></div>
+                <div className="w-[25%] h-[50%] bg-green-300 max-md:w-[50%] max-md:h-[100%]"></div>
+                <div className="w-[25%] h-[50%] bg-green-400 max-md:w-[50%] max-md:h-[100%]"></div>
+                <div className="w-[25%] h-[50%] bg-green-500 max-md:w-[50%] max-md:h-[100%]"></div>
+                <div className="w-[25%] h-[50%] bg-green-600 max-md:w-[50%] max-md:h-[100%]"></div>
+                <div className="w-[25%] h-[50%] bg-green-700 max-md:w-[50%] max-md:h-[100%]"></div>
             </div>
         </>
         // flex   flex-col flex-wrap overflow-x-scroll
