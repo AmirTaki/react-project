87cc16bcd0908fb1cb12529b1be63ad9f5b74f0e
amir <amirtaki623@gmail.com>
2025-09-16 22:53:33 +0330

    day 13 part 36

--- src/Compnonet/section/imageAdvert.jsx
+++ src/Compnonet/section/imageAdvert.jsx
@@ -12,18 +12,18 @@ const ImageAdvert = () => {
                 
                 <div className=" w-[50%] h-[100%]   relative max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%]">
                     <img src={img1} className="w-[100%] h-[100%]! object-cover" alt="" />
-                    <div className="bg-black bg-opacity-30 absolute bottom-0 text-white p-3 " >
+                    <div className="bg-black bg-opacity-20 absolute bottom-0 text-white p-3 " >
                         <h4 className="text-gray-400">0-The ROG Pelta wireless gaming headset :</h4>
-                        <p className="text-gray-200">The ROG Pelta's ergomomic, comfort-focused design, high-end audio spaecs, and stunning battery life make it an ideal match for any gamer.</p>
+                        <p className="text-gray-200 text-sm">The ROG Pelta's ergomomic, comfort-focused design, high-end audio spaecs, and stunning battery life make it an ideal match for any gamer.</p>
                     </div>
                 </div>
 
                 {listImg.map((item, index) => (
                     <div key = {index} className="w-[25%] h-[50%] bg-blue-300  max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%] relative">
                         <img src={item} className="w-[100%] h-[100%]! object-cover" alt="" />
-                        <div className="bg-black bg-opacity-30 absolute bottom-0 text-white p-3 " >
+                        <div className="bg-black bg-opacity-20 absolute bottom-0 text-white p-3 " >
                             <h4 className="text-gray-400">{index + 1}-The ROG Pelta wireless gaming headset :</h4>
-                            <p className="text-gray-200">The ROG Pelta's ergomomic, comfort-focused design, high-end audio spaecs, and stunning battery life make it an ideal match for any gamer.</p>
+                            <p className="text-gray-200 text-sm">The ROG Pelta's ergomomic, comfort-focused design, high-end audio spaecs, and stunning battery life make it an ideal match for any gamer.</p>
                         </div>
                     </div>
 
