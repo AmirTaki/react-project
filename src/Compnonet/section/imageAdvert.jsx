b8adf8075ef0c63ee10ce48b13fa794f6d1db0ba
amir <amirtaki623@gmail.com>
2025-09-17 01:56:52 +0330

    day 13 part 51

--- src/Compnonet/section/imageAdvert.jsx
+++ src/Compnonet/section/imageAdvert.jsx
@@ -12,7 +12,7 @@ import img10 from  "../../assets/img7.jpg"
 import img11 from  "../../assets/img9.jpg"
 
 const ImageAdvert = () => {
-    const listImg = [img1 ,img2, img3, img4, img5, img6, img7, img8, img9, img10, ]
+    const listImg = [img1 ,img2, img3, img4, img5, img6, img7, img8, img9 , img10, img11]
     const refSlider = useRef()
   
 
@@ -34,9 +34,9 @@ const ImageAdvert = () => {
     }
     const [state, dispatch] = useReducer(reducer, {
         conter : 0,
-        maxConter : Math.round(listImg.length / 2)
+        maxConter : Math.ceil(listImg.length /  2 )  
     })
-
+    console.log(Math.ceil(listImg.length /  2 ) )
     
     const RightButton = () => {
         dispatch({type : "right", payload : 1})
@@ -54,13 +54,13 @@ const ImageAdvert = () => {
         <div className="relative">
             <div ref = {refSlider} className="w-[100%] h-[500px] bg-white flex flex-col flex-wrap overflow-x-scroll  ">
                 
-                <div className=" w-[50%] h-[100%]   relative max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%] p-1 max-sm:p-0!">
+                {/* <div className=" w-[50%] h-[100%]   relative max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%] p-1 max-sm:p-0!">
                     <img src={img1} className="w-[100%] h-[100%]! object-fill" alt="" />
                     <div className="bg-black bg-opacity-20 absolute bottom-1 w-[90%] text-white p-3 max-sm:w-[100%]!" >
                         <h4 className="text-gray-400">0-The ROG Pelta wireless gaming headset :</h4>
                         <p className="text-gray-200 text-sm">The ROG Pelta's ergomomic, comfort-focused design, high-end audio spaecs, and stunning battery life make it an ideal match for any gamer.</p>
                     </div>
-                </div>
+                </div> */}
 
                 {listImg.map((item, index) => (
                     <div key = {index} className="w-[25%] h-[50%]   max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%] relative p-1 max-sm:p-0!">
@@ -73,7 +73,7 @@ const ImageAdvert = () => {
                 ))}
                
             </div>
-            <div className={`${state.conter >= state.maxConter   ? "hidden!" : "flex!" } absolute top-[50%] right-3 hover:scale-150 duration-300 text-white hover:text-gray-400 cursor-pointer`}>
+            <div className={`${state.conter  >= state.maxConter  - 1 ? "hidden!" : "flex!" } absolute top-[50%] right-3 hover:scale-150 duration-300 text-white hover:text-gray-400 cursor-pointer`}>
                 <i onClick = {RightButton} className="bi bi-caret-right text-2xl"></i>
             </div>
             <div className={`${state.conter <= 0 ? "hidden!" : "flex!"} absolute top-[50%] left-3  hover:scale-150 duration-300 text-white hover:text-gray-400 cursor-pointer`}>
