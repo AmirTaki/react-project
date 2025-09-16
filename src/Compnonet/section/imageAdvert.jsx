6b2e5950a40c98b2f9bff5be8672eaa8b62510a3
amir <amirtaki623@gmail.com>
2025-09-17 02:43:36 +0330

    day 13 part 52

--- src/Compnonet/section/imageAdvert.jsx
+++ src/Compnonet/section/imageAdvert.jsx
@@ -51,17 +51,16 @@ const ImageAdvert = () => {
     
     return(
         <>
-        <div className="relative">
-            <div ref = {refSlider} className="w-[100%] h-[500px] bg-white flex flex-col flex-wrap overflow-x-scroll  ">
+        <div className="relative flex h-[500px]">
                 
-                {/* <div className=" w-[50%] h-[100%]   relative max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%] p-1 max-sm:p-0!">
-                    <img src={img1} className="w-[100%] h-[100%]! object-fill" alt="" />
-                    <div className="bg-black bg-opacity-20 absolute bottom-1 w-[90%] text-white p-3 max-sm:w-[100%]!" >
-                        <h4 className="text-gray-400">0-The ROG Pelta wireless gaming headset :</h4>
-                        <p className="text-gray-200 text-sm">The ROG Pelta's ergomomic, comfort-focused design, high-end audio spaecs, and stunning battery life make it an ideal match for any gamer.</p>
-                    </div>
-                </div> */}
-
+            <div className=" w-[50%] h-[100%]   relative max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%] p-1 max-sm:p-0!">
+                <img src={img1} className="w-[100%] h-[100%]! object-fill" alt="" />
+                <div className="bg-black bg-opacity-20 absolute bottom-1 w-[90%] text-white p-3 max-sm:w-[100%]!" >
+                    <h4 className="text-gray-400">0-The ROG Pelta wireless gaming headset :</h4>
+                    <p className="text-gray-200 text-sm">The ROG Pelta's ergomomic, comfort-focused design, high-end audio spaecs, and stunning battery life make it an ideal match for any gamer.</p>
+                </div>
+            </div>
+            <div ref = {refSlider} className="w-[50%] h-[500px] bg-white flex flex-col flex-wrap overflow-x-scroll  ">
                 {listImg.map((item, index) => (
                     <div key = {index} className="w-[25%] h-[50%]   max-lg:w-[50%] max-lg:h-[100%] max-sm:w-[100%] max-sm:h-[100%] relative p-1 max-sm:p-0!">
                         <img src={item} className="w-[100%] h-[100%]! object-fill" alt="" />
