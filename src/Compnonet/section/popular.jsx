3d7c8280d0d0a04e0c3736a70766351286953a62
amir <amirtaki623@gmail.com>
2025-09-17 16:50:10 +0330

    day 14 part 4

--- src/Compnonet/section/popular.jsx
+++ src/Compnonet/section/popular.jsx
@@ -1,6 +1,6 @@
 const Popular = () => {
     return(
-        <div className="flex bg-green-500">
+        <div className="flex bg-green-500 w-[100%] items-center justify-center h-[400px]">
              <div className="flex flex-col bg-amber-200  w-[90%] h-[300px]">
             <div className="bg-red-500" >
                 <h1>Popular right now</h1>
