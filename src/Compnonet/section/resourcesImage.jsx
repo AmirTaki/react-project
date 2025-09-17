9411f7ba6670556c2c17cb31ce2d63f1523bf4ea
amir <amirtaki623@gmail.com>
2025-09-17 21:14:30 +0330

    day 14 part 22

--- src/Compnonet/section/resourcesImage.jsx
+++ src/Compnonet/section/resourcesImage.jsx
@@ -10,23 +10,11 @@ const ResourcesImage = () => {
     return(
         <>
            <div className="w-[90%] h-[700px] bg-blue-500  mx-auto">
-                <div className="w-[100%] h-[500px] bg-red-500 flex items-center   flex-col flex-wrap   overflow-scroll  justify-center">
-                    <div className="w-[400px] h-[100%] bg-green-200">
-                        <img src="" alt="" />
+                <div className="w-[100%] h-[350px] bg-red-500 flex items-center   flex-col flex-wrap   overflow-scroll  justify-center">
+                    <div className="w-[400px] h-[100%] bg-green-200 flex justify-center">
+                        <img src={img1} alt=""  className="w-[350px] h-[300px]!" />
                     </div>
-                    <div className="w-[450px] h-[100%] bg-green-300"></div>
-                    <div className="w-[450px] h-[100%] bg-green-400"></div>
-                    <div className="w-[450px] h-[100%] bg-green-500"></div>
-                    <div className="w-[450px] h-[100%] bg-green-600"></div>
-                    <div className="w-[450px] h-[100%] bg-green-700"></div>
-                    <div className="w-[450px] h-[100%] bg-green-800"></div>
-                    <div className="w-[450px] h-[100%] bg-green-200"></div>
-                    <div className="w-[450px] h-[100%] bg-green-300"></div>
-                    <div className="w-[450px] h-[100%] bg-green-400"></div>
-                    <div className="w-[450px] h-[100%] bg-green-500"></div>
-                    <div className="w-[450px] h-[100%] bg-green-600"></div>
-                    <div className="w-[450px] h-[100%] bg-green-700"></div>
-                    <div className="w-[450px] h-[100%] bg-green-800"></div>
+               
                 </div>
             </div> 
 
