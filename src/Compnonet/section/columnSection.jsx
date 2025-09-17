c0e86952c0e4485c3423e0904de2f8910c296c67
amir <amirtaki623@gmail.com>
2025-09-17 22:05:56 +0330

    day 14 part 29

--- src/Compnonet/section/columnSection.jsx
+++ src/Compnonet/section/columnSection.jsx
@@ -1,7 +1,7 @@
 const ColumnSection = () => {
   return (
-    <div>
-      column section
+    <div className="w-[90%] bg-blue-600 mx-auto h-[700px]">
+      <div className="w-[100%] h-[100px] hover:h-[500px] duration-500 bg-red-500"></div>
     </div>
   )
 }
