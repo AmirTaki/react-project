7ecd6034c564958be342744e68b59e5764fea49b
amir <amirtaki623@gmail.com>
2025-09-17 17:09:59 +0330

    day 14 part 7

--- src/Compnonet/section/popular.jsx
+++ src/Compnonet/section/popular.jsx
@@ -1,19 +1,17 @@
 const Popular = () => {
     return(
-        <div className="flex bg-green-500 w-[100%] items-center justify-center h-[400px]">
-             <div className="flex flex-col bg-amber-200  w-[90%] h-[300px]">
-            <div className="bg-red-500" >
-                <h1>Popular right now</h1>
-            </div>
-            <div className="bg-blue-500 grid grid-cols-3 h-[100px]">
-                <div className="bg-gray-200 w-[140px] h-[10px]">
-                    <h1>Ultraboost</h1>
+        <div className="flex bg-white w-[100%] items-center justify-center h-[400px]">
+            <div className="flex flex-col gap-3 bg-transparent w-[90%] h-[300px]">
+                <div className=" font-black text-2xl" >
+                    <h1>Popular right now</h1>
                 </div>
-                <div className="bg-gray-300 w-[140px] h-[10px]"></div>
-                <div className="bg-gray-400 w-[140px] h-[10px]"></div>
-                <div className="bg-gray-500 w-[140px] h-[10px]"></div>
-                <div className="bg-gray-600 w-[140px] h-[10px]"></div>
-                <div className="bg-gray-700 w-[140px] h-[10px]"></div>
+        
+            <div className="grid grid-cols-3 h-[100px] mt-5">
+                <div className=" bg-white w-[20vw] h-[33px]  duration-300 cursor-pointer group ">
+                    <h1>Ultraboost</h1>
+                    <div className="bg-black mt-2 w-[20vw] h-[1px] group-hover:h-[3px] duration-100"></div>
+                </div>
+              
             </div>
         </div>
         </div>
