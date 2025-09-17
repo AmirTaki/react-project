234048b7568dbe8f48ad3540491d4920461c5cd2
amir <amirtaki623@gmail.com>
2025-09-17 22:08:25 +0330

    day 14 part 30

--- src/Compnonet/section/columnSection.jsx
+++ src/Compnonet/section/columnSection.jsx
@@ -1,7 +1,60 @@
 const ColumnSection = () => {
   return (
     <div className="w-[90%] bg-blue-600 mx-auto h-[700px]">
-      <div className="w-[100%] h-[100px] hover:h-[500px] duration-500 bg-red-500"></div>
+      <div className="w-[100%] h-[100px] hover:h-[500px] duration-500 bg-red-500 overflow-hidden flex flex-row justify-center">
+        <div className="bg-green-400">
+          <ul>menu
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+
+          </ul>
+
+        </div>
+        <div className="bg-yellow-400">
+          <ul>menu
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+
+          </ul>
+
+        </div>
+        <div className="bg-pink-400">
+          <ul>menu
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+
+          </ul>
+
+        </div>
+        <div className="bg-purple-600">
+          <ul>menu
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+
+          </ul>
+
+        </div>
+      </div>
     </div>
   )
 }
