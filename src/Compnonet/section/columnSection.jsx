34af853865c4d0144a997692545e402338cbb219
amir <amirtaki623@gmail.com>
2025-09-17 22:13:46 +0330

    day 14 part 32

--- src/Compnonet/section/columnSection.jsx
+++ src/Compnonet/section/columnSection.jsx
@@ -3,15 +3,17 @@ const ColumnSection = () => {
     <div className="w-[90%] bg-blue-600 mx-auto h-[700px]">
       <div className="w-[100%] h-[100px] hover:h-[500px] duration-500 bg-red-500 overflow-hidden flex flex-row justify-center">
         <div className="bg-green-400 w-50">
-          <ul>menu
-            <li>item</li>
-            <li>item</li>
-            <li>item</li>
-            <li>item</li>
-            <li>item</li>
-            <li>item</li>
-            <li>item</li>
-
+          <ul className="flex flex-col    items-center">
+            <div className="text-gray-500">menu</div>
+            <div className=""> 
+              <li className="text-gray-200 hover:text-gray-600 duration-300 cursor-pointer hover:underline">item</li>
+              <li className="text-gray-200 hover:text-gray-600 duration-300 cursor-pointer hover:underline">item</li>
+              <li className="text-gray-200 hover:text-gray-600 duration-300 cursor-pointer hover:underline">item</li>
+              <li className="text-gray-200 hover:text-gray-600 duration-300 cursor-pointer hover:underline">item</li>
+              <li className="text-gray-200 hover:text-gray-600 duration-300 cursor-pointer hover:underline">item</li>
+              <li className="text-gray-200 hover:text-gray-600 duration-300 cursor-pointer hover:underline">item</li>
+              <li className="text-gray-200 hover:text-gray-600 duration-300 cursor-pointer hover:underline">item</li>
+            </div>
           </ul>
 
         </div>
