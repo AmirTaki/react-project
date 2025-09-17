ac3f17e41e6aea7a3a4fa8cf9aa10686729eb743
amir <amirtaki623@gmail.com>
2025-09-17 21:09:30 +0330

    day 14 part 20

--- src/Compnonet/section/resourcesImage.jsx
+++ src/Compnonet/section/resourcesImage.jsx
@@ -3,20 +3,22 @@ const ResourcesImage = () => {
         <>
            <div className="w-[90%] h-[700px] bg-blue-500  mx-auto">
                 <div className="w-[100%] h-[500px] bg-red-500 flex items-center   flex-col flex-wrap   overflow-scroll  justify-center">
-                    <div className="w-[250px] h-[100%] bg-green-200"></div>
-                    <div className="w-[250px] h-[100%] bg-green-300"></div>
-                    <div className="w-[250px] h-[100%] bg-green-400"></div>
-                    <div className="w-[250px] h-[100%] bg-green-500"></div>
-                    <div className="w-[250px] h-[100%] bg-green-600"></div>
-                    <div className="w-[250px] h-[100%] bg-green-700"></div>
-                    <div className="w-[250px] h-[100%] bg-green-800"></div>
-                    <div className="w-[250px] h-[100%] bg-green-200"></div>
-                    <div className="w-[250px] h-[100%] bg-green-300"></div>
-                    <div className="w-[250px] h-[100%] bg-green-400"></div>
-                    <div className="w-[250px] h-[100%] bg-green-500"></div>
-                    <div className="w-[250px] h-[100%] bg-green-600"></div>
-                    <div className="w-[250px] h-[100%] bg-green-700"></div>
-                    <div className="w-[250px] h-[100%] bg-green-800"></div>
+                    <div className="w-[400px] h-[100%] bg-green-200">
+                        
+                    </div>
+                    <div className="w-[450px] h-[100%] bg-green-300"></div>
+                    <div className="w-[450px] h-[100%] bg-green-400"></div>
+                    <div className="w-[450px] h-[100%] bg-green-500"></div>
+                    <div className="w-[450px] h-[100%] bg-green-600"></div>
+                    <div className="w-[450px] h-[100%] bg-green-700"></div>
+                    <div className="w-[450px] h-[100%] bg-green-800"></div>
+                    <div className="w-[450px] h-[100%] bg-green-200"></div>
+                    <div className="w-[450px] h-[100%] bg-green-300"></div>
+                    <div className="w-[450px] h-[100%] bg-green-400"></div>
+                    <div className="w-[450px] h-[100%] bg-green-500"></div>
+                    <div className="w-[450px] h-[100%] bg-green-600"></div>
+                    <div className="w-[450px] h-[100%] bg-green-700"></div>
+                    <div className="w-[450px] h-[100%] bg-green-800"></div>
                 </div>
             </div> 
 
