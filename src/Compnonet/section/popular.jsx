61cda2f2ea857072d00226380e894f6bc97900a8
amir <amirtaki623@gmail.com>
2025-09-17 17:11:36 +0330

    day 14 part 8

--- src/Compnonet/section/popular.jsx
+++ src/Compnonet/section/popular.jsx
@@ -6,7 +6,27 @@ const Popular = () => {
                     <h1>Popular right now</h1>
                 </div>
         
-            <div className="grid grid-cols-3 h-[100px] mt-5">
+            <div className="grid grid-cols-3 h-[100px] mt-5 gap-10">
+                <div className=" bg-white w-[20vw] h-[33px]  duration-300 cursor-pointer group ">
+                    <h1 className="font-bold">Ultraboost</h1>
+                    <div className="bg-black mt-2 w-[20vw] h-[2px] group-hover:h-[7px] duration-100"></div>
+                </div>
+                <div className=" bg-white w-[20vw] h-[33px]  duration-300 cursor-pointer group ">
+                    <h1>Ultraboost</h1>
+                    <div className="bg-black mt-2 w-[20vw] h-[1px] group-hover:h-[3px] duration-100"></div>
+                </div>
+                <div className=" bg-white w-[20vw] h-[33px]  duration-300 cursor-pointer group ">
+                    <h1>Ultraboost</h1>
+                    <div className="bg-black mt-2 w-[20vw] h-[1px] group-hover:h-[3px] duration-100"></div>
+                </div>
+                <div className=" bg-white w-[20vw] h-[33px]  duration-300 cursor-pointer group ">
+                    <h1>Ultraboost</h1>
+                    <div className="bg-black mt-2 w-[20vw] h-[1px] group-hover:h-[3px] duration-100"></div>
+                </div>
+                <div className=" bg-white w-[20vw] h-[33px]  duration-300 cursor-pointer group ">
+                    <h1>Ultraboost</h1>
+                    <div className="bg-black mt-2 w-[20vw] h-[1px] group-hover:h-[3px] duration-100"></div>
+                </div>
                 <div className=" bg-white w-[20vw] h-[33px]  duration-300 cursor-pointer group ">
                     <h1>Ultraboost</h1>
                     <div className="bg-black mt-2 w-[20vw] h-[1px] group-hover:h-[3px] duration-100"></div>
