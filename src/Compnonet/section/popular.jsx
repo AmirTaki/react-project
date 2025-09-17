e14cbaa90925ecde76a6ff36f5c5e3728db48e5d
amir <amirtaki623@gmail.com>
2025-09-17 17:34:10 +0330

    day 14 part 14

--- src/Compnonet/section/popular.jsx
+++ src/Compnonet/section/popular.jsx
@@ -1,9 +1,9 @@
 const Popular = () => {
     const Items = ['ultraboost', 'samba', 'campus', 'gazelle', 'soccer', 'spezial']
     return(
-        <div className="flex justify-center  mt-20 bg-red-500 h-[250px] max-lg:h-[500px] overflow-hidden">
+        <div className="flex justify-center  my-20! bg-white h-[250px] max-lg:h-[500px] overflow-hidden">
             <div className="flex flex-col gap-3 bg-transparent w-[90vw]  h-[300px]">
-                <div className=" font-black text-2xl" >
+                <div className=" font-black text-2xl text-gray-300" >
                     <h1>Popular right now</h1>
                 </div>
         
@@ -12,7 +12,7 @@ const Popular = () => {
 
                     <div key = {index} className=" bg-white w-[20vw] max-lg:w-[90vw]!  h-[33px]  duration-300 cursor-pointer group ">
                         <h1 className="font-bold">{item}</h1>
-                        <div className="bg-black mt-3 w-[20vw] max-lg:w-[90vw] h-[2px] group-hover:h-[7px] duration-100"></div>
+                        <div className="bg-gray-300 mt-3 w-[20vw] max-lg:w-[90vw] h-[2px] group-hover:h-[7px] duration-100"></div>
                     </div>
                 ))}
               
