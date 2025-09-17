69078a3437f18b980bc9740a244d143a5da718bd
amir <amirtaki623@gmail.com>
2025-09-17 22:15:05 +0330

    day 14 part 33

--- src/Compnonet/section/columnSection.jsx
+++ src/Compnonet/section/columnSection.jsx
@@ -3,9 +3,9 @@ const ColumnSection = () => {
     <div className="w-[90%] bg-blue-600 mx-auto h-[700px]">
       <div className="w-[100%] h-[100px] hover:h-[500px] duration-500 bg-red-500 overflow-hidden flex flex-row justify-center">
         <div className="bg-green-400 w-50">
-          <ul className="flex flex-col    items-center">
+          <ul className="flex flex-col  gap-2  items-center">
             <div className="text-gray-500">menu</div>
-            <div className=""> 
+            <div className="flex flex-col gap-3"> 
               <li className="text-gray-200 hover:text-gray-600 duration-300 cursor-pointer hover:underline">item</li>
               <li className="text-gray-200 hover:text-gray-600 duration-300 cursor-pointer hover:underline">item</li>
               <li className="text-gray-200 hover:text-gray-600 duration-300 cursor-pointer hover:underline">item</li>
@@ -15,7 +15,6 @@ const ColumnSection = () => {
               <li className="text-gray-200 hover:text-gray-600 duration-300 cursor-pointer hover:underline">item</li>
             </div>
           </ul>
-
         </div>
         <div className="bg-yellow-400 w-50">
           <ul>menu
