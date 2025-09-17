484c89c8279dd83aaad28dcd2b650a00bd9ffb07
amir <amirtaki623@gmail.com>
2025-09-17 22:18:47 +0330

    day 14 part 36

--- src/Compnonet/section/columnSection.jsx
+++ src/Compnonet/section/columnSection.jsx
@@ -5,18 +5,15 @@ const ColumnSection = () => {
     <div className="w-[90%] bg-blue-600 mx-auto h-[700px]">
       <div className="w-[100%] h-[100px] hover:h-[500px] duration-500 bg-red-500 overflow-hidden flex flex-row justify-center">
         {menus.map((menu, index) => (
-          <div className="bg-green-400 w-50">
+          <div key = {index} className="bg-green-400 w-50">
             <ul className="flex flex-col  gap-2  items-center">
-              <div className="text-gray-500">menu</div>
-              <div className="flex flex-col gap-3"> 
-                <li className="text-gray-200 hover:text-gray-600 duration-300 cursor-pointer hover:underline">item</li>
-                <li className="text-gray-200 hover:text-gray-600 duration-300 cursor-pointer hover:underline">item</li>
-                <li className="text-gray-200 hover:text-gray-600 duration-300 cursor-pointer hover:underline">item</li>
-                <li className="text-gray-200 hover:text-gray-600 duration-300 cursor-pointer hover:underline">item</li>
-                <li className="text-gray-200 hover:text-gray-600 duration-300 cursor-pointer hover:underline">item</li>
-                <li className="text-gray-200 hover:text-gray-600 duration-300 cursor-pointer hover:underline">item</li>
-                <li className="text-gray-200 hover:text-gray-600 duration-300 cursor-pointer hover:underline">item</li>
-              </div>
+              <div className="text-gray-500">{menu}</div>
+              {items.map((item, key) => (
+                <div key = {key} className="flex flex-col gap-3"> 
+                  <li className="text-gray-200 hover:text-gray-600 duration-300 cursor-pointer hover:underline">{item}</li>  
+                </div>
+              ))}
+      
             </ul>
           </div>
         ))}
