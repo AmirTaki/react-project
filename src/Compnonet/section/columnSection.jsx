579cd42b4b617e26d18a59137a6308531ff209c4
amir <amirtaki623@gmail.com>
2025-09-17 22:10:00 +0330

    day 14 part 31

--- src/Compnonet/section/columnSection.jsx
+++ src/Compnonet/section/columnSection.jsx
@@ -2,7 +2,7 @@ const ColumnSection = () => {
   return (
     <div className="w-[90%] bg-blue-600 mx-auto h-[700px]">
       <div className="w-[100%] h-[100px] hover:h-[500px] duration-500 bg-red-500 overflow-hidden flex flex-row justify-center">
-        <div className="bg-green-400">
+        <div className="bg-green-400 w-50">
           <ul>menu
             <li>item</li>
             <li>item</li>
@@ -15,7 +15,7 @@ const ColumnSection = () => {
           </ul>
 
         </div>
-        <div className="bg-yellow-400">
+        <div className="bg-yellow-400 w-50">
           <ul>menu
             <li>item</li>
             <li>item</li>
@@ -28,7 +28,7 @@ const ColumnSection = () => {
           </ul>
 
         </div>
-        <div className="bg-pink-400">
+        <div className="bg-pink-400 w-50">
           <ul>menu
             <li>item</li>
             <li>item</li>
@@ -41,7 +41,7 @@ const ColumnSection = () => {
           </ul>
 
         </div>
-        <div className="bg-purple-600">
+        <div className="bg-purple-600 w-50">
           <ul>menu
             <li>item</li>
             <li>item</li>
