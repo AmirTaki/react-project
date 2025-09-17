4616284e45c3147c87d44e5d8b9e57d600b24ffa
amir <amirtaki623@gmail.com>
2025-09-17 22:03:53 +0330

    day 14 part 28

---
+++ src/Compnonet/section/columnSection.jsx
@@ -0,0 +1,9 @@
+const ColumnSection = () => {
+  return (
+    <div>
+      column section
+    </div>
+  )
+}
+
+export default ColumnSection;
--- src/Compnonet/section/section.jsx
+++ src/Compnonet/section/section.jsx
@@ -1,11 +1,11 @@
 import BackGroundSlider from "./backGroundSlider";
+import ColumnSection from "./columnSection";
 import GridImageBox from "./gridImageBox";
 import ImageAdvert from "./imageAdvert";
 import ImageSlider from "./imageSlider";
 import Popular from "./popular";
 import ResourcesImage from "./resourcesImage";
 import SliderPage from "./sliderPage";
-
 
 
 const Section = () => {
@@ -18,8 +18,7 @@ const Section = () => {
             <ImageAdvert />
             <Popular />
             <ResourcesImage />
-        
- 
+            <ColumnSection />
         </section>
 
     )
