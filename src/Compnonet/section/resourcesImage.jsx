89ffe501a57c1e23b5c2c0c722beeb7cf99b8d40
amir <amirtaki623@gmail.com>
2025-09-17 21:11:18 +0330

    day 14 part 21

--- src/Compnonet/section/resourcesImage.jsx
+++ src/Compnonet/section/resourcesImage.jsx
@@ -1,10 +1,18 @@
+import img1 from "../../assets/resurse1.avif"
+import img2 from "../../assets/resurse2.avif"
+import img3 from "../../assets/resurse3.avif"
+import img4 from "../../assets/resurse4.avif"
+import img5 from "../../assets/resurse5.avif"
+import img6 from "../../assets/resurse6.avif"
+import img7 from "../../assets/resurse7.avif"
+
 const ResourcesImage = () => {
     return(
         <>
            <div className="w-[90%] h-[700px] bg-blue-500  mx-auto">
                 <div className="w-[100%] h-[500px] bg-red-500 flex items-center   flex-col flex-wrap   overflow-scroll  justify-center">
                     <div className="w-[400px] h-[100%] bg-green-200">
-                        
+                        <img src="" alt="" />
                     </div>
                     <div className="w-[450px] h-[100%] bg-green-300"></div>
                     <div className="w-[450px] h-[100%] bg-green-400"></div>
