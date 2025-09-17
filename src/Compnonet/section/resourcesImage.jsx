54e371fdee33a7ca93adb7b782db3290e83e958d
amir <amirtaki623@gmail.com>
2025-09-17 21:02:05 +0330

    day 14 part 18

--- src/Compnonet/section/resourcesImage.jsx
+++ src/Compnonet/section/resourcesImage.jsx
@@ -1,9 +1,14 @@
 const ResourcesImage = () => {
     return(
         <>
-           <div className="w-[90%] h-[500px] bg-blue-500  mx-auto">
-            
+           <div className="w-[90%] h-[700px] bg-blue-500  mx-auto">
+                <div className="w-[100%] h-[500px] bg-red-500 flex items-center flex-col flex-wrap  justify-center">
+                    <div className="w-[300px] h-[150px] bg-green-200"></div>
+                </div>
             </div> 
+
+
+            
         </>
     )
 }
