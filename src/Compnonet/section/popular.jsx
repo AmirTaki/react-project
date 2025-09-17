a1758777b0c424f9d032a2f9a76ddddcbe9d1d36
amir <amirtaki623@gmail.com>
2025-09-17 16:53:01 +0330

    day 14 part 6

--- src/Compnonet/section/popular.jsx
+++ src/Compnonet/section/popular.jsx
@@ -5,8 +5,10 @@ const Popular = () => {
             <div className="bg-red-500" >
                 <h1>Popular right now</h1>
             </div>
-            <div className="bg-blue-500 grid grid-cols-3">
-                <div className="bg-gray-200 w-[140px] h-[10px]"></div>
+            <div className="bg-blue-500 grid grid-cols-3 h-[100px]">
+                <div className="bg-gray-200 w-[140px] h-[10px]">
+                    <h1>Ultraboost</h1>
+                </div>
                 <div className="bg-gray-300 w-[140px] h-[10px]"></div>
                 <div className="bg-gray-400 w-[140px] h-[10px]"></div>
                 <div className="bg-gray-500 w-[140px] h-[10px]"></div>
