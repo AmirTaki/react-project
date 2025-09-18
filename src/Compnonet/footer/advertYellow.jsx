13b9c5f2645679a7df4cb63c6827efc06e023267
amir <amirtaki623@gmail.com>
2025-09-18 05:06:29 +0330

    day 15 part 12

--- src/Compnonet/footer/advertYellow.jsx
+++ src/Compnonet/footer/advertYellow.jsx
@@ -2,10 +2,10 @@ const AdvertYellow = () => {
     return(
         <>
             <div className=" bg-[rgb(238,232,55)] h-[120px] w-[100%] flex justify-center items-center gap-7 ">
-                <div className="text-2xl ">
+                <div className="text-2xl font-bold">
                     <h2>JOIN OUR ADICLUB & GET 15% OFF.</h2>
                 </div>
-                <div className=" bg-black text-white w-100 h-10 flex items-center p-2">
+                <div className=" bg-black text-white w-100 h-16 flex items-center p-2 cursor-pointer">
                     <div>SIGN UP FOR FREE</div>
                     <div className=""><i className="bi bi-caret-right-fill"></i></div>
                 </div>
--- src/Compnonet/section/columnSection.jsx
+++ src/Compnonet/section/columnSection.jsx
@@ -35,7 +35,7 @@ const ColumnSection = () => {
     const Menus = document.querySelectorAll(".menusControler")[index]
     const itemsHight =  Menus.querySelectorAll(".itemConterole").length
     if(state.innerWidth) {
-      state.height[index] >= "0px" ?  dispatch({type : "hightItmes" , payload : {height : `0`, index : index}}) : dispatch({type : "hightItmes" , payload : {height : `${itemsHight*48}px`, index : index}})
+      state.height[index] >= "0px" ?  dispatch({type : "hightItmes" , payload : {height : `0`, index : index}}) : dispatch({type : "hightItmes" , payload : {height : `${itemsHight*45}px`, index : index}})
     }
   }
 
@@ -52,20 +52,20 @@ const ColumnSection = () => {
   return (
     <>
     <div className="w-[90%] bg-white mx-auto  flex max-md:flex-col md:h-[100px] overflow-hidden md:hover:h-[415px]  duration-300
-      md:justify-center md:gap-3 md:border-b-1! border-b-gray-300! mb-10
+      md:justify-center md:gap-16 md:border-b-1! border-b-gray-300! mb-10 
     ">
       {menus.map((menu, index) => (
         <div  
-          className="max-md:w-[100%] w-[150px] bg-transparent menusControler flex flex-col  ">
+          className="max-md:w-[100%] w-[150px] bg-transparent menusControler flex flex-col  md:items-center  ">
           <div  onClick={ () => {MenuHandler (index)}} className="flex items-center justify-between p-3 bg-transparent max-md:cursor-pointer">
             <div className="text-gray-600">{menu}</div>
             <i className={`${state.height[index] > "0px" ? "rotate-180" : ""} text-gray-500 bi bi-chevron-down duration-300 md:hidden `}></i>
           </div>
           <div 
             style={{height : state.height[index]}}
-            className="bg-transparent max-md:h-0  overflow-hidden  duration-300 flex flex-col  md:p-3  max-md:pl-9 gap-3 pt-2 max-md:gap-6!">
+            className="bg-transparent max-md:h-0  overflow-hidden  duration-300 flex flex-col  md:p-3  max-md:pl-9 gap-6 pt-2 max-md:gap-6!">
               {items.map((item, key) => (
-                <div key = {key} className="itemConterole w-fit  text-gray-400 hover:underline hover:text-gray-800 duration-200 cursor-pointer">{item}</div>
+                <div key = {key} className="itemConterole w-fit text-[14px] text-gray-400 hover:underline hover:text-gray-800 duration-200 cursor-pointer">{item}</div>
               ))}
           </div>
 
