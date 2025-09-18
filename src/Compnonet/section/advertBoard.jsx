c2a3024575b995eb6dd12acf4bb03dfb9f7f2ca2
amir <amirtaki623@gmail.com>
2025-09-18 04:47:32 +0330

    day 15 part 9

--- src/Compnonet/section/advertBoard.jsx
+++ src/Compnonet/section/advertBoard.jsx
@@ -12,6 +12,9 @@ const AdvertBoard = () => {
                 <div className=" w-[80%] mx-auto">
                     <p>Founded on performance, adidas sporting goods equipment supports athletes at all levels. Men, women and kids will find their best form in sneakers and activewear made to perform under pressure. adidas sportswear breathes, manages sweat and helps support working muscles. Explore sport-specific clothes and gear for basketball, soccer, or the yoga studio. Runners will find a range of sneakers for training, racing and trail runs. Gym users will find tops, tees and tanks that support focused efforts with adidas CLIMACOOL to feel cool and dry. Explore warm-ups featuring four-way stretch to support mobility. Find a new outdoor jacket that helps protect against wind and rain. Lace up new athletic shoes that energize every step with adidas Boost cushioning. With sizes and styles for all ages, we have sporting goods for the whole family. Dedicated training demands dedicated workout clothes. Experience the latest performance fabrics and sneaker technologies to get the most out of your next traini</p>
                 </div>
+                <div className="w-10 mx-auto">
+                    <p><i class="bi bi-brilliance"></i></p>
+                </div>
             </div>
         </>
     )
