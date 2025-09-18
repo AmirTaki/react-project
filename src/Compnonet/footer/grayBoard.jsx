2de374f72e473fca6dfe148f0f74995464192017
amir <amirtaki623@gmail.com>
2025-09-18 22:27:30 +0330

    day 15 part 33

--- src/Compnonet/footer/grayBoard.jsx
+++ src/Compnonet/footer/grayBoard.jsx
@@ -2,10 +2,10 @@ const GrayBoard = () => {
     return (
         <>
             <div className="bg-gray-600 h-[30px] flex justify-center items-center">
-                <div className=""></div>
-                <div className=""></div>
-                <div className=""></div>
-                <div className=""></div>
+                <div className="">Cookie Settings</div>
+                <div className="">Your Privacy Choices</div>
+                <div className="">Privacy Policy</div>
+                <div className="">Terms and Conditions</div>
 
             </div>
         </>
