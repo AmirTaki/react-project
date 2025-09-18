1230197465ad64f723737d2d3ead6c5a8702165e
amir <amirtaki623@gmail.com>
2025-09-18 16:52:40 +0330

    day 15 part 24

--- src/Compnonet/footer/footer.jsx
+++ src/Compnonet/footer/footer.jsx
@@ -1,12 +1,14 @@
 import AdvertYellow from "./advertYellow";
 import BlackBoard from "./blackBoard";
-
+import WhiteBoard from "./whiteBoard";
 
 const Footer = () => {
     return (
         <>
             <AdvertYellow />
             <BlackBoard />
+            <WhiteBoard />
+            
         </>
     )
 }
--- src/Compnonet/footer/whiteBoard.jsx
+++ src/Compnonet/footer/whiteBoard.jsx
@@ -1,8 +1,11 @@
 const WhiteBoard = () => {
     return(
         <>
-            <div className="w-[100%] h-[500px] bg-gray-500 max-lg:hidden">
-                
+            <div className="w-[100%] h-[500px] bg-gray-500 max-lg:hidden! flex items-center justify-evenly">
+                <div className="w-[10%] h-[400px] bg-green-400"></div>
+                <div className="w-[10%] h-[400px] bg-red-400"></div>
+                <div className="w-[10%] h-[400px] bg-blue-400"></div>
+                <div className="w-[10%] h-[400px] bg-pink-400"></div>
             </div>
         </>
     )
