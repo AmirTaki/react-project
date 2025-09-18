26e71d7864bc6e1af671ad7acd6ab73f508fd505
amir <amirtaki623@gmail.com>
2025-09-18 16:38:28 +0330

    day 15 part 20

--- src/Compnonet/footer/blackBoard.jsx
+++ src/Compnonet/footer/blackBoard.jsx
@@ -2,13 +2,13 @@ const BlackBoard = () => {
     return (
         <>
             <div className="w-[100%] h-[300px] bg-black flex  items-center justify-around overflow-hidden">
-                <div className="w-[100px] h-[250px] bg-red-500">
-                    <div className="">Help</div>
-                    <div className="">Returns & Exchanges</div>
-                    <div className="">Order Tracker</div>
-                    <div className="">Shipping</div>
-                    <div className="">Promotions</div>
-                    <div className="">Sitemap</div>
+                <div className="w-[150px] h-[250px] bg-tra text-white flex flex-col  gap-5 ">
+                    <div className="hover:text-gray-400 cursor-pointer duration-300">Help</div>
+                    <div className="hover:text-gray-400 cursor-pointer duration-300">Returns & Exchanges</div>
+                    <div className="hover:text-gray-400 cursor-pointer duration-300">Order Tracker</div>
+                    <div className="hover:text-gray-400 cursor-pointer duration-300">Shipping</div>
+                    <div className="hover:text-gray-400 cursor-pointer duration-300">Promotions</div>
+                    <div className="hover:text-gray-400 cursor-pointer duration-300">Sitemap</div>
                 </div>
                 <div className="w-[100px] h-[250px] bg-green-500"></div>
 
