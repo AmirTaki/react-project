ca98f1563a7d384c20c38001056fd3e842f0d937
amir <amirtaki623@gmail.com>
2025-09-18 17:08:42 +0330

    day 15 part 28

--- src/Compnonet/footer/whiteBoard.jsx
+++ src/Compnonet/footer/whiteBoard.jsx
@@ -3,9 +3,9 @@ const WhiteBoard = () => {
     const Items = ['Help', 'Returns & Exchanges', 'Shipping', 'Order Tracker', 'Store Locator', 'Size Charts', 'Gift Card Balance', 'How to Clean Shoes', 'Bra Fit Guide', 'Breathing for Running', 'Promotions', 'Sitemap']
     return(
         <>
-            <div className="w-[100%] h-[500px] bg-gray-500 max-lg:hidden! flex items-center justify-evenly">
+            <div className="w-[100%] h-[500px] bg-gray-500 max-lg:hidden! flex items-center justify-evenly ">
                 {Menus.map((menu, index) => (
-                    <div key = {index} className="w-[200px] h-[400px] bg-white overflow-hidden flex flex-col items-start gap-5">
+                    <div key = {index} className="w-[200px] h-[400px] bg-white overflow-hidden  flex flex-col items-start gap-5">
                         <div className="text-2xl font-bold  ">{menu}</div>
                         <div className="flex flex-col gap-3 text-gray-400 text-xl ">
                             <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 1</div>
