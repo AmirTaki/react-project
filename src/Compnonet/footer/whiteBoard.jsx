8bd55bdf114d2a8708a0365cec661fd45cca1f32
amir <amirtaki623@gmail.com>
2025-09-18 17:07:48 +0330

    day 15 part 27

--- src/Compnonet/footer/whiteBoard.jsx
+++ src/Compnonet/footer/whiteBoard.jsx
@@ -4,36 +4,23 @@ const WhiteBoard = () => {
     return(
         <>
             <div className="w-[100%] h-[500px] bg-gray-500 max-lg:hidden! flex items-center justify-evenly">
-                <div className="w-[250px] h-[400px] bg-white overflow-hidden flex flex-col items-start gap-5">
-                    <div className="text-3xl font-bold  ">Menus</div>
-                    <div className="flex flex-col gap-3 text-gray-400 text-xl ">
-                        <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 1</div>
-                        <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 2</div>
-                        <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 3</div>
-                        <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 4</div>
-                        <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 5</div>
+                {Menus.map((menu, index) => (
+                    <div key = {index} className="w-[200px] h-[400px] bg-white overflow-hidden flex flex-col items-start gap-5">
+                        <div className="text-2xl font-bold  ">{menu}</div>
+                        <div className="flex flex-col gap-3 text-gray-400 text-xl ">
+                            <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 1</div>
+                            <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 2</div>
+                            <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 3</div>
+                            <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 4</div>
+                            <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 5</div>
+                        </div>
                     </div>
-                </div>
-                <div className="w-[250px] h-[400px] bg-white overflow-hidden flex flex-col items-start gap-5">
-                    <div className="text-3xl font-bold  ">Menus</div>
-                    <div className="flex flex-col gap-3 text-gray-400 text-xl ">
-                        <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 1</div>
-                        <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 2</div>
-                        <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 3</div>
-                        <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 4</div>
-                        <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 5</div>
-                        <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 5</div>
-                        <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 5</div>
-                        <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 5</div>
-                        <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 5</div>
-                        <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 5</div>
-                    </div>
-                </div>
+                ))}
+             
+               
                 
                 
-                <div className="w-[10%] h-[400px] bg-red-400"></div>
-                <div className="w-[10%] h-[400px] bg-blue-400"></div>
-                <div className="w-[10%] h-[400px] bg-pink-400"></div>
+
             </div>
         </>
     )
