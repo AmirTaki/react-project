ebd38cc0e358f73d01e8c9f82dc8d18602a20496
amir <amirtaki623@gmail.com>
2025-09-18 22:37:18 +0330

    day 15 part 36

--- src/Compnonet/footer/grayBoard.jsx
+++ src/Compnonet/footer/grayBoard.jsx
@@ -2,7 +2,7 @@ const GrayBoard = () => {
     return (
         <>
             <div className="bg-gray-600 h-[35px] flex justify-center items-center text-white gap-10 text-[10px] 
-                max-md:grid! max-md:grid-cols-2! max-md:grid-rows-2! max-md:h-[70px] max-md:gap-0! max-lg:">
+                max-md:grid! max-md:grid-cols-2! max-md:grid-rows-2! max-md:h-[80px] max-md:gap-0! max-md:items- max-md:justify-items-center">
                 <div className="hover:text-gray-400 duration-300 cursor-pointer">Cookie Settings</div>
                 <div className="hover:text-gray-400 duration-300 cursor-pointer">Your Privacy Choices</div>
                 <div className="hover:text-gray-400 duration-300 cursor-pointer">Privacy Policy</div>
