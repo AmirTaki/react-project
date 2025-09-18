d61855a47e862428f471b1067698df999fb4b44e
amir <amirtaki623@gmail.com>
2025-09-18 16:45:08 +0330

    day 15 part 22

--- src/Compnonet/footer/blackBoard.jsx
+++ src/Compnonet/footer/blackBoard.jsx
@@ -1,7 +1,7 @@
 const BlackBoard = () => {
     return (
         <>
-            <div className="w-[100%] h-[330px] bg-black flex  items-center justify-evenly overflow-hidden">
+            <div className="lg:hidden! w-[100%] h-[330px] bg-black flex  items-center justify-evenly overflow-hidden">
                 <div className="w-[150px] h-[250px] bg-tra text-white flex flex-col  gap-5 ">
                     <div className="hover:text-gray-400 cursor-pointer duration-300">Help</div>
                     <div className="hover:text-gray-400 cursor-pointer duration-300">Returns & Exchanges</div>
@@ -18,6 +18,8 @@ const BlackBoard = () => {
                     <div className="hover:text-gray-400 cursor-pointer duration-300">Size Charts</div>
                 </div>
             </div>
+
+            <div className="w-[100%] h-[500px] bg-gray-500 max-lg:hidden"></div>
         </>
     )
 }
