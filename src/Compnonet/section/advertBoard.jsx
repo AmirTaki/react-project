9f6672df419fbdae49e59027ee98a3824be2074b
amir <amirtaki623@gmail.com>
2025-09-18 04:41:13 +0330

    day 15 part 7

--- src/Compnonet/section/advertBoard.jsx
+++ src/Compnonet/section/advertBoard.jsx
@@ -1,13 +1,16 @@
 const AdvertBoard = () => {
     return(
         <>
-            <div className="bg-black w-[100%] h-[500px] text-white flex flex-col p-10">
-                <div className="bg-amber-500">
+            <div className="bg-black w-[100%]  text-gray-300 flex flex-col p-10 gap-5 ">
+                <div className=" bg-red-300 text-3xl text-center">
                     <h1 className="">Sneakers, Activewear and Sporting Goods</h1>
                 </div>
 
-                <div className="bg-red-500">
+                <div className="bg-orange-400 w-[80%] mx-auto">
                     <p>Calling all athletes. Gear up for your favorite sport with adidas sneakers and activewear for men and women. From running to soccer and the gym to the trail, performance workout clothes and shoes keep you feeling your best. Find sport-specific sneakers to support your passion, and shop versatile activewear and accessories that support everyday comfort. adidas has you covered with world-class performance, quality and unmatched comfort to fit your style. Explore the full range of adidas gear today.</p>
+                </div>
+                <div className=" w-[80%] mx-au">
+                    <p>Founded on performance, adidas sporting goods equipment supports athletes at all levels. Men, women and kids will find their best form in sneakers and activewear made to perform under pressure. adidas sportswear breathes, manages sweat and helps support working muscles. Explore sport-specific clothes and gear for basketball, soccer, or the yoga studio. Runners will find a range of sneakers for training, racing and trail runs. Gym users will find tops, tees and tanks that support focused efforts with adidas CLIMACOOL to feel cool and dry. Explore warm-ups featuring four-way stretch to support mobility. Find a new outdoor jacket that helps protect against wind and rain. Lace up new athletic shoes that energize every step with adidas Boost cushioning. With sizes and styles for all ages, we have sporting goods for the whole family. Dedicated training demands dedicated workout clothes. Experience the latest performance fabrics and sneaker technologies to get the most out of your next traini</p>
                 </div>
             </div>
         </>
