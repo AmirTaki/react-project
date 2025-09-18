d71603abde6e21e9a145eb10851eb2305c0fd833
amir <amirtaki623@gmail.com>
2025-09-18 04:36:56 +0330

    day 15 part 6

--- src/Compnonet/section/advertBoard.jsx
+++ src/Compnonet/section/advertBoard.jsx
@@ -1,7 +1,15 @@
 const AdvertBoard = () => {
     return(
         <>
-            <div className="bg-black w-[100%] h-[500px]"></div>
+            <div className="bg-black w-[100%] h-[500px] text-white flex flex-col p-10">
+                <div className="bg-amber-500">
+                    <h1 className="">Sneakers, Activewear and Sporting Goods</h1>
+                </div>
+
+                <div className="bg-red-500">
+                    <p>Calling all athletes. Gear up for your favorite sport with adidas sneakers and activewear for men and women. From running to soccer and the gym to the trail, performance workout clothes and shoes keep you feeling your best. Find sport-specific sneakers to support your passion, and shop versatile activewear and accessories that support everyday comfort. adidas has you covered with world-class performance, quality and unmatched comfort to fit your style. Explore the full range of adidas gear today.</p>
+                </div>
+            </div>
         </>
     )
 }
