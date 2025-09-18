fb4de9c002f98c253b93d9ff076491ca1013776e
amir <amirtaki623@gmail.com>
2025-09-18 16:34:59 +0330

    day 15 part 18

--- src/Compnonet/footer/blackBoard.jsx
+++ src/Compnonet/footer/blackBoard.jsx
@@ -1,9 +1,9 @@
 const BlackBoard = () => {
     return (
         <>
-            <div className="w-[100%] h-[300px] bg-black flex  items-center justify-center">
-                <div className="w-[100px] h-[200px] bg-red-500"></div>
-                <div className="w-[100px] h-[200px] bg-green-500"></div>
+            <div className="w-[100%] h-[300px] bg-black flex  items-center justify-around overflow-hidden">
+                <div className="w-[100px] h-[250px] bg-red-500"></div>
+                <div className="w-[100px] h-[250px] bg-green-500"></div>
 
             </div>
         </>
