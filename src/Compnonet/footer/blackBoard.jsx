962069669ea92ff6b390eb95c0b27cd9af4cd321
amir <amirtaki623@gmail.com>
2025-09-18 16:40:35 +0330

    day 15 part 21

--- src/Compnonet/footer/blackBoard.jsx
+++ src/Compnonet/footer/blackBoard.jsx
@@ -1,7 +1,7 @@
 const BlackBoard = () => {
     return (
         <>
-            <div className="w-[100%] h-[300px] bg-black flex  items-center justify-around overflow-hidden">
+            <div className="w-[100%] h-[330px] bg-black flex  items-center justify-evenly overflow-hidden">
                 <div className="w-[150px] h-[250px] bg-tra text-white flex flex-col  gap-5 ">
                     <div className="hover:text-gray-400 cursor-pointer duration-300">Help</div>
                     <div className="hover:text-gray-400 cursor-pointer duration-300">Returns & Exchanges</div>
@@ -10,8 +10,13 @@ const BlackBoard = () => {
                     <div className="hover:text-gray-400 cursor-pointer duration-300">Promotions</div>
                     <div className="hover:text-gray-400 cursor-pointer duration-300">Sitemap</div>
                 </div>
-                <div className="w-[100px] h-[250px] bg-green-500"></div>
-
+                <div className="w-[150px] h-[250px] bg-tra text-white flex flex-col  gap-5 ">
+                    <div className="hover:text-gray-400 cursor-pointer duration-300">adiClub</div>
+                    <div className="hover:text-gray-400 cursor-pointer duration-300">Store Finder</div>
+                    <div className="hover:text-gray-400 cursor-pointer duration-300">Gift Cards</div>
+                    <div className="hover:text-gray-400 cursor-pointer duration-300">adidas Apps</div>
+                    <div className="hover:text-gray-400 cursor-pointer duration-300">Size Charts</div>
+                </div>
             </div>
         </>
     )
