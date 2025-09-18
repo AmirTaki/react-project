c09b781feabef8ca2592a48bf6c021bb9fa549c0
amir <amirtaki623@gmail.com>
2025-09-18 16:55:52 +0330

    day 15 part 25

--- src/Compnonet/footer/whiteBoard.jsx
+++ src/Compnonet/footer/whiteBoard.jsx
@@ -1,4 +1,6 @@
 const WhiteBoard = () => {
+    const Menus = ['PRODUCTS', 'SPORTS', 'COLLECTIONS', 'SUPPORT', 'COMPANY INFO']
+    const Items = ['Help', 'Returns & Exchanges', 'Shipping', 'Order Tracker', 'Store Locator', 'Size Charts', 'Gift Card Balance', 'How to Clean Shoes', 'Bra Fit Guide', 'Breathing for Running', 'Promotions', 'Sitemap']
     return(
         <>
             <div className="w-[100%] h-[500px] bg-gray-500 max-lg:hidden! flex items-center justify-evenly">
