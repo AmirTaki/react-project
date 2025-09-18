7b55b4eb4aedfd4c687c83ebb5aba72c6f0e3c5e
amir <amirtaki623@gmail.com>
2025-09-18 17:39:49 +0330

    day 15 part 29

--- src/Compnonet/footer/whiteBoard.jsx
+++ src/Compnonet/footer/whiteBoard.jsx
@@ -1,18 +1,17 @@
 const WhiteBoard = () => {
-    const Menus = ['PRODUCTS', 'SPORTS', 'COLLECTIONS', 'SUPPORT', 'COMPANY INFO']
+    // 'SUPPORT', 'COMPANY INFO'
+    const Menus = ['PRODUCTS', 'SPORTS', 'COLLECTIONS','SUPPORT', 'COMPANY INFO' ]
     const Items = ['Help', 'Returns & Exchanges', 'Shipping', 'Order Tracker', 'Store Locator', 'Size Charts', 'Gift Card Balance', 'How to Clean Shoes', 'Bra Fit Guide', 'Breathing for Running', 'Promotions', 'Sitemap']
     return(
         <>
-            <div className="w-[100%] h-[500px] bg-gray-500 max-lg:hidden! flex items-center justify-evenly ">
+            <div className="w-[100%] h-[100%] bg-r max-md:hidden! flex items-start justify-center gap-14 ">
                 {Menus.map((menu, index) => (
-                    <div key = {index} className="w-[200px] h-[400px] bg-white overflow-hidden  flex flex-col items-start gap-5">
-                        <div className="text-2xl font-bold  ">{menu}</div>
-                        <div className="flex flex-col gap-3 text-gray-400 text-xl ">
-                            <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 1</div>
-                            <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 2</div>
-                            <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 3</div>
-                            <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 4</div>
-                            <div className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">item 5</div>
+                    <div key = {index} className="w-[15%]     bg-white  my-20  flex flex-col   gap-5">
+                        <div className="text-xl font-bold  ">{menu}</div>
+                        <div className="flex flex-col gap-5 text-gray-400 text-md ">                      
+                            {Items.map((item, key) => (
+                                <div key = {key} className="hover:text-gray-600 duration-500 cursor-pointer hover:underline">{item}</div>
+                            ))}
                         </div>
                     </div>
                 ))}
