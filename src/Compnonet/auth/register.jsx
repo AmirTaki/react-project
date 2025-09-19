dc0205287f35137db2c4c9271400842dff0771ef
amir <amirtaki623@gmail.com>
2025-09-19 22:29:39 +0330

    validatio part 37

--- src/Compnonet/auth/register.jsx
+++ src/Compnonet/auth/register.jsx
@@ -5,8 +5,8 @@ import Home from "../../Home";
 const Register = () => {
     return (
         <>
-        {/* after:content-['★'] after:text-red-500 after:absolute after:top-0 after:right-0 */}
-            <header className="text-white flex flex-row justify-center items-center h-[50px] gap-10 bg-amber-700 ">
+
+            <header className="text-white flex flex-row justify-center items-center h-[50px] gap-10  ">
                 <Link to = "/">
                     <div
                         className="relative after:content-[''] after:left-0 after:-bottom-[6px] after:absolute after:w-[100%] after:h-[3px] after:bg-white 
@@ -14,9 +14,21 @@ const Register = () => {
                         "
                     >Home</div>
                 </Link>
-                <div>About</div>
-                <div>Services</div>
-                <div>Contact</div>
+                <div
+                    className="relative after:content-[''] after:left-0 after:-bottom-[6px] after:absolute after:w-[100%] after:h-[3px] after:bg-white 
+                        after:origin-right!  after:rounded-4xl after:transition-transform duration-500 after:scale-0 after:duration-500! hover:after:duration-500! hover:after:origin-left! hover:after:scale-100
+                        "
+                >About</div>
+                <div
+                    className="relative after:content-[''] after:left-0 after:-bottom-[6px] after:absolute after:w-[100%] after:h-[3px] after:bg-white 
+                        after:origin-right!  after:rounded-4xl after:transition-transform duration-500 after:scale-0 after:duration-500! hover:after:duration-500! hover:after:origin-left! hover:after:scale-100
+                        "
+                >Services</div>
+                <div
+                    className="relative after:content-[''] after:left-0 after:-bottom-[6px] after:absolute after:w-[100%] after:h-[3px] after:bg-white 
+                        after:origin-right!  after:rounded-4xl after:transition-transform duration-500 after:scale-0 after:duration-500! hover:after:duration-500! hover:after:origin-left! hover:after:scale-100
+                        "
+                >Contact</div>
             </header>
         </>
     )
