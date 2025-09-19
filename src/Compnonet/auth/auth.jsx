1cd0cf7913e438285b6fe976120fe4fdd8986b11
amir <amirtaki623@gmail.com>
2025-09-19 22:10:18 +0330

    validatio part 3

--- src/Compnonet/auth/auth.jsx
+++ src/Compnonet/auth/auth.jsx
@@ -3,7 +3,8 @@ import imgBackGround from "../../assets/background.jpg"
 
 const Validation = () => {
     return (
-        <div className="">
+     
+        <div className=" bg-[url('../../assets/background.jpg')] bg-cover bg-center h-64">
             Validation
             <Register />
             
