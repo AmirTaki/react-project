6f91be0cc7dbb732618f2098443ee633818bfe6b
amir <amirtaki623@gmail.com>
2025-09-19 22:08:38 +0330

    validatio part 2

--- src/Compnonet/auth/auth.jsx
+++ src/Compnonet/auth/auth.jsx
@@ -1,13 +1,13 @@
 import Register from "./register"
-
+import imgBackGround from "../../assets/background.jpg"
 
 const Validation = () => {
     return (
-        <>
+        <div className="">
             Validation
             <Register />
             
-        </>
+        </div>
     )
 }
 
