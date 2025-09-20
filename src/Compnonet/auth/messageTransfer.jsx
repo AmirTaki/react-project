7d6a6485a9538b0d5d29ce04555888f56bcae086
amir <amirtaki623@gmail.com>
2025-09-20 22:07:24 +0330

    validation part 152

--- src/Compnonet/auth/messageTransfer.jsx
+++ src/Compnonet/auth/messageTransfer.jsx
@@ -1,6 +1,17 @@
-const MessageTransfer = () => {
+const MessageTransfer = ({message, onClick, page}) => {
     return(
-        <></>
+        <>
+            <div className="flex justify-center items-center mx-auto w-[80%] my-5">
+                <div className="flex justify-center! items-center! text-[14px]">
+                    <p>{message} 
+                        <span
+                            className="text-[16px] pl-1 cursor-pointer hover:underline duration-100 font-[600]!"
+                            onClick={onClick}
+                        >{page}</span>
+                    </p>
+                </div>
+            </div>
+        </>
     )
 }
 
