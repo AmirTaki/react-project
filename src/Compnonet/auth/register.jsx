993c3db534d2bbf32565bbd4389146acaf54dc2e
amir <amirtaki623@gmail.com>
2025-09-20 03:44:20 +0330

    validation part 94

--- src/Compnonet/auth/register.jsx
+++ src/Compnonet/auth/register.jsx
@@ -68,7 +68,8 @@ const Register = () => {
             {/* section login */}
             <div className={`
                 ${state.page ? "scale-100 duration-300" : "scale-0 duration-300"}
-                relative w-[400px] h-[440px] bg-transparent border-2 border-[rgba(255,255,255,.5)]
+                ${state.registerPage  ?  'h-[520px]! duration-500' : ' h-[440px]! duration-500'}
+                relative w-[400px] bg-transparent border-2 border-[rgba(255,255,255,.5)]
                 rounded-[20px]  backdrop-blur-[20px] shadow-[0_0_30px_rgba(0,0,0,0.5)] flex items-center!
                 justify-center! overflow-hidden
                 `}
@@ -80,11 +81,128 @@ const Register = () => {
                 > 
                    <i className="bi bi-x-lg text-[22px]"></i>
                 </span>
-
-                <div className={`${state.registerPage ? "left-100 duration-500" : "left-0 duration-500"} w-[100%] h-[100%] absolute! top-12`}>
+                
+                {/* login */}
+                <div className={`${state.registerPage ? "left-100 duration-500" : "left-0 duration-500"} bg-red-500  w-[100%] h-[100%] absolute! top-12`}>
                     <h2 className="text-3xl text-[#162938] text-center">Login</h2>
                     <form action = "#">
                         {/* email login */}
+                        <div 
+                         
+                            className="bg-transparent flex flex-col items-center justify-center border-b-2! 
+                               border-b-[#162938]! mx-auto w-[80%] mt-6 mb-10 relative"
+                        >
+                            <div className="absolute right-2">
+                                <span><i className="bi bi-envelope"></i></span>
+                            </div>
+                            <input
+                                onChange={(e)=>{dispatch({type : "moveLabel", payload : {index : 0, bool : e.target.value.length > 0 ? true : false }})}}
+                                onFocus={() => {dispatch({type : "moveLabel", payload : {index : 0, bool : true}})}}
+                                onBlur={() => {dispatch({type : "moveLabel", payload : {index : 0, bool : false}})}}
+                                type="email"  id  = 'email' 
+                                className=" outline-0 border-0 text-[1.5em] text-[#162038] w-[100%] h-10 mb-1 "
+                            />
+
+                            <label 
+                                htmlFor="email"
+                                className={`${state.moveLabel[0] ? "-top-5!" : "top-2!"} transition-all  duration-300 z-30 cursor-pointer absolute left-1  `}
+                            >
+                                Email
+                            </label>                               
+                        </div>
+                        {/*  password login */}
+                        <div 
+                         
+                            className="bg-transparent flex flex-col items-center justify-center border-b-2! 
+                               border-b-[#162938]! mx-auto w-[80%] mt-10 relative"
+                        >
+                            <div className="absolute right-2">
+                                <span><i className="bi bi-lock"></i></span>
+                            </div>
+                            <input
+                                onChange={(e)=>{dispatch({type : "moveLabel", payload : {index : 1, bool : e.target.value.length > 0 ? true : false }})}}
+                                onFocus={() => {dispatch({type : "moveLabel", payload : {index : 1, bool : true}})}}
+                                onBlur={() => {dispatch({type : "moveLabel", payload : {index : 1, bool : false}})}}
+                                type={`${state.checkBox ? "text" : "password"}`}  id  = 'password' 
+                                className=" outline-0 border-0 text-[1.5em] text-[#162038] w-[100%] h-10 mb-1 "
+                            />
+
+                            <label 
+                                htmlFor="password"
+                                className={`${state.moveLabel[1] ? "-top-5!" : "top-2!"} transition-all  duration-300 z-30 cursor-pointer absolute left-1  `}
+                            >
+                                Password
+                            </label>                               
+                        </div>
+                        {/* checkbox login */}
+                        <div 
+                            className="flex flex-row  justify-between 
+                             mx-auto w-[80%] my-3 relative p-1 "
+                        >
+                            <div className="flex gap-1  ">
+                                <input
+                                    onChange={(e) => {dispatch({type : "checkBox", payload : e.target.checked})}}
+                                    type="checkbox"  id  = 'checkboxLogin' 
+                                    className="accent-[#162938]  "
+                                />
+
+                                <label 
+                                    htmlFor="checkboxLogin" className="text-[13px] font-thin! "
+                                >
+                                    Show Password
+                                </label>     
+                            </div>
+                            <div className="text-sm cursor-pointer hover:underline">Forget Passwrod ?</div>                                                    
+                        </div>
+                        {/* button login */}
+                        <div className="flex justify-center items-center mx-auto w-[80%] ">
+                            <input type="submig" value = 'Login' className="border-0 outline-0 bg-[#162938] text-center h-12 w-[100%] rounded-lg
+                            text-white cursor-pointer" />
+                        </div>
+                        {/* move login to register */}
+                        <div className="flex justify-center items-center mx-auto w-[80%] my-5">
+                            <div className="flex justify-center! items-center! text-[14px]">
+                                <p>Don't have an account? 
+                                    <span
+                                        className="text-[16px] pl-1 cursor-pointer hover:underline duration-100 font-[600]!"
+                                        onClick={()=>{dispatch({type :"registerPage", payload : true})}}
+                                    >Register</span>
+                                </p>
+                            </div>
+                        </div>
+                    </form>
+            
+                </div>
+                {/* ----- */}
+                {/* register */}
+                <div className={`${state.registerPage ? "left-0 duration-500" : "left-100 duration-500"} bg-green-500  w-[100%] h-[100%] absolute! top-12`}>
+                    <h2 className="text-3xl text-[#162938] text-center">Registration</h2>
+                    <form action = "#">
+                        {/* username Registration */}
+                        <div 
+                         
+                            className="bg-transparent flex flex-col items-center justify-center border-b-2! 
+                               border-b-[#162938]! mx-auto w-[80%] mt-6 mb-10 relative"
+                        >
+                            <div className="absolute right-2">
+                                <span><i className="bi bi-envelope"></i></span>
+                            </div>
+                            <input
+                                onChange={(e)=>{dispatch({type : "moveLabel", payload : {index : 0, bool : e.target.value.length > 0 ? true : false }})}}
+                                onFocus={() => {dispatch({type : "moveLabel", payload : {index : 0, bool : true}})}}
+                                onBlur={() => {dispatch({type : "moveLabel", payload : {index : 0, bool : false}})}}
+                                type="email"  id  = 'email' 
+                                className=" outline-0 border-0 text-[1.5em] text-[#162038] w-[100%] h-10 mb-1 "
+                            />
+
+                            <label 
+                                htmlFor="email"
+                                className={`${state.moveLabel[0] ? "-top-5!" : "top-2!"} transition-all  duration-300 z-30 cursor-pointer absolute left-1  `}
+                            >
+                                Email
+                            </label>                               
+                        </div>
+                        {/* email Registration */}
                         <div 
                          
                             className="bg-transparent flex flex-col items-center justify-center border-b-2! 
