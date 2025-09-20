c00300a50bc5272439219614b0c3de68075ed41a
amir <amirtaki623@gmail.com>
2025-09-20 15:51:58 +0330

    validation part 106

--- src/Compnonet/auth/register.jsx
+++ src/Compnonet/auth/register.jsx
@@ -20,17 +20,21 @@ const Register = () => {
                 const {number} =  action.payload
                 const {flag}  = action.payload
                 return {...state, checkBox : {[number] :flag } }
+           
+            case "inputValue" : 
+
+                return{...state, inputValue : {[action.payload.index] : action.payload.value}}
             
-            case "emailLogin":
-                return {...state, emailLogin : action.payload}
+            // case "emailLogin":
+            //     return {...state, emailLogin : action.payload}
             
-            case 'warningEmail':
-                return {...state, warningEmail : action.payload }
+            // case 'warningEmail':
+            //     return {...state, warningEmail : action.payload }
                 
-            case "passwordLogin" : 
-                return {...state, passwordLogin : action.payload}
-            case 'warningPassword':
-                return {...state, warningPassword : action.payload}
+            // case "passwordLogin" : 
+            //     return {...state, passwordLogin : action.payload}
+            // case 'warningPassword':
+            //     return {...state, warningPassword : action.payload}
 
         }
     } 
@@ -48,38 +52,42 @@ const Register = () => {
         // warningPassword : '',
     })
 
-    const checkEmail = () => {
-        if (state.emailLogin.length > 0  && !state.emailLogin.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
-            dispatch({type : 'warningEmail', payload : 'Write full Email'})
+    useEffect({
 
-        }
-        else {
-            dispatch({type : 'warningEmail', payload : ''}) 
-        }
-    }
+    },[state.inputValue])
 
-    const submitLogin = (e) => {
-        e.preventDefault()
-        state.warningEmail.length === 0 && state.emailLogin.length !== 0 ?  dispatch({type : 'warningEmail', payload : ''}) :  dispatch({type : 'warningEmail', payload : 'complete Email'})
-        state.warningPassword.length === 0 && state.passwordLogin.length !== 0  ? dispatch({type : "warningPassword", payload : ''}) : dispatch ({type : "warningPassword" , payload : 'complete password'} )  
+    // const checkEmail = () => {
+    //     if (state.emailLogin.length > 0  && !state.emailLogin.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
+    //         dispatch({type : 'warningEmail', payload : 'Write full Email'})
 
-    }
-    useEffect(() => {
-        checkEmail()
-    },[state.emailLogin])
+    //     }
+    //     else {
+    //         dispatch({type : 'warningEmail', payload : ''}) 
+    //     }
+    // }
 
-    const checkPassword = () => {
-        if(state.passwordLogin.length > 0 && !state.passwordLogin.match(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/)){
-            dispatch({type : 'warningPassword', payload : 'Password should contain' })
-        }
-        else {
-             dispatch({type : 'warningPassword', payload : ''}) 
-        }
-    }
+    // const submitLogin = (e) => {
+    //     e.preventDefault()
+    //     state.warningEmail.length === 0 && state.emailLogin.length !== 0 ?  dispatch({type : 'warningEmail', payload : ''}) :  dispatch({type : 'warningEmail', payload : 'complete Email'})
+    //     state.warningPassword.length === 0 && state.passwordLogin.length !== 0  ? dispatch({type : "warningPassword", payload : ''}) : dispatch ({type : "warningPassword" , payload : 'complete password'} )  
 
-    useEffect(() => {
-        checkPassword()
-    },[state.passwordLogin])
+    // }
+    // useEffect(() => {
+    //     checkEmail()
+    // },[state.emailLogin])
+
+    // const checkPassword = () => {
+    //     if(state.passwordLogin.length > 0 && !state.passwordLogin.match(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/)){
+    //         dispatch({type : 'warningPassword', payload : 'Password should contain' })
+    //     }
+    //     else {
+    //          dispatch({type : 'warningPassword', payload : ''}) 
+    //     }
+    // }
+
+    // useEffect(() => {
+    //     checkPassword()
+    // },[state.passwordLogin])
     return (
     <div className=" flex justify-center! items-center! flex-col gap-10 ">
             <header className="text-white flex flex-row justify-center items-center h-[80px] gap-10  ">
@@ -134,106 +142,9 @@ const Register = () => {
                    <i className="bi bi-x-lg text-[22px]"></i>
                 </span>
                 
-                {/* login */}
-                <div className={`${state.registerPage ? " right-100!  duration-500! scale-0!" : " scale-100!  right-0! duration-500"}   w-[100%] h-[100%] absolute! top-12`}>
-                    <h2 className="text-3xl text-[#162938] text-center">Login</h2>
-                    <form action = "#">
-                        {/* email login */}
-                        <div 
-                        
-                            className="bg-transparent flex flex-col items-center justify-center border-b-2! 
-                               border-b-[#162938]! mx-auto w-[80%] mt-6 mb-10 relative"
-                        >
-                            <div className="absolute right-2">
-                                <span><i className="bi bi-envelope"></i></span>
-                            </div>
-                            <input
-                                onChange = {(e) => {dispatch({type : 'emailLogin', payload : e.target.value})}}
-                                onFocus={() => {dispatch({type : "moveLabel", payload : {index : 0, bool : true}})}}
-                                onBlur={() => {dispatch({type : "moveLabel", payload : {index : 0, bool : false}})}}
-                                type="email"  id  = 'email' 
-                                className=" outline-0 border-0 text-[1.5em] text-[#162038] w-[100%] h-10 mb-1 "
-                            />
 
-                            <label 
-                                htmlFor="email"
-                                className={`${state.moveLabel[0] ? "-top-5!" : "top-2!"} transition-all  duration-300 z-30 cursor-pointer absolute left-1  `}
-                            >
-                                Email
-                            </label>   
-                            {/* warning email */}
-                            <span className="text-red-500 absolute right-0 -top-5">{state.warningEmail}</span>
-                        </div>
-                        {/*  password login */}
-                        <div 
-                         
-                            className="bg-transparent flex flex-col items-center justify-center border-b-2! 
-                               border-b-[#162938]! mx-auto w-[80%] mt-10 relative"
-                        >
-                            <div className="absolute right-2">
-                                <span><i className="bi bi-lock"></i></span>
-                            </div>
-                            <input
-                                onChange={(e) => {dispatch({type : "passwordLogin", payload : e.target.value})}}                               
-                                onFocus={() => {dispatch({type : "moveLabel", payload : {index : 1, bool : true}})}}
-                                onBlur={() => {dispatch({type : "moveLabel", payload : {index : 1, bool : false}})}}
-                                type={`${state.checkBox[0] ? "text" : "password"}`}  id  = 'password' 
-                                className=" outline-0 border-0 text-[1.5em] text-[#162038] w-[100%] h-10 mb-1 "
-                            />
 
-                            <label 
-                                htmlFor="password"
-                                className={`${state.moveLabel[1] ? "-top-5!" : "top-2!"} transition-all  duration-300 z-30 cursor-pointer absolute left-1  `}
-                            >
-                                Password
-                            </label>       
-                            {/* warning password */}
-                            <span className="text-red-500 absolute right-0 -top-5">{state.warningPassword}</span>                        
-                        </div>
-                        {/* checkbox login */}
-                        <div 
-                            className="flex flex-row  justify-between 
-                             mx-auto w-[80%] my-3 relative p-1 "
-                        >
-                            <div className="flex gap-1  ">
-                                <input
-                                    onChange={(e) => {dispatch({type : "checkBox", payload : {number : 0 , flag : e.target.checked}})}}
-                                    type="checkbox"  id  = 'checkboxLogin' 
-                                    className="accent-[#162938]  "
-                                />
-
-                                <label 
-                                    htmlFor="checkboxLogin" className="text-[13px] font-thin! "
-                                >
-                                    Show Password
-                                </label>     
-                            </div>
-                            <div className="text-sm cursor-pointer hover:underline">Forget Passwrod ?</div>                                                    
-                        </div>
-                        {/* button login */}
-                        <div className="flex justify-center items-center mx-auto w-[80%] ">
-                            <input
-                                onClick={submitLogin} 
-                                type="submit" value = 'Login' className="border-0 outline-0 bg-[#162938] text-center h-12 w-[100%] rounded-lg
-                                text-white cursor-pointer" 
-                            />
-                        </div>
-                        {/* move login to register */}
-                        <div className="flex justify-center items-center mx-auto w-[80%] my-5">
-                            <div className="flex justify-center! items-center! text-[14px]">
-                                <p>Don't have an account? 
-                                    <span
-                                        className="text-[16px] pl-1 cursor-pointer hover:underline duration-100 font-[600]!"
-                                        onClick={()=>{dispatch({type :"registerPage", payload : true})}}
-                                    >Register</span>
-                                </p>
-                            </div>
-                        </div>
-                    </form>
-            
-                </div>
-                {/* ----- */}
-                {/* register */}
+                     {/* register */}
                 <div className={`${state.registerPage ? "left-0 duration-500 scale-100!" : " scale-0! left-100 duration-500"}  w-[100%] h-[100%] absolute! top-12`}>
                     <h2 className="text-3xl text-[#162938] text-center">Registration</h2>
                     <form action = "#">
@@ -247,7 +158,7 @@ const Register = () => {
                                 <span><i className="bi bi-person"></i></span>
                             </div>
                             <input
-                                onChange={(e)=>{dispatch({type : "moveLabel", payload : {index : 2, bool : e.target.value.length > 0 ? true : false }})}}
+                                onChange={(e)=> {dispatch({type : 'inputValue', payload : {index : 2, value : e.target.value} })}}
                                 onFocus={() => {dispatch({type : "moveLabel", payload : {index : 2, bool : true}})}}
                                 onBlur={() => {dispatch({type : "moveLabel", payload : {index : 2, bool : false}})}}
                                 type="text"  id  = 'textRegister' 
@@ -259,7 +170,9 @@ const Register = () => {
                                 className={`${state.moveLabel[2] ? "-top-5!" : "top-2!"} transition-all  duration-300 z-30 cursor-pointer absolute left-1  `}
                             >
                                 Username
-                            </label>                               
+                            </label>      
+                            {/* warning username */}
+                            <span className="text-red-500 absolute right-0 -top-5">{state.inputWarning[2]}</span>
                         </div>
                         {/* email Registration */}
                         <div 
@@ -348,6 +261,106 @@ const Register = () => {
                     </form>
             
                 </div>
+                {/* login */}
+                <div className={`${state.registerPage ? " right-100!  duration-500! scale-0!" : " scale-100!  right-0! duration-500"}   w-[100%] h-[100%] absolute! top-12`}>
+                    <h2 className="text-3xl text-[#162938] text-center">Login</h2>
+                    <form action = "#">
+                        {/* email login */}
+                        <div 
+                        
+                            className="bg-transparent flex flex-col items-center justify-center border-b-2! 
+                               border-b-[#162938]! mx-auto w-[80%] mt-6 mb-10 relative"
+                        >
+                            <div className="absolute right-2">
+                                <span><i className="bi bi-envelope"></i></span>
+                            </div>
+                            <input
+                                // onChange = {(e) => {dispatch({type : 'emailLogin', payload : e.target.value})}}
+                                onFocus={() => {dispatch({type : "moveLabel", payload : {index : 0, bool : true}})}}
+                                onBlur={() => {dispatch({type : "moveLabel", payload : {index : 0, bool : false}})}}
+                                type="email"  id  = 'email' 
+                                className=" outline-0 border-0 text-[1.5em] text-[#162038] w-[100%] h-10 mb-1 "
+                            />
+
+                            <label 
+                                htmlFor="email"
+                                className={`${state.moveLabel[0] ? "-top-5!" : "top-2!"} transition-all  duration-300 z-30 cursor-pointer absolute left-1  `}
+                            >
+                                Email
+                            </label>   
+                            {/* warning email */}
+                            {/* <span className="text-red-500 absolute right-0 -top-5">{state.warningEmail}</span> */}
+                        </div>
+                        {/*  password login */}
+                        <div 
+                         
+                            className="bg-transparent flex flex-col items-center justify-center border-b-2! 
+                               border-b-[#162938]! mx-auto w-[80%] mt-10 relative"
+                        >
+                            <div className="absolute right-2">
+                                <span><i className="bi bi-lock"></i></span>
+                            </div>
+                            <input
+                                // onChange={(e) => {dispatch({type : "passwordLogin", payload : e.target.value})}}                               
+                                onFocus={() => {dispatch({type : "moveLabel", payload : {index : 1, bool : true}})}}
+                                onBlur={() => {dispatch({type : "moveLabel", payload : {index : 1, bool : false}})}}
+                                type={`${state.checkBox[0] ? "text" : "password"}`}  id  = 'password' 
+                                className=" outline-0 border-0 text-[1.5em] text-[#162038] w-[100%] h-10 mb-1 "
+                            />
+
+                            <label 
+                                htmlFor="password"
+                                className={`${state.moveLabel[1] ? "-top-5!" : "top-2!"} transition-all  duration-300 z-30 cursor-pointer absolute left-1  `}
+                            >
+                                Password
+                            </label>       
+                            {/* warning password */}
+                            {/* <span className="text-red-500 absolute right-0 -top-5">{state.warningPassword}</span>                         */}
+                        </div>
+                        {/* checkbox login */}
+                        <div 
+                            className="flex flex-row  justify-between 
+                             mx-auto w-[80%] my-3 relative p-1 "
+                        >
+                            <div className="flex gap-1  ">
+                                <input
+                                    onChange={(e) => {dispatch({type : "checkBox", payload : {number : 0 , flag : e.target.checked}})}}
+                                    type="checkbox"  id  = 'checkboxLogin' 
+                                    className="accent-[#162938]  "
+                                />
+
+                                <label 
+                                    htmlFor="checkboxLogin" className="text-[13px] font-thin! "
+                                >
+                                    Show Password
+                                </label>     
+                            </div>
+                            <div className="text-sm cursor-pointer hover:underline">Forget Passwrod ?</div>                                                    
+                        </div>
+                        {/* button login */}
+                        <div className="flex justify-center items-center mx-auto w-[80%] ">
+                            <input
+                                // onClick={submitLogin} 
+                                type="submit" value = 'Login' className="border-0 outline-0 bg-[#162938] text-center h-12 w-[100%] rounded-lg
+                                text-white cursor-pointer" 
+                            />
+                        </div>
+                        {/* move login to register */}
+                        <div className="flex justify-center items-center mx-auto w-[80%] my-5">
+                            <div className="flex justify-center! items-center! text-[14px]">
+                                <p>Don't have an account? 
+                                    <span
+                                        className="text-[16px] pl-1 cursor-pointer hover:underline duration-100 font-[600]!"
+                                        onClick={()=>{dispatch({type :"registerPage", payload : true})}}
+                                    >Register</span>
+                                </p>
+                            </div>
+                        </div>
+                    </form>
+            
+                </div>
+                {/* ----- */}
+           
             </div>
         </div>
     )
