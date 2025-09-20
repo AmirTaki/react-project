3bef69f127b865d141550c0f926411f24c6391ea
amir <amirtaki623@gmail.com>
2025-09-20 21:39:59 +0330

    validation part 144

--- src/Compnonet/auth/checkBoxComponent.jsx
+++ src/Compnonet/auth/checkBoxComponent.jsx
@@ -1,6 +1,27 @@
-const CheckComponent = () => {
+const CheckComponent = ({id, onChange, label}) => {
     return (
-        <></>
+        <>
+            {/* checkbox Registration */}
+            <div 
+                className="flex flex-row  justify-between 
+                mx-auto w-[80%] my-3 relative p-1 "
+            >
+                <div className="flex gap-1  ">
+                    <input
+                        onChange={onChange}
+                        type="checkbox"  id  = {id} 
+                        className="accent-[#162938]  "
+                    />
+
+                    <label 
+                        htmlFor={id} className="text-[13px] font-thin! "
+                    >
+                       {label}
+                    </label>     
+                </div>
+            </div>
+        </>    
+   
     )
 }
 
