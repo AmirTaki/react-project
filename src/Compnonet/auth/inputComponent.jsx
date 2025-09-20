3e7cf3107e9c88e9a41194a13172e4ec5a185666
amir <amirtaki623@gmail.com>
2025-09-20 21:24:11 +0330

    validation part 139

--- src/Compnonet/auth/inputComponent.jsx
+++ src/Compnonet/auth/inputComponent.jsx
@@ -1,6 +1,33 @@
-const InputComponent = () => {
+const InputComponent = ({icon ,onChange, onFocus, onBlur,  type, id , MoveLabel, inputStatus, inputWarning }) => {
     return (
-        
+        <>
+        {/* username Registration */}
+            <div 
+            
+                className="bg-transparent flex flex-col items-center justify-center border-b-2! 
+                border-b-[#162938]! mx-auto w-[80%] mt-6 mb-10 relative"
+            >
+                <div className="absolute right-2">
+                    <span><i className={icon}></i></span>
+                </div>
+                <input
+                    onChange={onChange}
+                    onFocus={onFocus}
+                    onBlur={onBlur}
+                    type={type}  id  = {id} 
+                    className=" outline-0 border-0 text-[1.5em] text-[#162038] w-[100%] h-10 mb-1 bg-transparent! "
+                />
+
+                <label 
+                    htmlFor={id}
+                    className={`${MoveLabel ? "-top-5!" : "top-2!"} transition-all  duration-300 z-30 cursor-pointer absolute left-1  `}
+                >
+                    Username
+                </label>      
+                {/* warning username */}
+                <span className={`${inputStatus ? 'text-blue-500' : "text-red-500"} absolute right-0 -top-5`}>{ inputWarning}</span>
+            </div>
+        </>
     )
 }
 
