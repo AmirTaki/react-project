7e2c38f503bc5cef209c45d06d9f4e83bfd90673
amir <amirtaki623@gmail.com>
2026-01-27 16:35:29 +0330

    complete handlerMouse & touch in imageAdvert

--- src/Compnonet/section/imageAdvert.jsx
+++ src/Compnonet/section/imageAdvert.jsx
@@ -16,6 +16,19 @@ const ImageAdvert = () => {
   
 
     const reducer = (state, action) => {
+
+        const checkResize = () => {
+          if(window.innerWidth < 640){
+            return listImg.length - 1
+          }
+          else if(window.innerWidth >= 640 && window.innerWidth < 1024){
+            return Math.ceil(listImg.length /  2 ) - 1
+          }
+          else if (window.innerWidth >= 1204){
+            return  Math.ceil(listImg.length /  4 ) - 1
+          }
+    }
+    
         switch(action.type) {
             case "conter" : 
                 return {...state, conter : 0}
@@ -67,14 +80,17 @@ const ImageAdvert = () => {
 
             case "mouseUp": 
                 if(state.isDrag){
-                    console.log(state.diff)
                     if(state.diff < 0){
-                        state.conter += .5
+                        if(state.conter < checkResize() ){
+                            state.conter += .5
+                        }
                     }
                     else if (state.diff > 0){
-                        state.conter -= .5
+                        if(state.conter > 0){
+                            state.conter -= .5
+                        } 
                     }
-                    console.log(state.conter)
+                    // console.log(state.conter, checkResize())
                     return {...state, isDrag: false, diff: 0, startScroll: 0, startX: 0}
                 }
                 return {...state}
@@ -113,6 +129,7 @@ const ImageAdvert = () => {
     }
 
     useEffect(() => {
+        console.log(window.innerWidth)
         const handlerSlide = () => {
             dispatch({type : "conter"})
         }
@@ -123,17 +140,19 @@ const ImageAdvert = () => {
         }
     },[])
 
-    
+
     return(
         <>
 
-        <div className="flex h-[500px] w-[100%] bg-white relative! items-center justify-center">
+        <div className="flex h-[500px] w-[100%] bg-white relative! items-center justify-center ">
             <div className="h-[500px] w-[50%]  max-lg:hidden p-[2px] ">
                 <img src={listImg[state.imgSelect]} className="h-[100%]! duration-200 w-[100%]" alt="" />
             </div>
             
+
+            {/* max--lg: 1024 */}
             <div 
-                ref = {refSlider} className="h-[500px] w-[50%] max-lg:w-[100%]  flex flex-col flex-wrap overflow-hidden cursor-grab active:cursor-grabbing touch-pan-y"
+                ref = {refSlider} className="h-[500px] w-[50%] max-lg:w-[100%]  flex flex-col flex-wrap overflow-hidden  touch-pan-y select-none cursor-grab! active:cursor-grabbing!"
               
                 onMouseDown={(e) => {dispatch({type: 'mouseDown', payload: {event: e.clientX, slider: refSlider.current}})}}
                 onMouseMove={(e) => {dispatch({type: 'mouseMove', payload: {event: e.clientX, slider: refSlider.current}})}}
@@ -146,7 +165,7 @@ const ImageAdvert = () => {
 
             >
                 {listImg.map(((item, index) => (
-                    <div key = {index} className="h-[50%] w-[50%] max-lg:h-[100%] max-sm:w-[100%] relative p-[2px] max-sm:p-0  ">
+                    <div key = {index} className="h-[50%] w-[50%] max-lg:h-[100%] max-sm:w-[100%] relative p-[2px] max-sm:p-0   ">
                         <img draggable = {false} onClick={()=>{clickImage(index)}}  src={item} className="h-[100%]! w-[100%] lg:hover:grayscale-75 lg:hover:scale-110 lg:cursor-pointer cursor-default duration-500 " alt="" />
                         <div className=" absolute  bg-[rgba(0,0,0,0.2)]  bottom-0 left-0 p-3 flex flex-col m-[3px] max-sm:m-0 ">
                             <h6 className="text-sm text-gray-400">{index + 1}-ROG Travel books another killer vacation</h6>
