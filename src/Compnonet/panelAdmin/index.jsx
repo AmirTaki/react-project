fd3cbae344889062286c265a902b483238d9952e
amir <amirtaki623@gmail.com>
2026-02-18 23:49:55 +0330

    panelAdmin checkSection

--- src/Compnonet/panelAdmin/index.jsx
+++ src/Compnonet/panelAdmin/index.jsx
@@ -7,25 +7,30 @@ const PanelAdmin = () => {
     const navigate = useNavigate();
 
 
-    useEffect(() =>  {
-        try{
-            axios.get("http://localhost/project-react-Combination/back-end//functions//check_session.php").then((res)=> {
-                console.log(res.data)
-                setSesstion(res.data)
-                if(session === false){
-                    navigate('/validation');
-                }
-            })
-        }
-        catch(err){
-            if(err.message == "Request failed with status code 300"){
-                setSesstion(false)
-                
-            }
-            // console.error("warning",err.message)
-        }
+axios.defaults.withCredentials = true;
 
-    }, [])
+// یا در زمان درخواست
+axios.get('http://localhost/project/check_session.php', {
+    withCredentials: true
+})
+
+    // useEffect(() =>  {
+    //     try{
+    //         axios.defaults.withCredentials = true;
+    //         axios.get("http://localhost/project-react-Combination/back-end//functions//check_session.php").then((res)=> {
+    //             withCredentials: true
+    //             setSesstion(res.data)
+                
+    //             if(session === false){
+    //                 navigate('/validation');
+    //             }
+    //         })
+    //     }
+    //     catch(err){
+    //         setSesstion(false)
+    //     }
+
+    // }, [])
 
     return(
         
