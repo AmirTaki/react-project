028d9e0902fbca69d1260740a0ed893cee71a4d6
amir <amirtaki623@gmail.com>
2026-02-20 15:48:18 +0330

    header panelAdmin

--- src/Compnonet/panelAdmin/header/header.jsx
+++ src/Compnonet/panelAdmin/header/header.jsx
@@ -1,7 +1,29 @@
+import { FaReact } from "react-icons/fa";
+import { FaPhp } from "react-icons/fa";
+import { FaHome } from "react-icons/fa";
+import { BiLogOut } from "react-icons/bi";
+import { CiLogin } from "react-icons/ci";
 const HeaderPanelAdmin = () => {
     return (
-        <div className="bg-[blue] h-20 flex items-center">
-            header
+        <div className=" h-20 flex items-center border-b border-b-[#7c7c7c]! text-4xl justify-between px-8">
+            <div className="flex gap-3 ">
+                <FaReact />
+                <FaPhp />
+            </div>
+
+
+            <div className="">
+                panel admin
+            </div>
+
+
+            <div className="flex  gap-3">
+                <FaHome />
+                <BiLogOut />
+                <CiLogin />
+            </div>
+
+
         </div>
     )
 }
