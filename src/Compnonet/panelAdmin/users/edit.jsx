62d1b0171cd16ce7ccb3f50bb1cf3d79b7e7841f
amir <amirtaki623@gmail.com>
2026-02-28 03:17:59 +0330

    edit user in panelAdmin complete

--- src/Compnonet/panelAdmin/users/edit.jsx
+++ src/Compnonet/panelAdmin/users/edit.jsx
@@ -4,10 +4,8 @@ import api from "../../../axiosConfig";
 import { useNavigate, useParams } from "react-router-dom";
 
 const EditUsers = () => {
-    // const [users, setUsers] =  useState([])
     const {id} = useParams();
     const navigate =  useNavigate()
-
 
     const reducer = (state, action) => {
         switch(action.type){
@@ -17,11 +15,15 @@ const EditUsers = () => {
                     ...state,
                     username: action.payload.name,
                     email: action.payload.email,
-                    password: ""
-                }
+                    emailOld: action.payload.email,
+                    password: '',
+            }
 
             case "showPassword":
                 return {...state, showPassowrd : action.payload.target.checked}
+
+            case "editPassword": 
+                return {...state, editPassword : !action.payload.target.checked}
 
             case "username": 
                 return {...state, username : action.payload}
@@ -47,24 +49,24 @@ const EditUsers = () => {
     const [state, dispatch] = useReducer(reducer, {
         showPassowrd: false, 
         username : '',
+        emailOld: '',
         email : '', 
         password: '',
         usernameWarning: '',
         emailWarning: '',
-        passwordWarning: ''
+        passwordWarning: '',
+        editPassword : true,
     })
     
     const getUsers = async () => {
         // 
         try{
-            await api.get(`tables/users/edit.php/${id}`, {withCredentials: true}).then((res) => {
+            await api.get(`tables/users/reading.php/${id}`, {withCredentials: true}).then((res) => {
                 // const data = Array.isArray(res.data) ? res.data : [];
-                // setUsers(res.data);
                 dispatch({type : 'GetUsers', payload: {name : res.data.name, email : res.data.email, password: res.data.password}})
             }) 
         }
         catch(err){
-            
             console.error("message: ", err.message);
             navigate('/PanelAdmin/UsersPanelAdmin')
         }
@@ -73,18 +75,41 @@ const EditUsers = () => {
     useEffect(() => {getUsers()}, [])
 
     const editAccount = async (event) => {
+    
         event.preventDefault();
+
+        dispatch({type : 'warning', payload: { name: '', email: '',  password: ''}})
+
         try{
-            await api.put(`tables/users/edit.php/${id}`, state).then((res) =>{
-                res.data
+            await api.put(`tables/users/edit.php/${id}`, state).then((res) =>{  
+                res.data;  
+                navigate('/PanelAdmin/UsersPanelAdmin')
             })
         }
         catch(error){
-            console.error("خطا در شبکه یا CORS:", error.message);
+            if(error.message == "Request failed with status code 300"){
+                dispatch({type : 'warning', payload: {
+                    name: 'Write full Name',
+                    email: 'Write full Email', 
+                    password: 'Password should contain'
+                }})
+            }
+            else if(error.message == "Request failed with status code 400"){
+                dispatch({type : 'warning', payload: {
+                    name: 'Write full Name',
+                    email: 'Write full Email', 
+                    password: ''
+                }})
+            }
+            else if(error.message == "Request failed with status code 409"){
+                dispatch({type : 'warning', payload: {
+                    name: '',
+                    email: 'Email repeat ! please change email ??', 
+                    password: ''
+                }})
+            }
         }
     }
-
-
 
     return(
         <div className="">
@@ -107,7 +132,9 @@ const EditUsers = () => {
                             ></input>
                         </div>
                         <div className="text-gray-500 py-5">message:
-                            <span className="text-red-600 px-2"></span>
+                            <span className="text-red-600 px-2">
+                                {state.usernameWarning}
+                            </span>
                         </div>
 
                         <hr className="my-8"/>
@@ -122,7 +149,9 @@ const EditUsers = () => {
                             ></input>
                         </div>
                         <div className="text-gray-500 py-5">message:
-                            <span className="text-red-600 px-2"></span>
+                            <span className="text-red-600 px-2">
+                                {state.emailWarning}
+                            </span>
                         </div>
                 
                         <hr className="my-8"/>
@@ -130,6 +159,7 @@ const EditUsers = () => {
                         <div className="flex gap-5 items-center justify-center ">
                             <label htmlFor="password" className="text-red-500">password</label>
                             <input 
+                                disabled = {state.editPassword}
                                 value={state.password} 
                                 type = {state.showPassowrd ? 'text' : 'password'} 
                                 id = "password" className="border-2 w-[300px] rounded-md h-10 p-2"
@@ -137,18 +167,32 @@ const EditUsers = () => {
                             >        
                             </input>
                         </div>
-                        {/* checkbox show hidden password */}
-                        <div className="flex gap-2 items-center justify-left  pt-8">
-                            <label htmlFor = "showpass"> 
-                                {state.showPassowrd ? "hidden password" : "show password"}
-                            </label>
-                            <input  id = "showpass" type="checkbox" 
-                                onChange={(e) => {dispatch({type: "showPassword", payload: e})}}
-                            />
+
+                        {/* check box */}
+                        <div className="flex items-center justify-between">
+                            {/* checkbox show hidden password */}
+                            <div className="flex gap-2 items-center justify-left  pt-8">
+                                <label htmlFor = "showpass"> 
+                                    {state.showPassowrd ? "hidden password" : "show password"}
+                                </label>
+                                <input  id = "showpass" type="checkbox" 
+                                    onChange={(e) => {dispatch({type: "showPassword", payload: e})}}
+                                />
+                            </div>
+
+                            {/* edit password */}
+                            <div className="flex gap-2 items-center justify-left  pt-8">
+                                <label htmlFor = "editpassword"> 
+                                    edit password
+                                </label>
+                                <input  id = "editpassword" type="checkbox" 
+                                    onChange={(e) => {dispatch({type: "editPassword", payload: e})}}
+                                />
+                            </div>
                         </div>
 
                         <div className="text-gray-500 py-5">message:
-                            {/* <span className="text-red-600 px-2">{state.passwordWarning}</span> */}
+                            <span className="text-red-600 px-2">{state.passwordWarning}</span>
                         </div>
 
 
@@ -157,7 +201,8 @@ const EditUsers = () => {
                         <div className="flex justify-center items-center">
                             <input 
                                 onClick={(event) => {editAccount(event)}}
-                                type="submit" 
+                                type="submit"
+                                value = "update" 
                                 className="border-2 px-4 py-2 rounded-xl cursor-pointer hover:text-green-600 duration-300 hover:border-green-600" 
                             />
                         </div>
