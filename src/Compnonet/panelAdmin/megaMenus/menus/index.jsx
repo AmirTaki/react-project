a326f7ec14f2a091a5d2f62c214a28f34a8c27f7
amir <amirtaki623@gmail.com>
2026-02-28 15:35:13 +0330

    panelAdmin -> mgaMenus -> menus : getMnuses() & useEffect

--- src/Compnonet/panelAdmin/megaMenus/menus/index.jsx
+++ src/Compnonet/panelAdmin/megaMenus/menus/index.jsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from "react";
+import api from "../../../../axiosConfig";
 import HeaderPanelAdmin from "../../header/header";
 
 const MenuHeaderPanelAdmin = () => {
+    const [menus, setMenus] =  useState([]);
+    const getMenus = async () => {
+        try{
+            await api.get('tables/megaMenu/menus/menu.php', {withCredentials: true}).then((res) => {
+                const data = Array.isArray(res.data) ? res.data : [];
+                setMenus(data);
+            })
+        }
+        catch(err){
+            console.error('message: ', err);
+        }
+    }
+    useEffect(() => {getMenus()}, [])
     return(
         <>
             <HeaderPanelAdmin />
