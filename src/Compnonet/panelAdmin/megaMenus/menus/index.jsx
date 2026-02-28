6873fb42175fc24109bdc421c7eb3fc696c3cf2b
amir <amirtaki623@gmail.com>
2026-02-28 15:39:31 +0330

    panelAdmin -> mgaMenus -> menus : menus map && infromation

--- src/Compnonet/panelAdmin/megaMenus/menus/index.jsx
+++ src/Compnonet/panelAdmin/megaMenus/menus/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import api from "../../../../axiosConfig";
 import HeaderPanelAdmin from "../../header/header";
+import { Link } from "react-router-dom";
 
 const MenuHeaderPanelAdmin = () => {
     const [menus, setMenus] =  useState([]);
@@ -20,7 +21,63 @@ const MenuHeaderPanelAdmin = () => {
         <>
             <HeaderPanelAdmin />
             <div className=" top-20 absolute w-full  min-h-screen bg-[#252525]!  text-white z-10">
-                MenuHeaderPanelAdmin
+            <div className="flex flex-col items-center justify-around ">
+                <Link to = "/">
+                    <button className="mt-10 border-0 flex items-center justify-center text-green-500  text-lg hover:tracking-[.2rem] duration-200">create menu</button>
+                </Link>
+                
+
+                <h1 className="text-center my-5 text-3xl hover:tracking-[.3rem] duration-200"> Menu Header table</h1> 
+            </div>
+    
+        <div className="w-[90%]  mx-auto flex flex-col">
+            <table className="w-full">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Title</th>
+                        <th>status</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {menus.map((menu, ind) => { 
+                        return(
+                            <tr key = {ind}>
+                                <th>{menu.id}</th>
+                                <th>{menu.title}</th>
+                                <th className = {`${menu.status == 10 ? "text-green-400" : 'text-rose-400' }`}>
+                                    {menu.status == 10 ? 'enable' : 'disable'}
+                                </th>
+
+                                <th className="flex justify-center items-center gap-7! max-md:flex-col max-md:gap-1!" >
+                                    
+                                    <div 
+                                        // onClick={() => {chageStatus(user)}}
+                                        className="text-yellow-500 cursor-pointer duration-200 hover:text-yellow-300 "
+                                    >
+                                        change status
+                                    </div>
+
+                                    <Link to = {`/PanelAdmin/users/editUser/${menu.id}/edit`}>
+                                        <div className="text-sky-500 cursor-pointer duration-200 hover:text-blue-700 ">edit</div>
+                                    </Link>
+                                    
+                                    <div 
+                                        // onClick={() => {deleteUser(user)}}
+                                        className="text-rose-500 cursor-pointer duration-200 hover:text-red-700!"
+                                    >
+                                        delete
+                                    </div>
+                                </th>
+                            </tr>
+                        )
+                    })}
+
+    
+                </tbody>
+            </table>
+        </div>
             </div>
         </>
 
