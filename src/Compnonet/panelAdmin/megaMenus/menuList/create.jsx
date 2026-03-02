56a53fb68c2f43d67694dea71350c1ef7440f9c9
amir <amirtaki623@gmail.com>
2026-03-02 14:34:00 +0330

    panelAdmin -> mageMenus -> menuList ->create.jsx

--- src/Compnonet/panelAdmin/megaMenus/menuList/create.jsx
+++ src/Compnonet/panelAdmin/megaMenus/menuList/create.jsx
@@ -4,13 +4,14 @@ const CreateMegaMenuList = () => {
     return (
         <div className="">
             <HeaderPanelAdmin  id = {3}/>
+            <div className=" top-20 absolute w-full  min-h-screen bg-[#252525]!  text-white z-10">
                 <div className="flex flex-col justify-center items-center">
                     <h1 className="text-4xl my-5 hover:tracking-[.4rem] duration-200 ">ADD ITEM</h1>
 
                     <form>
                         {/* title */}
                         <div className="flex gap-5 items-center justify-center">
-                            <label htmlFor="name" className="text-blue-500">title</label>
+                            <label htmlFor="name" className="text-blue-500">list</label>
                             <input 
                                 // value = {state.title}
                                 type="text" id = "name" className="border-2 w-[300px] rounded-md h-10 p-2"
@@ -24,16 +25,35 @@ const CreateMegaMenuList = () => {
                         </div>
 
                         <hr className="my-8"/>
+                        <div className="flex gap-5 items-center justify-center">
+                            
+                            <label htmlFor="name" className="text-blue-500">title</label>
+                            <select className="bg-[#252525]!  text-white border-2 w-[300px] rounded-md h-10 p-2 ">
+                                <option>one</option>
+                                <option>two</option>
+                                <option>three</option>
+                            </select>
+                        </div> 
+                        
+                        <div className="text-gray-500 py-5">message:
+                            <span className="text-red-600 px-2">
+                                {/* {state.warning} */}
+                            </span>
+                        </div>
+                     
+                            
+                        <hr className="my-8"/>
                         <div className="flex justify-center items-center">
                             <input 
                                 // onClick={(event) => {saveTitle(event)}}
-                                type="submit" value = "save" 
+                                type="submit" value = "ADD" 
                                 className="border-2 px-4 py-2 rounded-xl cursor-pointer hover:text-green-600 duration-300 hover:border-green-600" 
                             />
                         </div>
 
                     </form>
                 </div>
+            </div>
         </div>
     )
 }
