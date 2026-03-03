b01d717f87c24914e6a62cf13b639d2b9e7a48e6
amir <amirtaki623@gmail.com>
2026-03-03 15:48:56 +0330

    panelAdmin -> megaMenus => menuCategory -> index.jsx => getRequest

--- src/Compnonet/panelAdmin/megaMenus/menuCategorty/index.jsx
+++ src/Compnonet/panelAdmin/megaMenus/menuCategorty/index.jsx
@@ -1,12 +1,88 @@
+import { Link } from "react-router-dom"
 import HeaderPanelAdmin from "../../header/header"
+import { useEffect, useState } from "react"
+import api from "../../../../axiosConfig"
 
 
 const MegaMenuCategoryPanelAdmin = () => {
+    const [category, setCategory] = useState([])
+    const getCategoryMegaMenu = async () => {
+        try{
+            await api.get('', {withCredentials: true}).then((res) => {
+               const data =  Array.isArray(res.data) ? res.data : []
+               setCategory(data);
+            })
+
+        }
+        catch(err){
+            console.error('message: ',err);
+        }
+    }
+
+    useEffect(() => {getCategoryMegaMenu()}, [])
     return(
         <div className="">
             <HeaderPanelAdmin id = {4} />
             <div className=" top-20 absolute w-full  min-h-screen bg-[#252525]!  text-white z-10">
-                megaMenu category
+                <div className="flex flex-col items-center justify-around ">
+                    <Link to = "/panelAdmin/megaMenu/AddList">
+                        <button className="mt-10 border-0 flex items-center justify-center text-green-500  text-lg hover:tracking-[.2rem] duration-200">create category</button>
+                    </Link>
+                    
+
+                    <h1 className="text-center my-5 text-3xl hover:tracking-[.3rem] duration-200">MegaMenu: List table</h1> 
+                </div>
+              
+                <div className="w-[90%]  mx-auto flex flex-col">
+                    <table className="w-full">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>List</th>
+                                <th>Title</th>
+                                <th>status</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {lists.map((list, ind) => { 
+                                return(
+                                    <tr key = {ind}>
+                                        <th>{list.id}</th>
+                                        <th>{list.list}</th>
+                                        <th>{list.title}</th>
+                                        <th className = {`${list.status == 10 ? "text-green-400" : 'text-rose-400' }`}>
+                                            {list.status == 10 ? 'enable' : 'disable'}
+                                        </th>
+
+                                        <th className="flex justify-center items-center gap-7! max-md:flex-col max-md:gap-1!" >
+                                            
+                                            <div 
+                                                onClick={() => {chageStatus(list.id)}}
+                                                className="text-yellow-500 cursor-pointer duration-200 hover:text-yellow-300 "
+                                            >
+                                                change status
+                                            </div>
+
+                                            <Link to = {`/panelAdmin/megaMenu/EditList/${list.id}/edit`}>
+                                                <div className="text-sky-500 cursor-pointer duration-200 hover:text-blue-700 ">edit</div>
+                                            </Link>
+                                            
+                                            <div 
+                                                onClick={() => {deleteItem(list.id)}}
+                                                className="text-rose-500 cursor-pointer duration-200 hover:text-red-700!"
+                                            >
+                                                delete
+                                            </div>
+                                        </th>
+                                    </tr>
+                                )
+                            })}
+
+            
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     )
