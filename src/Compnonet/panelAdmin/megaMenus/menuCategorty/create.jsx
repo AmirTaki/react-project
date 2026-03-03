83c5039d7b3e2c9b76995d071ce29dc94d15f565
amir <amirtaki623@gmail.com>
2026-03-03 17:14:41 +0330

    megaMenus -> menuCategory -> create.jsx reducer

--- src/Compnonet/panelAdmin/megaMenus/menuCategorty/create.jsx
+++ src/Compnonet/panelAdmin/megaMenus/menuCategorty/create.jsx
@@ -1,6 +1,19 @@
+import { useReducer } from "react";
 import HeaderPanelAdmin from "../../header/header";
+import { reducer } from "../../../auth/reducer";
 
 const CreateMegaMenuCategory = () => {
+
+    const reducer = (state, action) => {
+        switch(action.type){
+            case "":
+                return {...state}
+
+            default :
+                return state
+        }
+    }
+    const [state, dispatch] =  useReducer(reducer, {})
     return(
         <div className="">
             <HeaderPanelAdmin id = {4}/>
