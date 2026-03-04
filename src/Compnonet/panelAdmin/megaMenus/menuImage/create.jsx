31727b426d70a3bd75dc1d427d6a25c27dc64b10
amir <amirtaki623@gmail.com>
2026-03-04 14:26:53 +0330

    panelAdmin -> megaMenus -> menuImage ->create.jsx: check():image

--- src/Compnonet/panelAdmin/megaMenus/menuImage/create.jsx
+++ src/Compnonet/panelAdmin/megaMenus/menuImage/create.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 import HeaderPanelAdmin from "../../header/header"
-Link
+
 const CreateMegaMenuImage = () => {
     return (
         <div className="">
@@ -10,18 +10,42 @@ const CreateMegaMenuImage = () => {
                     <h1 className="text-4xl my-5 hover:tracking-[.4rem] duration-200 ">ADD ITEM</h1>
 
                     <form>
-                        {/* category */}
+
+                        <div className="flex flex-col justify-center items-center my-10 ">
+                            <img src="" alt="" />
+                            view image
+                        </div>
+                        {/* image */}
                         <div className="flex gap-5 items-center justify-center">
-                            <label htmlFor="category" className="text-blue-500">category</label>
+                            
+                            <label htmlFor="image" className="text-blue-500">image</label>
+
                             <input 
-                                value = {state.category}
-                                type="text" id = "category" className="border-2 w-[300px] rounded-md h-10 p-2"
-                                onChange={(e) => {dispatch({type: 'category', payload: e.target.value})}}
+                                // value = {state.category}
+                                type="file" id = "image" className="border-2 w-[300px] rounded-md h-10 p-2"
+                                placeholder="select image ...."
+                                // onChange={(e) => {dispatch({type: 'category', payload: e.target.value})}}
                             ></input>
                         </div>
                         <div className="text-gray-500 py-5">message:
                             <span className="text-red-600 px-2">
-                                {state.warningCategory}
+                                {/* {state.warningCategory} */}
+                            </span>
+                        </div>
+                        
+                        <hr className="my-8"/>
+                        {/*---- */}
+                        <div className="flex gap-5 items-center justify-center">
+                            <label htmlFor="category" className="text-blue-500">category</label>
+                            <input 
+                                // value = {state.category}
+                                type="text" id = "category" className="border-2 w-[300px] rounded-md h-10 p-2"
+                                // onChange={(e) => {dispatch({type: 'category', payload: e.target.value})}}
+                            ></input>
+                        </div>
+                        <div className="text-gray-500 py-5">message:
+                            <span className="text-red-600 px-2">
+                                {/* {state.warningCategory} */}
                             </span>
                         </div>
 
@@ -31,23 +55,23 @@ const CreateMegaMenuImage = () => {
                             
                             <label htmlFor="title" className="text-blue-500">title</label>
                             <select
-                                onChange={(e) => {dispatch({type: 'title', payload: e.target.value})}}
+                                // onChange={(e) => {dispatch({type: 'title', payload: e.target.value})}}
                                 id = "title" className="bg-[#252525]!  text-white border-2 w-[300px] rounded-md h-13 p-2 "
                             >
                                 <option value= "" className="hidden">select one option ?</option>
-                                {title?.map((t) => {
+                                {/* {title?.map((t) => {
                                     return(
                                         <option  key = {t.id} value={t.title}>
                                             {t.title}
                                         </option>
                                     )
-                                })}
+                                })} */}
                             </select>
                         </div> 
                         
                         <div className="text-gray-500 py-5">message:
                             <span className="text-red-600 px-2">
-                                {state.warningTitle}
+                                {/* {state.warningTitle} */}
                             </span>
                         </div>
                        
@@ -57,47 +81,30 @@ const CreateMegaMenuImage = () => {
                             
                             <label htmlFor="list" className="text-blue-500">list</label>
                             <select
-                                onChange={(e) => {dispatch({type: 'list', payload: e.target.value})}}
+                                // onChange={(e) => {dispatch({type: 'list', payload: e.target.value})}}
                                 id = "list" className="bg-[#252525]!  text-white border-2 w-[300px] rounded-md h-13 p-2 "
                             >
                                 <option value= "" className="hidden">select one option ?</option>
-                                {lists?.map((li) => {
+                                {/* {lists?.map((li) => {
                                     return(
                                         <option  key = {li.id} value={li.list}>
                                             {li.list}
                                         </option>
                                     )
-                                })}
+                                })} */}
                             </select>
                         </div> 
                         
                         <div className="text-gray-500 py-5">message:
                             <span className="text-red-600 px-2">
-                                {state.warningList}
+                                {/* {state.warningList} */}
                             </span>
                         </div>
-                        
-                        <hr className="my-8"/>
-                        
-                        {/* sign */}
-                        <div className="flex gap-5 items-center justify-center">
-                            <label htmlFor="sign" className="text-blue-500">sign</label>
-                            <input 
-                                value = {state.sign}
-                                type="text" id = "sign" className="border-2 w-[300px] rounded-md h-10 p-2"
-                                onChange={(e) => {dispatch({type: 'sign', payload: e.target.value})}}
-                            ></input>
-                        </div>
-                        <div className="text-gray-500 py-5">message:
-                            <span className="text-red-600 px-2">
-                                {/* {state.w} */}
-                            </span>
-                        </div>              
-                            
+                                          
                         <hr className="my-8"/>
                         <div className="flex justify-center items-center">
                             <input 
-                                onClick={(event) => {addList(event)}}
+                                // onClick={(event) => {addList(event)}}
                                 type="submit" value = "ADD" 
                                 className="border-2 px-4 py-2 rounded-xl cursor-pointer hover:text-green-600 duration-300 hover:border-green-600" 
                             />
