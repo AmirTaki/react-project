ab50c956618d792634a52a590f90c25bc9efa227
amir <amirtaki623@gmail.com>
2026-03-09 23:31:04 +0330

    panelAdmin -> session -> index.jsx : delete.php

--- src/Compnonet/panelAdmin/session/backGroundSlider/index.jsx
+++ src/Compnonet/panelAdmin/session/backGroundSlider/index.jsx
@@ -32,6 +32,18 @@ const BackGroundSliderPanelAdmin = () => {
         }
     }
 
+    const deleteItem = async (id) => {
+        try{
+            await api.delete(`tables/session/backGroundSlider/delete.php/${id}/delete`).then((res) => {
+                res.data;
+                GetBackGroundSlider();
+            })
+        }
+        catch(err){
+            console.error('message: ', err);
+        }
+    }
+
     return(
         <div>
             <HeaderPanelAdmin id = {7} />
@@ -81,7 +93,7 @@ const BackGroundSliderPanelAdmin = () => {
                                             </Link>
                                             
                                             <div 
-                                                // onClick={() => {deleteItem(img.id)}}
+                                                onClick={() => {deleteItem(img.id)}}
                                                 className="text-rose-500 cursor-pointer duration-200 hover:text-red-700!"
                                             >
                                                 delete
