9ba119c618fb792b42470a414f3c02606d1d8a0e
amir <amirtaki623@gmail.com>
2026-03-14 15:35:41 +0330

    panelAdmin -> session -> popular -> index.jsx: getPopularRequest()

--- src/Compnonet/panelAdmin/session/popular/index.jsx
+++ src/Compnonet/panelAdmin/session/popular/index.jsx
@@ -4,24 +4,25 @@ import HeaderPanelAdmin from "../../header/header";
 import { Link } from "react-router-dom";
 
 const PopularPanelAdmin = () => {
-    const [menus, setMenus] =  useState([]);
-    const getMenus = async () => {
+    const [popular, setPopular] =  useState([]);
+   
+    const getPopularRequest = async () => {
         try{
-            await api.get('tables/megaMenu/menus/menu.php', {withCredentials: true}).then((res) => {
+            await api.get('tables/session/papular/papular.php', {withCredentials: true}).then((res) => {
                 const data = Array.isArray(res.data) ? res.data : [];
-                setMenus(data);
+                setPopular(data);
             })
         }
         catch(err){
             console.error('message: ', err);
         }
     }
-    useEffect(() => {getMenus()}, [])
+    useEffect(() => {getPopularRequest()}, [])
 
 
     const chageStatus = async (id) => {
         try {
-            await api.get(`/tables/megaMenu/menus/status.php/${id}/changeStatus`, {withCredentials: true}).then((res) => {
+            await api.get(``, {withCredentials: true}).then((res) => {
                 res;
                 getMenus();
             })
@@ -33,7 +34,7 @@ const PopularPanelAdmin = () => {
 
     const deleteItem = async (id) => {
         try {
-            await api.delete(`tables/megaMenu/menus/delete.php/${id}/delete`).then((res) => {
+            await api.delete(``).then((res) => {
                 res.data;
                 getMenus();
             })
@@ -48,12 +49,12 @@ const PopularPanelAdmin = () => {
             <HeaderPanelAdmin id = {2} />
             <div className=" top-20 absolute w-full  min-h-screen bg-[#252525]!  text-white z-10">
             <div className="flex flex-col items-center justify-around ">
-                <Link to = "/PanelAdmin/header/AddMenu">
-                    <button className="mt-10 border-0 flex items-center justify-center text-green-500  text-lg hover:tracking-[.2rem] duration-200">create menu</button>
+                <Link to = "">
+                    <button className="mt-10 border-0 flex items-center justify-center text-green-500  text-lg hover:tracking-[.2rem] duration-200">create popular</button>
                 </Link>
                 
 
-                <h1 className="text-center my-5 text-3xl hover:tracking-[.3rem] duration-200"> Menu Header table</h1> 
+                <h1 className="text-center my-5 text-3xl hover:tracking-[.3rem] duration-200"> papular table</h1> 
             </div>
     
         <div className="w-[90%]  mx-auto flex flex-col">
@@ -67,30 +68,30 @@ const PopularPanelAdmin = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {menus.map((menu, ind) => { 
+                    {popular.map((item, ind) => { 
                         return(
                             <tr key = {ind}>
-                                <th>{menu.id}</th>
-                                <th>{menu.title}</th>
-                                <th className = {`${menu.status == 10 ? "text-green-400" : 'text-rose-400' }`}>
-                                    {menu.status == 10 ? 'enable' : 'disable'}
+                                <th>{ind + 1}</th>
+                                <th>{item.title}</th>
+                                <th className = {`${item.status == 10 ? "text-green-400" : 'text-rose-400' }`}>
+                                    {item.status == 10 ? 'enable' : 'disable'}
                                 </th>
 
                                 <th className="flex justify-center items-center gap-7! max-md:flex-col max-md:gap-1!" >
                                     
                                     <div 
-                                        onClick={() => {chageStatus(menu.id)}}
+                                        onClick={() => {chageStatus(item.id)}}
                                         className="text-yellow-500 cursor-pointer duration-200 hover:text-yellow-300 "
                                     >
                                         change status
                                     </div>
 
-                                    <Link to = {`/PanelAdmin/header/EditMenu/${menu.id}/edit`}>
+                                    <Link to = {``}>
                                         <div className="text-sky-500 cursor-pointer duration-200 hover:text-blue-700 ">edit</div>
                                     </Link>
                                     
                                     <div 
-                                        onClick={() => {deleteItem(menu.id)}}
+                                        onClick={() => {deleteItem(item.id)}}
                                         className="text-rose-500 cursor-pointer duration-200 hover:text-red-700!"
                                     >
                                         delete
