b43983b363291e5a55f7afdd16203a4431370c06
amir <amirtaki623@gmail.com>
2026-03-16 02:30:34 +0330

    panelAdmin -> footer -> menuFooter -> index.jsx : changeStatus()

--- src/Compnonet/panelAdmin/footer/menuFooter/index.jsx
+++ src/Compnonet/panelAdmin/footer/menuFooter/index.jsx
@@ -22,7 +22,7 @@ const MenuFooterPanelAdmin = () => {
 
     const changeStatus = async (id) => {
         try {
-            await api.get(`/${id}/changeStatus`, {withCredentials: true}).then((res) => {
+            await api.get(`tables/footer/menuFooter/status.php/${id}/changeStatus`, {withCredentials: true}).then((res) => {
                 res;
                 getMenuFooterRequest();
             })
