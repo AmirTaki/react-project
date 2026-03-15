ec02cb1eef3fe39df3f33ddc43318df1574ecf11
amir <amirtaki623@gmail.com>
2026-03-15 19:00:11 +0330

    panelAdmin -> session -> advertBoard -> index.jsx : requestAdvertBoard && table

--- src/Compnonet/panelAdmin/session/advertBoard/index.jsx
+++ src/Compnonet/panelAdmin/session/advertBoard/index.jsx
@@ -1,10 +1,83 @@
+import { Link } from "react-router-dom";
 import HeaderPanelAdmin from "../../header/header";
+import { useEffect, useState } from "react";
+import api from "../../../../axiosConfig";
 
 const AdvertBoardPanelAdmin = () => {
+    const [advertImage, setAdvertImage] = useState([])
+    const requestAdvertBoard = async () => {
+        try{
+            await api.get('tables/session/advertBoard/advert.php', {withCredentials: true}).then((res) => {
+                const data = Array.isArray(res.data) ? res.data : [];
+                setAdvertImage(data)
+            })
+        }
+        catch(err){
+            console.error('message: ',err)
+        }
+    }
+    useEffect(() => {requestAdvertBoard()}, [])
     return(
         <>
             <HeaderPanelAdmin  id = {13}/>
             <div className=" top-20 absolute w-full  min-h-screen bg-[#252525]!  text-white z-10">
+                <div className="flex flex-col items-center justify-around ">
+                    <Link to = "">
+                        <button className="mt-10 border-0 flex items-center justify-center text-green-500  text-lg hover:tracking-[.2rem] duration-200">create advert</button>
+                    </Link>
+                    <h1 className="text-center my-5 text-3xl hover:tracking-[.3rem] duration-200"> advert board table</h1> 
+                </div>
+
+                <div className="w-[90%]  mx-auto flex flex-col">
+                    <table className="w-full">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Title</th>
+                                <th>Caption</th>
+                                <th>Description</th>
+                                <th>Status</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {advertImage.map((item, ind) => { 
+                                return(
+                                    <tr key = {ind}>
+                                        <th>{ind + 1}</th>
+                                        <th>{item.title}</th>
+                                        <th>{item.body}</th>
+                                        <th>{item.description}</th>
+                                        <th className = {`${item.status == 10 ? "text-green-400" : 'text-rose-400' }`}>
+                                            {item.status == 10 ? 'enable' : 'disable'}
+                                        </th>
+
+                                        <th className="flex justify-center items-center gap-7! max-md:flex-col max-md:gap-1!" >
+                                            
+                                            <div 
+                                                onClick={() => {changeStatus(item.id)}}
+                                                className="text-yellow-500 cursor-pointer duration-200 hover:text-yellow-300 "
+                                            >
+                                                change status
+                                            </div>
+
+                                            <Link to = {`/panelAdmin/session/EditPopular/${item.id}/edit`}>
+                                                <div className="text-sky-500 cursor-pointer duration-200 hover:text-blue-700 ">edit</div>
+                                            </Link>
+                                            
+                                            <div 
+                                                onClick={() => {deleteItem(item.id)}}
+                                                className="text-rose-500 cursor-pointer duration-200 hover:text-red-700!"
+                                            >
+                                                delete
+                                            </div>
+                                        </th>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </>
     )
