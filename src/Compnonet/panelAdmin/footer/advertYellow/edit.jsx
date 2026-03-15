a7405b5a98de1d01f6b43b0cdcdb6a8c4755e1a0
amir <amirtaki623@gmail.com>
2026-03-15 23:40:34 +0330

    footer -> advertYellow -> requstApiAdvert

--- src/Compnonet/panelAdmin/footer/advertYellow/edit.jsx
+++ src/Compnonet/panelAdmin/footer/advertYellow/edit.jsx
@@ -81,7 +81,7 @@ const EditFooterAdvertYellow = () => {
 
     return(
         <div className="">
-            <HeaderPanelAdmin  id = {16}/>
+            <HeaderPanelAdmin  id = {17}/>
             <div className=" top-20 absolute w-full  min-h-screen bg-[#252525]!  text-white z-10">
                 <div className="flex justify-center items-center flex-col ">
                     <h1 className="my-4 text-4xl hover:tracking-[.3rem] duration-200 hover:text-sky-400">edit items</h1>
